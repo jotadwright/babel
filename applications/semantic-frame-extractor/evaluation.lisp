42f1506168f0ac0747a77324dbed8f9ae7266831
Jens Nevens <jnevens@vub.ac.be>
2018-10-16 09:49:23 +0200

    Changed evaluation to work with json-stream-process. Changed process-entry fuction: decoding and encoding is handled by json-stream-process

--- applications/semantic-frame-extractor/evaluation.lisp
+++ applications/semantic-frame-extractor/evaluation.lisp
@@ -9,10 +9,9 @@
     (trivial-timeout:timeout-error (error)
       'timeout)))
 
-(defun process-entry (json-object &key (time-out 30))
+(defun process-entry (decoded-json-object &key (time-out 30))
   ""
-  (let* ((decoded-json-object (decode-json-from-string json-object))
-         (sentence (cdr (assoc :sentence decoded-json-object)))
+  (let* ((sentence (cdr (assoc :sentence decoded-json-object)))
          (sentence-id (assoc :sentence-id  decoded-json-object))
          (article-id (assoc :article-id  decoded-json-object)))
 
@@ -24,34 +23,32 @@
         
         (if (eql frame-set 'timeout)
           
-          (encode-json-alist-to-string
-           `(,sentence-id
-             ("sentence" . ,sentence)
-             ("frame-elements" . nil)
-             ,article-id
-             ("timeout" . t)))
-
+         `(,sentence-id
+           ("sentence" . ,sentence)
+           ("frame-elements" . nil)
+           ,article-id
+           ("timeout" . t))
+        
           (when (pie::entities frame-set)
             (let ((cause (frame-extractor::cause (first (pie::entities frame-set)))) ;;what if there are more?
                   (effect (frame-extractor::effect (first (pie::entities frame-set)))))
               (when (and cause effect)
-                (encode-json-alist-to-string
-                 `(,sentence-id
-                   ("sentence" . ,sentence)
-                   ("frame-elements" (("frame-evoking-element" . ,(pie::frame-evoking-element (first (pie::entities frame-set))))
-                                      ("cause" . ,cause)
-                                      ("effect" . ,effect)))
-                   ,article-id
-                   ("timeout" . nil)))))))))))
+                `(,sentence-id
+                  ("sentence" . ,sentence)
+                  ("frame-elements" (("frame-evoking-element" . ,(pie::frame-evoking-element (first (pie::entities frame-set))))
+                                     ("cause" . ,cause)
+                                     ("effect" . ,effect)))
+                  ,article-id
+                   ("timeout" . nil))))))))))
 
 
 (defun evaluate-guardian-grammar-in-parallel (inputfile outputfile &optional (time-out 30))   
-  (process-corpus :function #'process-entry
-                  :function-kwargs (list :time-out time-out)
-                  :inputfile inputfile
-                  :outputfile outputfile
-                  :number-of-threads 8
-                  :number-of-lines-per-thread 50))
+  (json-stream-process :function #'process-entry
+                       :function-kwargs (list :time-out time-out)
+                       :input-file inputfile
+                       :output-file outputfile
+                       :number-of-threads 8
+                       :number-of-objects-per-thread 50))
 
 (evaluate-guardian-grammar-in-parallel (babel-pathname :directory '(:up "Corpora" "Guardian")
                                                        :name "guardian-article-sentences-one-sentence-per-line"
