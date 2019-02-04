5e8f84dc3cdb7259e85e8cdc91db0c5b64522e38
Vanja Sophie Warms-Cangalovic <vanja@uni-bremen.de>
2019-02-04 22:30:12 +0100

    added simple wordlevel evaluation-function to frame-extractor

--- applications/semantic-frame-extractor/evaluate-guardian-annotations.lisp
+++ applications/semantic-frame-extractor/evaluate-guardian-annotations.lisp
@@ -29,57 +29,37 @@
   "Cleans up the slot filler of given frame element by downcasing and replacing punctuation."
   (downcase (string-trim " ." (cl-ppcre:regex-replace-all "\\s+" (cl-ppcre:regex-replace-all "[0-9]+|'|%" (cl-ppcre:regex-replace-all "-" (cl-ppcre:regex-replace-all "[,|\.|\"|:]" (cdr frame-elem) "") " ") #'(lambda (match &rest registers) (format nil " ~A" match)) :simple-calls t) " "))))
 
+(defun clean-string (s)
+  "Cleans up the given string by downcasing and replacing punctuation."
+  (downcase (string-trim " ." (cl-ppcre:regex-replace-all "\\s+" (cl-ppcre:regex-replace-all "[0-9]+|'|%" (cl-ppcre:regex-replace-all "-" (cl-ppcre:regex-replace-all "[,|\.|\"|:]" s "") " ") #'(lambda (match &rest registers) (format nil " ~A" match)) :simple-calls t) " "))))
+
 (defun coarse-frame-similarity (this-frame other-frame)
   "Returns similarity between given frames via string matching."
   (length
     (intersection this-frame other-frame :key #'clean-slot-filler :test #'string=)))
 
-(defun substring-wordlevel-frame-similarity (this-frame other-frame)
-  "Checks for matching substrings and returns similarity between given frames (correct words, total words and correct words per total words)."
-  (let* ((this-count (length this-frame))
-         (other-count (length other-frame))
-         (this-padded (concatenate 'list this-frame (make-list (max 0 (- other-count this-count)) :initial-element nil)))
-         (other-padded (concatenate 'list other-frame (make-list (max 0 (- this-count other-count)) :initial-element nil)))
-         (similarities (loop for this-slot-filler in this-padded
-                            for other-slot-filler in other-padded
-                            for this = (split-sequence:split-sequence #\Space (clean-slot-filler this-slot-filler))
-                            for other = (split-sequence:split-sequence #\Space (clean-slot-filler other-slot-filler))
-                            if (or (search this other :test #'string=)
-                                   (search other this :test #'string=))
-                              collect (/ (min (list-length this) (list-length other))
-                                       (max (list-length this) (list-length other))) into percentages
-                              and collect (list (min (list-length this) (list-length other))
-                                                (max (list-length this) (list-length other))) into fractions
-                            else
-                              collect 0 into percentages
-                              and collect (list 0 (list-length other)) into fractions
-                            finally (return (list percentages fractions)))))
+(defun evaluate-sentence-on-wordlevel (frames-alignment sentence)
+  "Returns number of correctly parsed words and number of total words in given sentence from given parse annotations and gold standard."
+  (let* ((clean-sentence-list (split-sequence:split-sequence #\Space (clean-string sentence)))
+         (parsed-frames (mapcar #'car frames-alignment))
+         (gold-frames (mapcar #'cadr frames-alignment))
+         (similarities (loop for parsed-frame in parsed-frames
+                             for gold-frame in gold-frames
+                             append (loop for parsed-slot-filler in parsed-frame
+                                          for gold-slot-filler in gold-frame
+                                          for parsed = (split-sequence:split-sequence #\Space (clean-slot-filler parsed-slot-filler))
+                                          for gold = (split-sequence:split-sequence #\Space (clean-slot-filler gold-slot-filler))
+                                          if (or (search parsed gold :test #'string=)
+                                                 (search gold parsed :test #'string=))
+                                          collect (list (min (list-length parsed) (list-length gold))
+                                                (max (list-length parsed) (list-length gold))) ; based on assumption that annotated frame does not contain anything from outside the original sentence
+                                          else
+                                          collect (list 0 (list-length gold))))))
     (if similarities
-      (list (/ (reduce #'+ (car similarities)) (list-length (car similarities)))
-              (list
-                (reduce #'+ (cadr similarities) :key #'car)
-                (reduce #'+ (cadr similarities) :key #'cadr)))
-      (list 0 0))))
-
-(defun substring-frame-similarity (this-frame other-frame)
-  "Checks for matching substrings and returns similarity between given frames in correct characters per total characters."
-  (let* ((this-count (length this-frame))
-         (other-count (length other-frame))
-         (this-padded (concatenate 'list this-frame (make-list (max 0 (- other-count this-count)) :initial-element nil)))
-         (other-padded (concatenate 'list other-frame (make-list (max 0 (- this-count other-count)) :initial-element nil)))
-         (similarities (loop for this-slot-filler in this-padded
-                            for other-slot-filler in other-padded
-                            for this = (clean-slot-filler this-slot-filler)
-                            for other = (clean-slot-filler other-slot-filler)
-                            if (or (search this other)
-                                   (search other this))
-                            collect (/ (min (length this) (length other))
-                                       (max (length this) (length other)))
-                            else
-                            collect 0)))
-    (if similarities
-      (/ (reduce #'+ similarities) (list-length similarities))
-      0)))
+      (list 
+        (+ (- (list-length clean-sentence-list) (reduce #'+ similarities :key #'cadr)) (reduce #'+ similarities :key #'car))
+        (list-length clean-sentence-list))
+    (list 0 0))))
 
 (defun frame-slots (this-frame other-frame)
   "Returns number of different frame slots in given frames."
@@ -108,7 +88,7 @@
                 (lambda (perm)
                     (mapcar (lambda (this that) (cons this that))
                             these-padded perm))
-                (all-permutations other-padded))
+                (list (all-permutations other-padded)))
                #'> :key
                (lambda (e)
                    (reduce #'+ (mapcar (lambda (pair) (coarse-frame-similarity (car pair) (cdr pair))) e)))))))
@@ -117,39 +97,23 @@
   "Assigns the number of correct frames and frame-slot-fillers to each given sentence-output."
   (let* ((frames (cdr (assoc :frame-elements sentence-structure)))
          (annotated (cdr (assoc :annotated-frame-elements sentence-structure)))
+         (sentence (cdr (assoc :sentence sentence-structure)))
          (alignment (bruteforce-alignment frames annotated))
          (frame-similarity
           (mapcar (lambda (pair)
-                    (list (coarse-frame-similarity (car pair) (cdr pair))
-                          (frame-slots (car pair) (cdr pair))))
+                    (list (coarse-frame-similarity (car pair) (cadr pair))
+                          (frame-slots (car pair) (cadr pair))))
                   alignment))
-         ;(char-similarity not needed
-         ; (mapcar (lambda (pair)
-         ;           (substring-frame-similarity (car pair) (cdr pair)))
-         ;         alignment))
-         (wordlevel-stats
-          (mapcar (lambda (pair)
-                    (substring-wordlevel-frame-similarity (car pair) (cdr pair)))
-                  alignment))
-         (word-percentage-similarity
-           (list (caar wordlevel-stats)))
-         (word-similarity-counts
-           (cdar wordlevel-stats)))
+         (wordlevel-result (evaluate-sentence-on-wordlevel alignment sentence)))
     (append sentence-structure
             (list
-             (cons
-               :word-similarity-counts
-               word-similarity-counts)
-             (cons
-              :word-percentage-similarity
-              word-percentage-similarity)
-             ;(cons
-             ; :char-similarity
-             ; char-similarity)
-             (cons
+              (cons
+                :wordlevel-result
+                wordlevel-result)
+              (cons
               :frame-similarity
               frame-similarity)
-             (cons
+              (cons
               :slot-similarity
               (reduce (lambda (a v) (mapcar #'+ a v)) frame-similarity :initial-value (list 0 0)))))))
 
@@ -158,21 +122,11 @@
    over a set of sentences."
   (reduce (lambda (a v) (mapcar #'+ a v)) (mapcar (lambda (v) (cdr (assoc :slot-similarity v))) sentences) :initial-value (list 0 0)))
 
-(defun total-word-similarity (sentences)
-  "Calculates the total number of words and the number of correct words
-   over a set of sentences."
-  (reduce (lambda (a v) (mapcar #'+ a v)) (mapcar (lambda (v) (cadr (assoc :word-similarity-counts v))) sentences) :initial-value (list 0 0)))
-
 (defun total-correct-sentences (sentences)
   "Calculates the total number of sentences and the number of correct ones over a given set of sentences."
   (list (length
          (remove-if-not (lambda (slot-sim) (equal (first slot-sim) (second slot-sim))) sentences :key (lambda (sent) (cdr (assoc :slot-similarity sent)))))
         (length sentences)))
-
-(defun overall-similarity (sentences assoc-keyword)
-  "Calculates the overall, average 'assoc-keyword'-similarity in correct items per frame slot filler."
-  (/ (loop for sent in sentences
-        sum (second (assoc assoc-keyword sent))) (list-length sentences)))
 
 (defun evaluate-grammar-output-for-evoking-elem (evoking-elems)
   "Evaluates the frame-extractor output for given frame-evoking-elements by comparing it with corresponding annotations.
@@ -189,22 +143,19 @@
          (print-result (mapcar #'evaluate-sentence filtered-parsings))
          (total-correct-sentences (total-correct-sentences print-result))
          (total-slot-similarity (total-slot-similarity print-result))
-         (total-word-similarity (total-word-similarity print-result))
-         ;(overall-char-similarity (* 100 (overall-similarity print-result :char-similarity))) not needed
-         (overall-word-percentage-similarity (* 100 (overall-similarity print-result :word-percentage-similarity))))
+         (total-word-result (reduce (lambda (a v) (mapcar #'+ a v)) (mapcar (lambda (v) (cdr (assoc :wordlevel-result v))) print-result) :initial-value (list 0 0))))
     (spit-json (babel-pathname :directory '(:up "Corpora" "Guardian") :name "frame-extractor-output-with-annotations" :type "json")
                print-result)
     (format t "Incorrectly parsed sentences:~%~%")
     (loop for parsing in print-result
-          for result = (cdr (assoc :slot-similarity parsing))
-          when (not (equal (first result) (second result)))
-          do (format t "~s: ~s (slots) ~s (words)~%~%" (cdr (assoc :sentence parsing)) result (cadr (assoc :word-similarity-counts parsing)))
-          finally (format t "correct slots and total slots: ~s~%correct sentences and total sentences: ~s~%correct words per slot on average: ~$%~%correct words and total words: ~s~%correct words per total words as ratio: ~$%~%~%" total-slot-similarity total-correct-sentences overall-word-percentage-similarity total-word-similarity (* 100 (/ (car total-word-similarity) (cadr total-word-similarity)))))
+          for slot-result = (cdr (assoc :slot-similarity parsing))
+          when (not (equal (first slot-result) (second slot-result)))
+          do (format t "~s: ~s (slots) ~s (words)~%~%" (cdr (assoc :sentence parsing)) slot-result (cdr (assoc :wordlevel-result parsing)))
+          finally (format t "correct slots and total slots: ~s~%correct sentences and total sentences: ~s~%correct words and total words overall: ~s~%~%" total-slot-similarity total-correct-sentences total-word-result))
           (values
            total-slot-similarity
            total-correct-sentences
-           overall-word-percentage-similarity
-           total-word-similarity)))
+           total-word-result)))
 
 (defun spit-json (path-name output-list)
   "Encodes given alist into json and writes resulting json-objects into file of given name."
