dbbb7ce60055cb3d9f84b832ec418db20c76c880
paulvaneecke <paul@ai.vub.ac.be>
2019-02-25 17:17:42 +0100

    solved bugs in reading conll line + some testing

--- applications/semantic-frame-extractor/conll-evaluation.lisp
+++ applications/semantic-frame-extractor/conll-evaluation.lisp
@@ -10,29 +10,52 @@
 
 (defun read-corpus (corpus)
   ""
-  (let ((corpus-lines (with-open-file (inputstream corpus :direction :input)
+  (let* ((corpus-lines (with-open-file (inputstream corpus :direction :input)
                         (loop for line = (read-line inputstream nil nil)
                               while line
-                              collect (read-conll-line line)))))
-    corpus-lines))
+                              collect (read-conll-line line))))
+        (corpus-sentences (loop with sentence = nil
+                                with all-sentences = nil
+                                for line in corpus-lines
+                                if (and sentence (= 1 (cdr (assoc :ID line))))
+                                do
+                                (setf all-sentences (append all-sentences (list sentence)))
+                                (setf sentence (list line))
+                                else do (setf sentence (append sentence (list line)))
+                                finally
+                                (progn (setf all-sentences (append all-sentences (list sentence)))
+                                  (return all-sentences)))))
+    corpus-sentences))
+
+;; (pprint (read-corpus *corpus*))
+;; (setf *conll-corpus*  (read-corpus *corpus*))
+;; (setf *conll-sentence* (first (read-corpus *corpus*)))
 
 (defun read-conll-line (string)
   (let* ((parts (split-sequence:split-sequence #\Space string :remove-empty-subseqs t))
-         (line-id (first parts))
-         (id (second parts))
+         (line-id (read-from-string (first parts)))
+         (id (read-from-string (second parts)))
          (form (third parts))
          (lemma (fourth parts))
          (upos (fifth parts))
          (xpos (sixth parts))
          (feats (seventh parts))
-         (head (eighth parts))
+         (head (read-from-string (eighth parts)))
          (deprel (ninth parts)))
-   string 
-  ))
+    `((:line-id . ,line-id)
+      (:id . ,id)
+      (:form . ,form)
+      (:lemma . ,lemma)
+      (:upos . ,upos)
+      (:xpos . ,xpos)
+      (:feats . ,feats)
+      (:head . ,head)
+      (:deprel . ,deprel))))
 
 
-(find-all 12 (read-corpus *corpus*) :test #'< :key #'length)
-                        
+;; testing
+(set-configuration *fcg-constructions* :de-render-mode :de-render-conll)
+(pie-comprehend (third *conll-corpus*))
                         
     
     
