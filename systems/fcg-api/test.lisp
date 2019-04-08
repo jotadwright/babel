fc9fb275dd2c90347808f198a6f13716e503151f
paulvaneecke <paul@ai.vub.ac.be>
2019-04-08 10:56:21 +0200

    systems/fcg-api/README.md
    started on fcg-api with define-constructicon and define-cxn

---
+++ systems/fcg-api/test.lisp
@@ -0,0 +1,142 @@
+;; (ql:quickload :fcg-api)
+
+(in-package :fcg-api)
+
+(activate-monitor trace-fcg)
+
+(define-constructicon fcg-api-demo-grammar
+  :feature-types ((args sequence)
+                  (form set-of-predicates)
+                  (meaning set-of-predicates)
+                  (constituents sequence)
+                  (dependents sequence))
+  :hierarchy-features (constituents dependents)
+  
+  ;; Lexical constructions
+  (define-cxn the-cxn
+               ((?the-word
+                 (args (?x))
+                 (sem-cat (sem-class referent))
+                 (syn-cat (lex-class article)))
+                <-
+                (?the-word
+                 (HASH meaning ((unique ?x)))                     
+                 --
+                 (HASH form ((string ?the-word  "the"))))))
+
+  (define-cxn mouse-cxn
+               ((?mouse-word
+                 (args (?x))
+                 (sem-cat (sem-class physical-entity))
+                 (syn-cat (lex-class noun)))
+                <-
+                (?mouse-word
+                 (HASH meaning ((mouse ?x)))                     
+                 --
+                 (HASH form ((string ?mouse-word  "mouse"))))))
+  
+  (define-cxn likes-cxn
+               ((?likes-word
+                 (args (?x ?y))
+                 (sem-cat (sem-class relation))
+                 (syn-cat (lex-class verb)
+                          (type transitive)))
+                <-
+                (?likes-word
+                 (HASH meaning ((deep-affection ?x ?y)))                     
+                 --
+                 (HASH form ((string ?likes-word  "likes"))))))
+  
+  (define-cxn linguist-cxn
+               ((?linguist-word
+                 (args (?x))
+                 (sem-cat (sem-class physical-entity))
+                 (syn-cat (lex-class noun)))
+                <-
+                (?linguist-word
+                 (HASH meaning ((linguist ?x)))                     
+                 --
+                 (HASH form ((string ?linguist-word  "linguist"))))))
+  
+  ;;Grammatical Constructions
+  ;; NP -> ART NOUN
+  (define-cxn noun-phrase-cxn
+               ((?noun-phrase
+                 (args (?x))
+                 (sem-cat (sem-class referring-expression))
+                 (syn-cat (lex-class noun-phrase))
+                 (constituents (?article ?noun)))
+                (?noun
+                 (dependents (?article)))
+                <-
+                (?article
+                 (args (?x))
+                 (sem-cat (sem-class referent))
+                 --
+                 (syn-cat (lex-class article)))
+                (?noun
+                 (args (?x))
+                 (sem-cat (sem-class physical-entity))                   
+                 --
+                 (syn-cat (lex-class noun)))
+                (?noun-phrase
+                 --
+                 (HASH form ((meets ?article ?noun))))))
+  
+  ;; VP -> V
+  (define-cxn verb-phrase-cxn
+               ((?verb-phrase
+                 (args (?x ?y))
+                 (sem-cat (sem-class relational-expression))
+                 (syn-cat (lex-class verb-phrase)
+                          (type transitive))
+                 (constituents (?verb)))
+                <-
+                (?verb
+                 (args (?x ?y))
+                 (sem-cat (sem-class relation))
+                 --
+                 (syn-cat (lex-class verb)
+                          (type transitive)))))
+  
+  ;; Transitive-clause -> NP VP NP
+  (define-cxn transitive-clause-cxn
+               ((?transitive-clause
+                 (args (?x ?y))
+                 (sem-cat (sem-class predicating-expression))
+                 (syn-cat (lex-class transitive-clause))
+                 (constituents (?subject-noun-phrase ?verb-phrase ?object-noun-phrase)))
+                (?verb
+                 (dependents (?subject-noun ?object-noun)))
+                <-
+                (?subject-noun-phrase
+                 (args (?x))
+                 (sem-cat (sem-class referring-expression))
+                 (constituents (?subject-article ?subject-noun))
+                 --
+                 (syn-cat (lex-class noun-phrase))
+                 (constituents (?subject-article ?subject-noun)))
+                (?verb-phrase
+                 (args (?x ?y))
+                 (sem-cat (sem-class relational-expression))
+                 (constituents (?verb))
+                 --
+                 (syn-cat (lex-class verb-phrase)
+                          (type transitive))
+                 (constituents (?verb)))
+                (?object-noun-phrase
+                 (args (?y))
+                 (sem-cat (sem-class referring-expression))
+                 (constituents (?object-article ?object-noun))
+                 --
+                 (syn-cat (lex-class noun-phrase))
+                 (constituents (?object-article ?object-noun)))
+                (?transitive-clause
+                 --
+                 (HASH form ((meets ?subject-noun ?verb)
+                             (meets ?verb ?object-article)))))))
+
+(comprehend "the linguist likes the mouse" :cxn-inventory *fcg-constructions*)
+(produce '((mouse x) (linguist y)
+           (unique x) (unique y)
+           (deep-affection x y)) :cxn-inventory *fcg-constructions*)
