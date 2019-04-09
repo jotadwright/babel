a35bcf4fb4affb50d14d99ae457bb02dcb84f201
paulvaneecke <paul@ai.vub.ac.be>
2019-04-09 15:09:49 +0200

    parallel parsing of list of sentences

--- applications/semantic-frame-extractor/parse-guardian-parallel.lisp
+++ applications/semantic-frame-extractor/parse-guardian-parallel.lisp
@@ -43,18 +43,42 @@
                    ("timeout" . nil))))))))))
 
 
+
+(defun process-entry-comments (decoded-json-object &key (time-out 30))
+  ""
+  (let* ((comment decoded-json-object)
+         (sentences (get-penelope-sentence-tokens comment)))
+    
+    (format t "~%Comprehending \"~a\"~%"  sentences)
+    
+    (let* ((frame-sets (loop for sentence in sentences
+                             for frame-set =  (pie-comprehend-with-timeout sentence time-out)
+                             when frame-set
+                             collect it))
+           (aggregated-frame-set (loop for frame-set in frame-sets
+                                       when (and (not (eql frame-set 'timeout))
+                                                 (pie::entities frame-set))
+                                       append (pie::entities frame-set))))
+      
+      `((:sentence . ,comment)
+        (:frames . ,aggregated-frame-set)))))
+
+
+
 (defun evaluate-guardian-grammar-in-parallel (inputfile outputfile &optional (time-out 30))   
-  (json-stream-process :function #'process-entry
+  (json-stream-process :function #'process-entry-comments
                        :function-kwargs (list :time-out time-out)
                        :input-file inputfile
                        :output-file outputfile
                        :output-format :lines
-                       :number-of-threads 8
-                       :number-of-objects-per-thread 50))
+                       :number-of-threads 1
+                       :number-of-objects-per-thread 2000))
 
-(evaluate-guardian-grammar-in-parallel (babel-pathname :directory '(:up "Corpora" "Guardian")
-                                                       :name "guardian-article-sentences-one-sentence-per-line"
-                                                       :type "json")
-                                       (babel-pathname :directory '(:up "Corpora" "Guardian")
-                                                       :name "guardian-causation-frames-all"
-                                                       :type "json"))
+(evaluate-guardian-grammar-in-parallel "/Users/Shared/big-data-and-society/comments_x.txt"
+                                       "/Users/Shared/big-data-and-society/commments_x_frames.json")
+
+(evaluate-guardian-grammar-in-parallel "/Users/Shared/big-data-and-society/comments_y.txt"
+                                       "/Users/Shared/big-data-and-society/commments_y_frames.json")
+
+(activate-monitor trace-fcg)
+(pie-comprehend "@Rob Honeycutt - You are compelled to follow me around and post empty knee-jerk rejections of everything I say, but you can't or you refuse to answer simple questions: \n97% of climate scientists agree that some amount of A in AGW is caused by humans. \nIs that the conclusion of the Cook paper, or ain't it? If the Cook paper stated a quantity for A, tell us what it is. (watch rob squirm)")
