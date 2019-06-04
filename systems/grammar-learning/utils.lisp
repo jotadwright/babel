d5fb7cd40e0aac734597352558ddc59cc42ea09f
paulvaneecke <paul@ai.vub.ac.be>
2019-06-04 15:03:08 +0200

    grammar learning

---
+++ systems/grammar-learning/utils.lisp
@@ -0,0 +1,166 @@
+
+(in-package :grammar-learning)
+
+(defun non-overlapping-meaning (meaning cxn &key (nom-cxn nil) (nom-observation nil))
+  (when (and nom-cxn nom-observation) (error "only nom-cxn or nom-observeration can be true"))
+  (multiple-value-bind (non-overlapping-meaning-observation non-overlapping-meaning-cxn)
+      (non-overlapping-predicates meaning (extract-meaning-predicates cxn))
+      (cond (nom-cxn non-overlapping-meaning-cxn)
+            (nom-observation non-overlapping-meaning-observation))))
+
+(defun non-overlapping-form (utterance cxn &key (nof-cxn nil) (nof-observation nil))
+  (when (and nof-cxn nof-observation) (error "only nof-cxn or nof-observeration can be true"))
+  (multiple-value-bind (non-overlapping-form-observation non-overlapping-form-cxn)
+      (non-overlapping-predicates (form-constraints-with-variables utterance (get-configuration (cxn-inventory cxn) :de-render-mode))
+                                  (extract-form-predicates cxn))
+      (cond (nof-cxn non-overlapping-form-cxn)
+            (nof-observation non-overlapping-form-observation))))
+
+(defun non-overlapping-predicates (network-1 network-2)
+  (let ((unique-part-network-1 (set-difference network-1 network-2 :test #'irl:unify-irl-programs))
+        (unique-part-network-2 (set-difference network-2 network-1 :test #'irl:unify-irl-programs)))
+    (when (and (= (length unique-part-network-1)
+                  (length unique-part-network-2))
+               (irl:equivalent-irl-programs? (set-difference network-1 unique-part-network-1)
+                                             (set-difference network-2 unique-part-network-2)))
+      (values unique-part-network-1 unique-part-network-2))))
+
+
+(defgeneric make-cxn-name (thing cxn-inventory &key add-cxn-suffix))
+(defmethod make-cxn-name ((string string) (cxn-inventory fcg-construction-set) &key (add-cxn-suffix t))
+  "Transform an utterance into a suitable construction name"
+  (declare (ignore cxn-inventory))
+  (make-const (substitute #\- #\Space (upcase (if add-cxn-suffix
+                                                 (string-append string "-cxn")
+                                                 string)))))
+
+;; (make-cxn-name "What is the color of the cube" *fcg-constructions*) 
+
+(defmethod make-cxn-name ((form list) (cxn-inventory fcg-construction-set) &key (add-cxn-suffix t))
+  "Transform an utterance into a suitable construction name"
+  (make-cxn-name (format nil "~{~a~^-~}" (render form (get-configuration cxn-inventory :render-mode))) cxn-inventory))
+
+;; (make-cxn-name '((string ?x "x") (string ?y "y") (precedes ?y ?x)) *fcg-constructions*)
+;; (make-cxn-name '((precedes ?y ?x)) *fcg-constructions*)
+
+
+(defun form-constraints-with-variables (utterance mode)
+  "Extract form constraints from utterance in the format they would appear in a construction."
+  (let ((form-constraints-with-constants (remove 'sequence
+                                                 (extract-forms (left-pole-structure
+                                                                 (de-render utterance mode)))
+                                                 :key #'first)))
+    (loop for fc-with-const in form-constraints-with-constants
+          for first-fc-with-var = (first fc-with-const)
+          for rest-fc-with-var = (if (equal 'string (first fc-with-const))
+                                   (list (variablify (second fc-with-const)) (third fc-with-const))
+                                   (mapcar #'variablify (rest fc-with-const)))
+          collect (cons first-fc-with-var rest-fc-with-var))))
+
+;; (form-constraints-with-variables "what is the color of the cube" :de-render-string-meets-precedes)
+
+(defun variablify (symbol)
+  "Turn a symbol into a variable if it isn't one yet."
+  (if (variable-p symbol)
+    symbol
+    (intern (format nil "?~a" symbol))))
+
+;; (variablify 'x)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Finding possible alignments for predicate networks and form ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defclass alignment-state ()
+  ((assigned-predicates
+    :type (or list null)
+    :initform nil
+    :initarg :assigned-predicates
+    :accessor assigned-predicates)
+   (remaining-predicates-longest-list
+    :type (or list null)
+    :initform nil
+    :initarg :remaining-predicates-longest-list
+    :accessor remaining-predicates-longest-list)
+   (remaining-predicates-shortest-list
+    :type (or list null)
+    :initform nil
+    :initarg :remaining-predicates-shortest-list
+    :accessor remaining-predicates-shortest-list)))
+
+(defun all-possible-alignments (list-1 list-2 &key
+                                       (nr-of-unpaired-predicates-allowed 0))
+  (let* ((longest-list  (if (>= (length list-1) (length list-2))
+                        list-1 list-2))
+        (shortest-list (if (< (length list-1) (length list-2))
+                         list-1 list-2))
+        (queue (list (make-instance 'alignment-state
+                                    :assigned-predicates nil
+                                    :remaining-predicates-longest-list longest-list
+                                    :remaining-predicates-shortest-list shortest-list)))
+        (possible-alignments nil))
+    (loop until (not queue)
+          for current-state = (pop queue)
+          for assigned-predicates = (assigned-predicates current-state)
+          for remaining-predicates-longest-list = (remaining-predicates-longest-list current-state)
+          for remaining-predicates-shortest-list = (remaining-predicates-shortest-list current-state)
+          do
+          (if (or remaining-predicates-longest-list remaining-predicates-shortest-list)
+            (loop for predicate-1 in (cons nil remaining-predicates-longest-list)
+                  do
+                  (loop for predicate-2 in (cons nil remaining-predicates-shortest-list)
+                        for state = (make-instance 'alignment-state
+                                          :assigned-predicates (cons `(,predicate-1 ,predicate-2) assigned-predicates)
+                                          :remaining-predicates-longest-list (remove predicate-1 remaining-predicates-longest-list)
+                                          :remaining-predicates-shortest-list (remove predicate-2 remaining-predicates-shortest-list))
+                        if (and (or predicate-1 predicate-2)
+                                (if (and predicate-1 predicate-2)
+                                  (and (= (length predicate-1) (length predicate-2))
+                                       (equalp (first predicate-1) (first predicate-2))
+                                       (loop with equal-symbols = t
+                                             for el-1 in predicate-1
+                                             for el-2 in predicate-2
+                                             if (and (not (or (variable-p el-1) (variable-p el-2)))
+                                                     (not (equalp el-1 el-2)))
+                                             do (setf equal-symbols nil)
+                                             finally (return equal-symbols)))
+                                  t)
+                                (<= (count nil (mapcar #'first (assigned-predicates state))) nr-of-unpaired-predicates-allowed)
+                                (<= (count nil (mapcar #'second (assigned-predicates state))) nr-of-unpaired-predicates-allowed)
+                                (<= (length assigned-predicates) (+ (length longest-list) (length shortest-list))))
+                        do (push state queue)))
+            (unless (find assigned-predicates possible-alignments :test #'(lambda (l1 l2) (permutation-of? l1 l2 :test #'equal)))
+              (push assigned-predicates possible-alignments))))
+    possible-alignments))
+
+#| 
+(length (all-possible-alignments (shuffle '((get-context ?source)
+                                            (bind attribute-category ?attribute color)
+                                            (bind shape-category ?shape cube)
+                                            (unique ?object ?cubes)
+                                            (filter ?cubes ?source ?shape)
+                                            (query ?response ?object ?attribute)))
+                                 (shuffle '((get-context ?source)
+                                            (bind attribute-category ?attribute color)
+                                            (bind shape-category ?shape sphere)
+                                            (unique ?object ?spheres)
+                                            (filter ?spheres ?source ?shape)
+                                            (query ?response ?object ?attribute)))
+                                 :nr-of-unpaired-predicates-allowed 2))
+|#
+
+
+(defmethod irl::find-map-function ((v1 string) (v2 string) 
+                        &optional (frame (make-map-frame))
+                        &key (extension-test #'function-frame))
+  "Adding case for strings, used when comparing predicate networks"
+  (declare (ignore extension-test))
+      (when (string= v1 v2) 
+        frame))
+
+
+
+
+
