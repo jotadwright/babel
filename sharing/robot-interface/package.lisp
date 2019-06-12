9a31b6f91a14e13ba9105debba0453dece406c29
kbeuls <katrien@ai.vub.ac.be>
2019-06-12 13:39:19 +0200

    git subrepo clone gitlab@gitlab.ai.vub.ac.be:ehai/robot-interface.git sharing/robot-interface
    subrepo:
      subdir:   "sharing/robot-interface"
      merged:   "51aa20f"
    upstream:
      origin:   "gitlab@gitlab.ai.vub.ac.be:ehai/robot-interface.git"
      branch:   "master"
      commit:   "51aa20f"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ sharing/robot-interface/package.lisp
@@ -0,0 +1,5 @@
+(in-package :cl-user)
+
+(defpackage :robot-interface
+  (:use :common-lisp :nao-interface)
+  (:documentation "A high-level interface for using robots in the Babel multi-agent experiment framework"))
