baf24b2046940fdf60b4888fce6af28bb606c19a
kbeuls <katrien@ai.vub.ac.be>
2019-06-12 13:39:05 +0200

    temporarily removing robot interface for subrepo issues

--- sharing/robot-interface/package.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-user)
-
-(defpackage :robot-interface
-  (:use :common-lisp :nao-interface)
-  (:documentation "A high-level interface for using robots in the Babel multi-agent experiment framework"))
