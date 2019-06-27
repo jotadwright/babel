bb1287df5326f075bcbb7936cd25b3a43c17ffa4
Jens Nevens <jnevens@vub.ac.be>
2019-06-27 12:06:44 +0200

    need to run longer on cluster

--- experiments/multidimensional-word-meanings/experiment.lisp
+++ experiments/multidimensional-word-meanings/experiment.lisp
@@ -11,7 +11,7 @@
 (define-configuration-default-value :data-source :clevr)
 (define-configuration-default-value :data-sets (list "val"))
 (define-configuration-default-value :data-path
-                                    (merge-pathnames (make-pathname :directory '(:relative "CLEVR" "CLEVR-v1.0" "scenes" "cval"))
+                                    (merge-pathnames (make-pathname :directory '(:relative "CLEVR" "CLEVR-v1.0" "scenes" "bjorn"))
                                                      cl-user:*babel-corpora*))
 
 (define-configuration-default-value :determine-interacting-agents-mode :tutor-speaks)
