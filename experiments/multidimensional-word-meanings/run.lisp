4b01ecbbf48be2ee44d285f29643621518a93511
Jens Nevens <jnevens@vub.ac.be>
2019-07-02 16:57:15 +0200

    mwm job script

--- experiments/multidimensional-word-meanings/run.lisp
+++ experiments/multidimensional-word-meanings/run.lisp
@@ -52,24 +52,24 @@
 ;; ---------------------------------
 
 (run-experiments '(
-                   (test
+                   (cogent-test
                     ((:data-source . :clevr)
                      (:scale-world . nil)
-                     (:category-representation . :prototype)
+                     (:category-representation . :min-max)
                      (:determine-interacting-agents-mode . :tutor-speaks)
-                     (:test-after-n-interactions . 10)
+                     (:test-after-n-interactions . 5000)
                      (:data-sets . ("valA"))))
                    )
-                 :number-of-interactions 20
+                 :number-of-interactions 10000
                  :number-of-series 1
                  :monitors (list "export-communicative-success"
-                                 "export-lexicon-size"
+                                 ;"export-lexicon-size"
                                  ;"export-features-per-form"
                                  ;"export-utterance-length"
                                  ))
 
 (create-graph-for-single-strategy
- :experiment-name "test"
+ :experiment-name "cogent-test"
  :measure-names '("communicative-success")
  :y-axis '(1)
  :y1-max 1
@@ -77,13 +77,14 @@
  :y1-label "Success")
 
 (create-graph-comparing-strategies
- :experiment-names '("cogent-exponential-train-10"
-                     "cogent-exponential-train-50"
-                     "cogent-exponential-train-100"
-                     "cogent-exponential-train-200"
-                     "cogent-exponential-train-500"
-                     "cogent-exponential-train-1000")
+ :experiment-names '("cogent-min-max-train-10"
+                     "cogent-min-max-train-50"
+                     "cogent-min-max-train-100"
+                     "cogent-min-max-train-200"
+                     "cogent-min-max-train-500"
+                     "cogent-min-max-train-1000"
+                     "cogent-min-max-train-2000")
  :measure-name "communicative-success"
  :y-min 0 :y-max 1 :xlabel "Number of games" :y1-label "Success"
- :captions '("train=10" "train=50" "train=100" "train=200" "train=500" "train=1000")
- :title "CLEVR CoGenT (exponential)" :end nil)
+ :captions '("train=10" "train=50" "train=100" "train=200" "train=500" "train=1000" "train=2000") ;
+ :title "CLEVR CoGenT (min-max)" :end nil)
