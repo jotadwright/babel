9d2fbe35c75e4ea95c137d1846ec43d7e837ba8e
jnevens <jnevens@ai.vub.ac.be>
2019-10-28 10:07:14 +0100

    [multidimensional-word-meanings] Added new alignment mechanism

--- experiments/multidimensional-word-meanings/alignment.lisp
+++ experiments/multidimensional-word-meanings/alignment.lisp
@@ -24,25 +24,49 @@
   (rewarded-attrs list)
   (punished-attrs list))
 
-(defun filter-uncertain-attributes (concept)
-  ;; optimization attempt 1
-  (remove-if #'(lambda (certainty) (< certainty 0.1))
-             concept :key #'cdr))
+(defun find-discriminating-attributes (agent concept topic)
+  "Find all attributes that are discriminating for the topic"
+  (let ((context (remove topic (objects (context agent)))))
+    (loop with discriminating-attributes = nil
+          for (category . certainty) in concept
+          for topic-similarity = (similarity topic category)
+          for best-other-similarity
+          = (loop for object in context
+                  maximize (similarity object category))
+          when (> topic-similarity best-other-similarity)
+          do (push (attribute category) discriminating-attributes)
+          finally
+          (return discriminating-attributes))))
+
+
+(defun filter-subsets (all-subsets discriminating-attributes)
+  "Filter all subsets with the discriminating attributes, only
+   keeping those subsets where all discriminating attributes
+   occur in"
+  (loop with applicable-subsets = nil
+        for subset in all-subsets
+        for subset-attributes
+        = (mapcar #'attribute (mapcar #'car subset))
+        when (null (set-difference discriminating-attributes subset-attributes))
+        do (push subset applicable-subsets)
+        finally
+        (return applicable-subsets)))
+        
                       
-(defun find-most-discriminating-subset (agent concept topic)
+(defun find-most-discriminating-subset (agent subsets topic)
   "Find the subset that maximizes the difference in similarity
    between the topic and the best other object"
-  (let ((subsets (all-subsets (filter-uncertain-attributes concept)))
-        (context (remove topic (objects (context agent))))
+  (let ((context (remove topic (objects (context agent))))
         best-subset largest-diff)
     (dolist (subset subsets)
       (let ((topic-similarity (weighted-similarity topic subset)))
-        (when (> topic-similarity 0) ;; optimization attempt 2
+        (when (> topic-similarity 0)
           (let* ((best-other-similarity
                   (loop for object in context
                         maximize (weighted-similarity object subset)))
                  (diff (- topic-similarity best-other-similarity)))
-            (when (or (null best-subset) (> diff largest-diff))
+            (when (or (null best-subset)
+                      (> diff largest-diff))
               (setf best-subset subset
                     largest-diff diff))))))
     best-subset))
@@ -51,29 +75,32 @@
 (defgeneric align-known-words (agent topic categories)
   (:documentation "Align known words"))
 
+;; find the set of all attributes that are discriminating (S)
+;; compute all subsets of all attributes (A)
+;; only consider the subsets of which the set-difference
+;; between S and A is empty
+
 (defmethod align-known-words ((agent mwm-agent) (topic mwm-object)
                               category-representation)
   (declare (ignorable category-representation))
   (let* ((cxn (first (applied-cxns agent)))
          (applied-concept (attr-val cxn :meaning)))
+    ;; add to cxn history
     (add-to-cxn-history agent cxn)
+    ;; update prototypes
     (loop for (category . certainty) in applied-concept
           do (update-category category topic
                               :success (communicated-successfully agent)
                               :interpreted-object (topic agent)))
-    (if (communicated-successfully agent)
+    ;; update certainty scores
+    (let* ((discriminating-attributes
+            (find-discriminating-attributes agent applied-concept topic))
+           (all-subsets (all-subsets applied-concept))
+           (subsets-to-consider
+            (filter-subsets all-subsets discriminating-attributes))
+           (best-subset
+            (find-most-discriminating-subset agent subsets-to-consider topic)))
       (loop with rewarded = nil
-            for (category . certainty) in applied-concept
-            when (> certainty 0.1)
-            do (progn (push (attribute category) rewarded)
-                 (adjust-certainty agent cxn (attribute category)
-                                   (get-configuration agent :certainty-incf)
-                                   :remove-on-lower-bound (get-configuration agent :remove-on-lower-bound)))
-            finally
-            (notify scores-updated cxn rewarded nil))
-      (loop with best-subset
-            = (find-most-discriminating-subset agent applied-concept topic)
-            with rewarded = nil
             with punished = nil
             for (category . certainty) in applied-concept
             if (member category best-subset :key #'car)
@@ -87,8 +114,7 @@
                                    (get-configuration agent :certainty-decf)
                                    :remove-on-lower-bound (get-configuration agent :remove-on-lower-bound)))
             finally
-            (notify scores-updated cxn rewarded punished)))))
-  
+            (notify scores-updated cxn rewarded punished))))) 
         
  
 ;;;; Align Agent        
