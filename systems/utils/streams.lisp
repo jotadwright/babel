cff876a6150c3f67c1740a42692e7a635e434e74
jnevens <jnevens@ai.vub.ac.be>
2020-02-04 16:35:40 +0100

    [corpus-processing/utils] bugfix in stream->list and corpus-processing for ccl

--- systems/utils/streams.lisp
+++ systems/utils/streams.lisp
@@ -5,13 +5,11 @@
 (defun stream->list (stream &key number-of-lines)
   "Returns a list of the next number-of-lines lines of the stream.
    If number-of-lines is unspecified, the entire stream is processed
-   untill an empty line is encountered." 
-  (let (lines)
-    (do ((line (read-line stream nil nil)
-               (read-line stream nil nil))
-         (n 1 (1+ n)))
-        ((if number-of-lines
-           (or (> n number-of-lines) (null line))
-           (null line))
-         (reverse lines))
-      (push line lines))))
+   untill an empty line is encountered."
+  (if number-of-lines
+    (loop repeat number-of-lines
+          for line = (read-line stream nil nil)
+          when line collect line)
+    (loop for line = (read-line stream nil nil)
+          while line
+          collect line)))
