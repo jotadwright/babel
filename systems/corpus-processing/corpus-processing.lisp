ef334efc3288562484af1276cde6a23213e95abe
jnevens <jnevens@ai.vub.ac.be>
2020-02-11 10:13:51 +0100

    [corpus-processing] Improved corpus processing with less I/O

--- systems/corpus-processing/corpus-processing.lisp
+++ systems/corpus-processing/corpus-processing.lisp
@@ -39,11 +39,13 @@
                              *max-nr-parallel-processes*)
                             (number-of-lines-per-process 2000)
                             (write-empty-lines-p nil)
-                            (inferior-lisp *inferior-lisp*))
+                            (inferior-lisp *inferior-lisp*)
+                            (silent-processes nil))
   "Applies function to every line in inputfile and writes the result in outputfile.
-   A higher number-of-threads results in a higher speed (if these threads are available).
-   A higher number-of-lines-per-thread results in a higher speed, but also in a higher
-   memory use."
+   A higher number-of-processes results in a higher speed (if these processes are available).
+   A higher number-of-lines-per-process results in a higher speed, but also in a higher
+   memory use. Every process writes to a temporary file using the name argument."
+  
    ;; Printing some information
   (format t "~%~%****************** Started Corpus Processing ******************")
   (format t "~%ASDF System: ~a" asdf-system)
@@ -82,7 +84,8 @@
           (format t "~%Starting complete batch ~a/~a..." (+ 1 n) number-of-complete-batches)
           (process-batch-multi-process asdf-system package function function-kwargs
                                        n batch-start number-of-processes number-of-lines-per-batch
-                                       inputfile tmpfile write-empty-lines-p inferior-lisp)
+                                       inputfile tmpfile write-empty-lines-p inferior-lisp
+                                       silent-processes)
           (incf batch-start number-of-lines-per-batch)
           (format t "~%Finished"))
         ;; process an optional extra batch
@@ -90,12 +93,12 @@
           (format t "~%Started extra batch...")
           (process-batch-multi-process asdf-system package function function-kwargs
                                        number-of-complete-batches batch-start number-of-processes number-of-lines-in-last-batch
-                                       inputfile tmpfile write-empty-lines-p inferior-lisp)
+                                       inputfile tmpfile write-empty-lines-p inferior-lisp silent-processes)
           (format t "~%Finished")))
 
-      ;; Create outputfile by combining the output of the separate processes
+      ;; Create outputfile by combining the output of the separate batches
       (format t "~%.. reading the temp files written by the client processes~%")
-      (write-output tmpfile outputfile number-of-complete-batches number-of-processes number-of-lines-in-last-batch))
+      (write-output tmpfile outputfile number-of-complete-batches number-of-lines-in-last-batch))
 
     ;; Finish
     (let ((finish-time (get-universal-time)))
@@ -103,51 +106,16 @@
           (seconds-to-hours-minutes-seconds (- finish-time start-time))
       (format t "~%~%Processing took ~a hours, ~a minutes and ~a seconds." h m s))))
   (format t "~%~%Wrote output to file ~a." outputfile)
-  (format t "~%~%***************** Finished Corpus Processing *****************~%~%"))
-
-
-
-(defun write-output (tmpfile outputfile number-of-complete-batches number-of-processes number-of-lines-in-last-batch)
-  "Reads the output of the separate processes and combines it into the requested output file"
-  (let ((complete-batch-temp-file-names
-         (loop for i from 0 below number-of-complete-batches
-               append (loop for j from 0 below number-of-processes
-                            collect (make-pathname :directory (pathname-directory tmpfile)
-                                                   :name (format nil "~a-batch-~a-process-~a"
-                                                                 (pathname-name tmpfile) i j)
-                                                   :type (pathname-type tmpfile)))))
-        (last-batch-temp-file-names
-         (when (> number-of-lines-in-last-batch 0)
-           (loop for j from 0 below number-of-processes
-                 for pathname
-                 = (make-pathname :directory (pathname-directory tmpfile)
-                                  :name (format nil "~a-batch-~a-process-~a"
-                                                (pathname-name tmpfile)
-                                                number-of-complete-batches j)
-                                  :type (pathname-type tmpfile))
-                 when (probe-file pathname)
-                 collect pathname))))
-    (with-open-file (out outputfile :direction :output
-                         :if-exists :supersede
-                         :if-does-not-exist :create)
-      (loop for file in (append complete-batch-temp-file-names
-                                last-batch-temp-file-names)
-            do (with-open-file (in file :direction :input)
-                 (loop for line = (read-line in nil nil)
-                       while line
-                       do (write-line line out))))
-      (finish-output out))))
+  (format t "~%~%***************** Finished Corpus Processing *****************~%~%")) 
     
-    
-
 
 
 (defun process-batch-multi-process (asdf-system package function function-kwargs
                                     batch-number batch-start number-of-processes number-of-lines
                                     inputfile tmpfile write-empty-lines-p
-                                    inferior-lisp)
+                                    inferior-lisp silent-processes)
   "takes a batch of lines, divides them over processes, applies function on each line and writes
-   the output to tmpfiles"
+   the output to a single temp file per batch."
   (let ((list-of-process-batches nil))
     ;; Divide lines over processes
     (multiple-value-bind (lines-per-process lines-last-process)
@@ -165,12 +133,11 @@
                list-of-process-batches))
 
     ;; Start the processes
-    (let* ((temp-file-names (loop for i from 0 below number-of-processes
-                                  collect (make-pathname :directory (pathname-directory tmpfile)
-                                                         :name (format nil "~a-batch-~a-process-~a"
-                                                                       (pathname-name tmpfile)
-                                                                       batch-number i)
-                                                         :type (pathname-type tmpfile))))
+    (let* ((temp-file-name (make-pathname :directory (pathname-directory tmpfile)
+                                          :name (format nil "~a-batch-~a"
+                                                        (pathname-name tmpfile)
+                                                        batch-number)
+                                          :type (pathname-type tmpfile)))
            (external-process-fn #+sbcl 'sb-ext:run-program
                                 #+ccl 'ccl:run-program
                                 #+lispworks 'system::run-shell-command)
@@ -181,6 +148,13 @@
                                                       :error-output :output
                                                       :wait nil :save-exit-status t))
            (continue-running t))
+
+      ;; clear the temp file
+      (with-open-file (stream temp-file-name :direction :output
+                              :if-exists :supersede
+                              :if-does-not-exist :create)
+        (declare (ignore stream)))
+    
       (loop while continue-running
             for processes = nil
             for input-streams = nil
@@ -192,8 +166,9 @@
                   for input-stream = nil
                   for output-stream = nil
                   do
-                  (format t "~%.. starting batch ~a - process ~a:" (1+ batch-number) (1+ j))
-                  (format t "~%  ~(~w~)~%" (cons external-process-fn inferior-lisp))
+                  (unless silent-processes
+                    (format t "~%.. starting batch ~a - process ~a:" (1+ batch-number) (1+ j))
+                    (format t "~%  ~(~w~)~%" (cons external-process-fn inferior-lisp)))
                   do
                   #+:sbcl (progn
                             (setf process (apply (symbol-function external-process-fn)
@@ -229,9 +204,8 @@
             
             ;; piping the commands
             (loop for output-stream in output-streams
-                  for file-name in temp-file-names
                   for (start . end) in list-of-process-batches
-                  for i from 1
+                  for process-number from 1
                   for commands = (list
                                   "(setf cl-user::*automatically-start-web-interface* nil)"
                                   "(setf test-framework::*dont-run-tests-when-loading-asdf-systems* t)"
@@ -245,18 +219,20 @@
                                    "\" :start " start " :end " end
                                    " :write-empty-lines-p " write-empty-lines-p
                                    " :input-file \"" inputfile
-                                   "\" :output-file \"" file-name 
-                                   "\""
+                                   "\" :output-file \"" temp-file-name 
+                                   "\" :process-number " process-number
+                                   " :silent " silent-processes
                                    ")")
                                   "(utils:quit-lisp)"
                                   "(sleep 0.1)" ;; make sure the status of the process can be changed
                                   )
                   do
                   ;; write commands to processes
-                  (format t "~%.. writing commands to process ~a:~%" i)
+                  (unless silent-processes
+                    (format t "~%.. writing commands to process ~a:~%" process-number))
                   (loop for c in commands
-                        do
-                        (format t "  ~a~%" c)
+                        do (unless silent-processes
+                             (format t "  ~a~%" c))
                         (write-string c output-stream)
                         (princ  #\lf output-stream))
                   (force-output t)
@@ -266,7 +242,8 @@
                   (close output-stream))
      
             ;; wait until the clients terminate and print the output
-            (format t "~%.. waiting for processes finish~%~%")
+            (unless silent-processes
+              (format t "~%.. waiting for processes finish~%~%"))
             (loop with running-processes = (copy-list processes)
                   with running-input-streams = (copy-list input-streams)
                   with running-process-numbers = (loop for i from 1 to (length running-processes)
@@ -280,9 +257,9 @@
                         for n in running-process-numbers
                         for char = nil
                         while (setf char (read-char-no-hang input-stream nil))
-                        do
-                        (unread-char char input-stream)
-                        (format t "~%process ~d: ~a" n (read-line input-stream)))
+                        do (unread-char char input-stream)
+                        (unless silent-processes
+                          (format t "~%process ~d: ~a" n (read-line input-stream))))
 
                   ;; check processes if still running
                   (loop for p in running-processes
@@ -305,8 +282,7 @@
                                             running-process-numbers))
                               (setf continue-running running-processes))))
             #+sbcl(loop for p in processes
-                     do (sb-ext:process-close p)))
-      (format t "~% Batch finished~%~%"))))
+                     do (sb-ext:process-close p))))))
       
 
   
@@ -314,7 +290,12 @@
 ;; this is the function that is run by the inferior/client lisp
 (defun run-client-process (&key asdf-system package function function-kwargs
                                 start end write-empty-lines-p
-                                input-file output-file)
+                                input-file output-file process-number
+                                (silent nil))
+  "this is the function that is run by the client lisp. It loads the
+   requested asdf-system and package, applies the function to the lines
+   between start and end and writes these to the output file. The process
+   number is added in front in order to sort the complete output later"
   ;; set random seed
   (setf *random-state* 
         #+sbcl (sb-ext:seed-random-state t)
@@ -322,49 +303,81 @@
         #+lispworks (make-random-state))
 
   ;; load the requested asdf system
-  (ql:quickload asdf-system :verbose nil)
+  (ql:quickload asdf-system :verbose nil :silent silent)
 
   ;; set the package
   (setf *package* (find-package (read-from-string package)))
 
   ;; do the actual processing
-  (let ((process-batch nil)
-        (fn (symbol-function (read-from-string function)))
+  (let ((fn (symbol-function (read-from-string function)))
         (kwargs (loop for elem in (read-from-string function-kwargs)
                       for i from 0
                       if (evenp i) collect (make-kw elem)
                       else collect elem))
+        (batch-input nil)
         (batch-output nil))
     ;; open the input-file and get the lines between start and end
     (with-open-file (stream input-file :direction :input)
-      (setf process-batch (read-lines-by-index stream start end)))
-    ;; apply the function to them
+      (setf batch-input (read-lines-by-index stream start end)))
+    ;; apply the function to them and cons the process-number to it
     (setf batch-output
           (mapcar #'(lambda (line)
                       (if kwargs
                         (apply fn line kwargs)
                         (funcall fn line)))
-                  process-batch))
+                  batch-input))
     ;; write the result to an output file    
-    (format t ".. writing recorded data to ~s.~%" output-file)
+    (unless silent
+      (format t ".. writing recorded data to ~s.~%" output-file))
     (force-output t)
-    (ensure-directories-exist output-file)
     (with-open-file (stream output-file :direction :output
-                            :if-exists :supersede
+                            :if-exists :append
                             :if-does-not-exist :create)
       (unless stream (error "could not open ~s" output-file))
       (if write-empty-lines-p
-        (mapcar #'(lambda (line) (format stream "~a~%" line)) batch-output)
-        (mapcar #'(lambda (line) (when line (format stream "~a~%" line))) batch-output))
+        (mapcar #'(lambda (line) (format stream "(~a . \"~a\")~%" process-number line)) batch-output)
+        (mapcar #'(lambda (line) (when line (format stream "(~a . \"~a\")~%" process-number line))) batch-output))
       (finish-output stream)))
       
   ;; finish
-  (format t ".. finished.~%")
+  (unless silent
+    (format t ".. finished.~%"))
   (force-output t)
   (sleep 0.2))
 
 
+
+(defun write-output (tmpfile outputfile number-of-complete-batches number-of-lines-in-last-batch)
+  "Reads the output of the separate batches and combines it into the requested output file.
+   Here, the content of the temp files is sorted."
+  (let ((complete-batch-temp-file-names
+         (loop for batch from 0 below number-of-complete-batches
+               collect (make-pathname :directory (pathname-directory tmpfile)
+                                      :name (format nil "~a-batch-~a"
+                                                    (pathname-name tmpfile) batch)
+                                      :type (pathname-type tmpfile))))
+        (last-batch-temp-file-names
+         (when (> number-of-lines-in-last-batch 0)
+           (list (make-pathname :directory (pathname-directory tmpfile)
+                                :name (format nil "~a-batch-~a"
+                                              (pathname-name tmpfile)
+                                              number-of-complete-batches)
+                                :type (pathname-type tmpfile))))))
+    (with-open-file (out outputfile :direction :output
+                         :if-exists :supersede
+                         :if-does-not-exist :create)
+      (loop for batch-file in (append complete-batch-temp-file-names
+                                      last-batch-temp-file-names)
+            for batch-lines = (mapcar #'read-from-string
+                                      (with-open-file (in batch-file :direction :input)
+                                        (stream->list in)))
+            do (loop for line in (sort batch-lines #'< :key #'car)
+                     do (write-line (cdr line) out)))
+      (finish-output out))))
+
+
 (defun read-lines-by-index (stream start end)
+  "Read the lines from position start to end from the stream"
   (loop for line = (read-line stream nil nil)
         for i from 0
         if (and (>= i start) (< i end) line) collect line into lines
