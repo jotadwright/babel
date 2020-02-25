49f605ba11992880bfc4765d9ec6e0a6f7899646
jnevens <jnevens@ai.vub.ac.be>
2020-02-25 15:02:09 +0100

    [clevr-evaluation] using hydra worker toolkit

---
+++ applications/clevr-evaluation/run-epilog.lisp
@@ -0,0 +1,48 @@
+
+(ql:quickload :clevr-evaluation)
+(in-package :clevr-evaluation)
+
+;; #######################################
+;; The epilog script will read the outputs
+;; of the separate jobs and combine it
+;; into a single output file
+;; ######################################
+
+(defun merge-batch-files (&key inputdir outputfile)
+  ;; check some stuff
+  (unless (probe-file inputdir)
+    (error "Could not find the directory ~a" inputdir))
+  ;; print some stuff
+  (format t "~%~%************************Running epilog************************")
+  (format t "~%Reading input from ~a" (namestring inputdir))
+  (format t "~%Writing output to ~a" (namestring outputfile))
+  ;; merge the batches
+  (ensure-directories-exist outputfile)
+  (with-open-file (out outputfile :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+    (let ((infiles (directory inputdir)))
+      (loop for infile in (sort infiles #'string< :key #'namestring)
+            do (with-open-file (in infile :direction :input)
+                 (loop for line = (read-line in nil nil)
+                       while line
+                       do (write-line line out)))))))
+        
+
+(defun args->plist (args)
+  (loop for arg in args
+        for i from 0
+        if (evenp i) collect (internal-symb (upcase arg))
+        else collect arg))
+
+
+(defun main (args)
+  (let ((arg-plist (args->plist args)))
+    (loop for indicator in '(inputdir outputfile)
+          unless (getf arg-plist indicator)
+          do (error "Missing command line argument: ~a" indicator))
+    (merge-batch-files :inputdir (parse-namestring (getf arg-plist 'inputdir))
+                       :outputfile (parse-namestring (getf arg-plist 'outputfile)))
+    (format t "~%Done!")))
+
+(main ccl:*unprocessed-command-line-arguments*)
---
+++ applications/clevr-evaluation/run-prolog.lisp
@@ -0,0 +1,91 @@
+
+(ql:quickload :clevr-evaluation)
+(in-package :clevr-evaluation)
+
+;; #######################################
+;; The prolog script will divide the total
+;; input file over several separate input
+;; files. Additionally, it creates a .csv
+;; file that can be used to create a job
+;; array.
+;; ######################################
+
+(defun split-into-batchfiles (&key inputfile batchdir datafile batchsize)
+  ;; check some stuff
+  (unless (probe-file inputfile)
+    (error "The file ~a could not be found." inputfile))
+  ;; print some stuff
+  (format t "~%~%************************Running prolog************************")
+  (format t "~%Reading input from ~a" (namestring inputfile))
+  (format t "~%Writing batch files to ~a" (namestring batchdir))
+  (format t "~%Writing data lines to ~a" (namestring datafile))
+  (format t "~%Using batchsize ~a" batchsize)
+  ;; determine the number of batches
+  (let ((number-of-lines (number-of-lines inputfile))
+        (batch-directory (pathname-directory batchdir))
+        (input-name (pathname-name inputfile))
+        (input-type (pathname-type inputfile))
+        (in (open inputfile :direction :input))
+        (data-lines nil))
+    (multiple-value-bind (number-of-complete-batches number-of-lines-in-last-batch)
+        (floor number-of-lines batchsize)
+      ;; write the complete batches
+      (dotimes (n number-of-complete-batches)
+        (let ((outputfile (make-pathname :directory batch-directory
+                                         :name (format nil "~a-batch-~a" input-name n)
+                                         :type input-type)))
+          (ensure-directories-exist outputfile)
+          (with-open-file (out outputfile :direction :output
+                               :if-exists :supersede
+                               :if-does-not-exist :create)
+            (loop repeat batchsize
+                  do (write-line (read-line in) out)))
+          (push (list n outputfile batchsize) data-lines)))
+      ;; do the final batch
+      (when (> number-of-lines-in-last-batch 0)
+        (let ((outputfile (make-pathname :directory batch-directory
+                                         :name (format nil "~a-~a" input-name
+                                                       number-of-complete-batches)
+                                         :type input-type)))
+          (ensure-directories-exist outputfile)
+          (with-open-file (out outputfile :direction :output
+                               :if-exists :supersede
+                               :if-does-not-exist :create)
+            (loop repeat number-of-lines-in-last-batch
+                  do (write-line (read-line in) out)))
+          (push (list number-of-complete-batches
+                      outputfile
+                      number-of-lines-in-last-batch)
+                data-lines))))
+    ;; close the input stream
+    (close in)
+    ;; write the data file
+    (ensure-directories-exist datafile)
+    (with-open-file (data datafile :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+      (loop for line in (reverse data-lines)
+            for list-of-strings = (mapcar #'mkstr line)
+            for string = (format nil "~{~a~^, ~}" list-of-strings)
+            do (write-line string data)))))
+            
+      
+(defun args->plist (args)
+  (loop for arg in args
+        for i from 0
+        if (evenp i) collect (internal-symb (upcase arg))
+        else collect arg))
+
+
+(defun main (args)
+  (let ((arg-plist (args->plist args)))
+    (loop for indicator in '(inputfile batchdir datafile batchsize)
+          unless (getf arg-plist indicator)
+          do (error "Missing command line argument: ~a" indicator))
+    (split-into-batchfiles :inputfile (parse-namestring (getf arg-plist 'inputfile))
+                           :batchdir (parse-namestring (getf arg-plist 'batchdir))
+                           :datafile (parse-namestring (getf arg-plist 'datafile))
+                           :batchsize (parse-integer (getf arg-plist 'batchsize)))
+    (format t "~%Done!")))
+
+(main ccl:*unprocessed-command-line-arguments*)
