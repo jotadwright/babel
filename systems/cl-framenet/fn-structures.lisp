aeef8ee3fafa864dd995c7356d12d2fb34e546a7
paulvaneecke <paul@ai.vub.ac.be>
2020-04-21 17:43:04 +0200

    [cl-framenet] started on classes for framenet structures

--- systems/cl-framenet/fn-structures.lisp
+++ systems/cl-framenet/fn-structures.lisp
@@ -6,3 +6,22 @@
 ;;                                                                         ;;                    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;
 
+(defclass framenet-data ()
+  ((frames 
+    :type list :initarg :frames 
+    :accessor frames
+    :initform nil 
+    :documentation "The frames of Framenet.")
+   (lexical-units 
+    :type list :initarg :lexical-units 
+    :accessor lexical-units
+    :initform nil 
+    :documentation "The lexical units of Framenet.")
+   (example-sentences 
+    :type list :initarg :example-sentences 
+    :accessor example-sentences
+    :initform nil 
+    :documentation "The annotated example sentences in Framenet."))
+  (:documentation "Holds all the Framenet data in the form of Lisp objects."))
+
+
