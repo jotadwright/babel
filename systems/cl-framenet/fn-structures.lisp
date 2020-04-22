f13c2f4feb09f81ee00eaae1133acf1bad4f05ff
paulvaneecke <paul@ai.vub.ac.be>
2020-04-22 13:11:42 +0200

    [cl-framenet] progress on cl-framenet

--- systems/cl-framenet/fn-structures.lisp
+++ systems/cl-framenet/fn-structures.lisp
@@ -8,12 +8,12 @@
 
 (defclass framenet-data ()
   ((frames 
-    :type list :initarg :frames 
+    :type hash-table :initarg :frames 
     :accessor frames
     :initform nil 
     :documentation "The frames of Framenet.")
    (lexical-units 
-    :type list :initarg :lexical-units 
+    :type hash-table :initarg :lexical-units 
     :accessor lexical-units
     :initform nil 
     :documentation "The lexical units of Framenet.")
@@ -25,3 +25,39 @@
   (:documentation "Holds all the Framenet data in the form of Lisp objects."))
 
 
+(defclass frame ()
+  ((name 
+    :type symbol :initarg :name 
+    :accessor name
+    :initform nil 
+    :documentation "The name of the frame.")
+   (frame-elements 
+    :type list :initarg :frame-elements 
+    :accessor frame-elements
+    :initform nil 
+    :documentation "The frame elements.")
+   (lexical-units 
+    :type list :initarg :lexical-units 
+    :accessor lexical-units
+    :initform nil 
+    :documentation "The names of de lexical units that evoke this frame."))
+  (:documentation "The representation of a Framenet frame."))
+
+
+(defclass frame-element ()
+  ((name 
+    :type symbol :initarg :name 
+    :accessor name
+    :initform nil 
+    :documentation "The name of the frame.")
+   (abbrev
+    :type symbol :initarg :abbrev 
+    :accessor abbrev
+    :initform nil 
+    :documentation "An abbreviation of the FE name as used in the annotations.")
+   (core-type 
+    :type list :initarg :core-type 
+    :accessor core-type
+    :initform nil 
+    :documentation "Type if frame-element."))
+  (:documentation "The representation of a Framenet frame-element."))
--- systems/cl-framenet/start.lisp
+++ systems/cl-framenet/start.lisp
@@ -11,3 +11,70 @@
 (ql:quickload :cl-framenet)
 (in-package :cl-framenet)
 
+(ql:quickload :s-xml)
+(ql:quickload :web-interface)
+
+(defun parse-frame )
+
+(defun read-frame-from-xml (frame-name)
+  (let ((opinion-frame-file (merge-pathnames (make-pathname :directory '(:relative "frame")
+                                                            :name (string-downcase frame-name :start 1)
+                                                            :type "xml")
+                                             *framenet-data-directory*)))
+    (with-open-file (inputstream opinion-frame-file :direction :input)
+      (xmls:parse inputstream))))
+
+
+;; (read-frame-from-xml 'opinion)
+
+
+;; Frames ;;
+;;;;;;;;;;;;
+
+(defun xml-frame-name (xml-frame)
+  "Returns the name of the frame as a symbol."
+  (make-symbol (string-upcase (xmls:xmlrep-attrib-value "name" xml-frame))))
+
+;; (xml-frame-name (read-frame-from-xml 'opinion))
+
+
+;; Frame Elements ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(defun make-frame-elements (xml-frame)
+  "Returns a list of frame-element object for the given frame."
+  (loop for FE in (xmls:xmlrep-find-child-tags "FE" xml-frame)
+        collect (make-instance 'frame-element
+                               :name (xml-frame-element-name FE)
+                               :abbrev (xml-frame-element-abbrev FE)
+                               :core-type (xml-frame-element-core-type FE))))
+
+;; (setf *A* (make-frame-elements (read-frame-from-xml 'opinion)))
+
+
+(defun xml-frame-element-name (xml-frame-element)
+  "Returns the name of the frame-element as a symbol."
+  (let ((value-as-string (xmls:xmlrep-attrib-value "name" xml-frame-element)))
+    (unless (equalp "" value-as-string)
+      (make-symbol (string-upcase value-as-string)))))
+
+;; (xml-frame-element-name (fourth (xml-frame-elements (read-frame-from-xml 'opinion))))
+
+
+(defun xml-frame-element-abbrev (xml-frame-element)
+  "Returns the name of the frame-element as a symbol."
+  (let ((value-as-string (xmls:xmlrep-attrib-value "abbrev" xml-frame-element)))
+    (unless (equalp "" value-as-string)
+      (make-symbol (string-upcase value-as-string)))))
+
+;; (xml-frame-element-abbrev (first (xml-frame-elements (read-frame-from-xml 'opinion))))
+
+
+(defun xml-frame-element-core-type (xml-frame-element)
+  "Returns the name of the frame-element as a symbol."
+  (let ((value-as-string  (xmls:xmlrep-attrib-value "coreType" xml-frame-element)))
+    (unless (equalp "" value-as-string)
+      (make-symbol (string-upcase value-as-string)))))
+
+;; (xml-frame-element-core-type (fourth (xml-frame-elements (read-frame-from-xml 'opinion))))
+
