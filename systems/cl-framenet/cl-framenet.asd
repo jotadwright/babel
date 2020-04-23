f2cf9cc7f967ba257d2ea3285d0a61734c39517d
paulvaneecke <paul@ai.vub.ac.be>
2020-04-23 11:34:54 +0200

    [cl-framenet] frame element complete

--- systems/cl-framenet/cl-framenet.asd
+++ systems/cl-framenet/cl-framenet.asd
@@ -5,12 +5,13 @@
   :maintainer "EHAI <ehai@ai.vub.ac.be>"
   :license "To be determined"
   :homepage "https://gitlab.ai.vub.ac.be/ehai/cl-framenet"
-  :depends-on (:cl-store :xmls)
+  :depends-on (:cl-store :xmls :cl-ppcre)
   :description "A Common Lisp interface to FrameNet."
   :serial t
   :components ((:file "package")
                (:file "utils")
                (:file "configuration")
                (:file "framenet-data")
+               (:file "frame")
                (:file "frame-element")
                (:file "load-fn-data")))
--- systems/cl-framenet/frame-element.lisp
+++ systems/cl-framenet/frame-element.lisp
@@ -22,10 +22,25 @@
     :initform nil 
     :documentation "An abbreviation of the frame element name as used in the annotations.")
    (core-type 
-    :type list :initarg :core-type 
+    :type symbol :initarg :core-type 
     :accessor core-type
     :initform nil 
-    :documentation "Type of frame-element."))
+    :documentation "Type of frame-element.")
+   (sem-types
+    :type list :initarg :sem-types 
+    :accessor sem-types
+    :initform nil 
+    :documentation "Possible sem-types of frame-element.")
+   (requires-frame-elements
+    :type list :initarg :requires-frame-elements 
+    :accessor requires-frame-elements
+    :initform nil 
+    :documentation "Required frame elements.")
+   (excludes-frame-elements
+    :type list :initarg :excludes-frame-elements 
+    :accessor excludes-frame-elements
+    :initform nil 
+    :documentation "Excluded frame elements."))
   (:documentation "The representation of a Framenet frame-element."))
 
 
@@ -38,32 +53,64 @@
         collect (make-instance 'frame-element
                                :name (xml-frame-element-name FE)
                                :abbrev (xml-frame-element-abbrev FE)
-                               :core-type (xml-frame-element-core-type FE))))
+                               :core-type (xml-frame-element-core-type FE)
+                               :sem-types (xml-frame-element-sem-types FE)
+                               :requires-frame-elements (xml-frame-element-requires FE)
+                               :excludes-frame-elements (xml-frame-element-excludes FE))))
 
-;; (setf *A* (make-frame-elements (read-frame-from-xml 'opinion)))
+;; (make-list-of-frame-elements  (read-frame-from-xml 'opinion))
 
 
 (defun xml-frame-element-name (xml-frame-element)
   "Returns the name of the frame-element as a symbol."
   (let ((value-as-string (xmls:xmlrep-attrib-value "name" xml-frame-element)))
     (unless (equalp "" value-as-string)
-      (make-symbol (string-upcase value-as-string)))))
+      (framenet-string->symbol value-as-string))))
 
-;; (xml-frame-element-name (fourth (xml-frame-elements (read-frame-from-xml 'opinion))))
+;; (mapcar #'xml-frame-element-name (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'opinion)))
 
 
 (defun xml-frame-element-abbrev (xml-frame-element)
   "Returns the name of the frame-element as a symbol."
   (let ((value-as-string (xmls:xmlrep-attrib-value "abbrev" xml-frame-element)))
     (unless (equalp "" value-as-string)
-      (make-symbol (string-upcase value-as-string)))))
+      (framenet-string->symbol value-as-string))))
+
+;; (mapcar #'xml-frame-element-abbrev (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'opinion)))
 
 
 (defun xml-frame-element-core-type (xml-frame-element)
   "Returns the name of the frame-element as a symbol."
   (let ((value-as-string  (xmls:xmlrep-attrib-value "coreType" xml-frame-element)))
     (unless (equalp "" value-as-string)
-      (make-symbol (string-upcase value-as-string)))))
+      (framenet-string->symbol value-as-string))))
 
-;; (xml-frame-element-core-type (fourth (xml-frame-elements (read-frame-from-xml 'opinion))))
+;; (mapcar #'xml-frame-element-core-type (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'opinion)))
+
+
+(defun xml-frame-element-sem-types (xml-frame-element)
+  "Returns a possibly empty list of SemTypes."
+  (let ((sem-types-xml  (xmls:xmlrep-find-child-tags "semType" xml-frame-element)))
+    (loop for el in sem-types-xml
+          collect (framenet-string->symbol (xmls:xmlrep-attrib-value "name" el)))))
+
+;; (mapcar #'xml-frame-element-sem-types (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'opinion)))
+
+
+(defun xml-frame-element-requires (xml-frame-element)
+  "Returns a possibly empty list of SemTypes."
+  (let ((requires-xml  (xmls:xmlrep-find-child-tags "requiresFE" xml-frame-element)))
+    (loop for el in requires-xml
+          collect (framenet-string->symbol (xmls:xmlrep-attrib-value "name" el)))))
+
+;; (mapcar #'xml-frame-element-requires  (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'apply_heat)))
+
+
+(defun xml-frame-element-excludes (xml-frame-element)
+  "Returns a possibly empty list of SemTypes."
+  (let ((excludes-xml  (xmls:xmlrep-find-child-tags "excludesFE" xml-frame-element)))
+    (loop for el in excludes-xml
+          collect (framenet-string->symbol (xmls:xmlrep-attrib-value "name" el)))))
+
+;; (mapcar #'xml-frame-element-excludes  (xmls:xmlrep-find-child-tags "FE" (read-frame-from-xml 'age)))
 
