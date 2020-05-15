312c7ca1084c79dcd02d7b2662073f8254be3606
paulvaneecke <paul@ai.vub.ac.be>
2020-05-15 18:08:23 +0200

    [cl-propbank] exporting more accessors

--- systems/cl-propbank/package.lisp
+++ systems/cl-propbank/package.lisp
@@ -2,7 +2,7 @@
 
 (defpackage :cl-propbank
   (:use :common-lisp :cl-user)
-  (:export *PB-DATA* LOAD-PB-DATA)
+  (:export *PB-DATA* LOAD-PB-DATA rolesets lemma aliases framenet id)
   (:documentation "A Common Lisp interface to PropBank"))
 
 (pushnew :cl-propbank *features*)
