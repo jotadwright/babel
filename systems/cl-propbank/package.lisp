c0f87a5ac6d2785ba77107ab72dc74dc041b6278
paulvaneecke <paul@ai.vub.ac.be>
2020-05-15 11:32:17 +0200

    [cl-propbank] small bugfix

--- systems/cl-propbank/package.lisp
+++ systems/cl-propbank/package.lisp
@@ -3,6 +3,6 @@
 (defpackage :cl-propbank
   (:use :common-lisp :cl-user)
   (:export *PB-DATA* LOAD-PB-DATA)
-  (:documentation "A Common Lisp interface to PropBank"))s
+  (:documentation "A Common Lisp interface to PropBank"))
 
 (pushnew :cl-propbank *features*)
