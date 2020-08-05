9cbc9e5c8e8e4cbe9a2fe6ef495fe8e545989e69
jnevens <jnevens@ai.vub.ac.be>
2020-08-05 15:56:16 +0200

    [clevr-world] minor

--- sharing/clevr-world/package.lisp
+++ sharing/clevr-world/package.lisp
@@ -8,7 +8,7 @@
         :irl-2)
   ;(:import-from :fcg :size :attributes) ;; needed to resolve name conflict
   (:shadowing-import-from :fcg :size)
-  (:shadowing-import-from :irl-2 :size)
+  ;(:shadowing-import-from :irl-2 :size)
   (:import-from :cl-json
                 :decode-json-from-string
                 :decode-json-from-source)
