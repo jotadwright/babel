17267639955c8a3fd51b8b3b02fe61da025f977e
jnevens <jnevens@ai.vub.ac.be>
2020-08-11 11:04:23 +0200

    [monitoring] web interface bugfix

--- systems/irl-2/composer/monitoring/web-monitors.lisp
+++ systems/irl-2/composer/monitoring/web-monitors.lisp
@@ -27,8 +27,11 @@
 ;; chunk-composer
 ;; ----------------------------------------------------------------------------
 
+(defparameter *composer-start-time* nil)
+
 (define-event-handler ((trace-irl trace-irl-verbose)
                        chunk-composer-get-next-solutions-started)
+  (setf *composer-start-time* (get-universal-time))
   (add-element '((hr)))
   (add-element '((h2) "Computing next composer solution"))
   (add-element `((p) ,(make-html composer)))
@@ -38,6 +41,7 @@
 
 (define-event-handler ((trace-irl trace-irl-verbose)
                        chunk-composer-get-all-solutions-started)
+  (setf *composer-start-time* (get-universal-time))
   (add-element '((hr)))
   (add-element '((h2) "Computing all composer solutions"))
   (add-element `((p) ,(make-html composer)))
@@ -46,6 +50,7 @@
 
 (define-event-handler ((trace-irl trace-irl-verbose)
                        chunk-composer-get-solutions-until-started)
+  (setf *composer-start-time* (get-universal-time))
   (add-element '((hr)))
   (add-element '((h2) "Computing all composer solutions until stop criterion"))
   (add-element `((p) ,(make-html composer)))
@@ -100,23 +105,28 @@
 
 (define-event-handler ((trace-irl trace-irl-verbose)
                        chunk-composer-finished)
-  (add-element '((hr)))
-  (add-element '((h2) "Result"))
-  (add-element
-   (make-html-for-composition-process composer))
-  (add-element '((h3) "Composer queue:"))
-  (add-element
-   (html-hide-rest-of-long-list
-    (queue composer) 3
-    #'(lambda (n)
-        (make-html n :draw-as-tree nil))))
-  (if solutions
-    (progn
-      (add-element `((h3) ,(format nil "Found ~a solutions:"
-                                   (length solutions))))
-      (composer-solutions->html solutions))
-    (add-element '((h3) "No solutions found.")))
-  (when (and (solutions composer)
-             (not (length= (solutions composer) solutions)))
-    (add-element '((h3) "All composer solutions so far"))
-    (composer-solutions->html (solutions composer))))
+  (let ((finish-time (get-universal-time)))
+    (multiple-value-bind (h m s)
+        (seconds-to-hours-minutes-seconds (- finish-time *composer-start-time*))
+      (add-element '((hr)))
+      (add-element '((h2) "Result"))
+      (add-element
+       (make-html-for-composition-process composer))
+      (add-element '((h3) "Composer queue:"))
+      (add-element
+       (html-hide-rest-of-long-list
+        (queue composer) 3
+        #'(lambda (n)
+            (make-html n :draw-as-tree nil))))
+      (add-element `((h3) ,(format nil "Composer took ~a hours, ~a minutes and ~a seconds"
+                                   h m s)))
+      (if solutions
+        (progn
+          (add-element `((h3) ,(format nil "Found ~a solutions:"
+                                       (length solutions))))
+          (composer-solutions->html solutions))
+        (add-element '((h3) "No solutions found.")))
+      (when (and (solutions composer)
+                 (not (length= (solutions composer) solutions)))
+        (add-element '((h3) "All composer solutions so far"))
+        (composer-solutions->html (solutions composer))))))
