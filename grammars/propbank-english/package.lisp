d6204802f5b4ab8815fdfb4768706c1683e6747d
paulvaneecke <paul@ai.vub.ac.be>
2020-09-11 17:31:41 +0200

    [propbank-english] solved import-from issue

--- grammars/propbank-english/package.lisp
+++ grammars/propbank-english/package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage :propbank-english
   (:documentation "A large propbank-based construction grammar for English")
-  (:shadowing-import-from :cl-propbank :id )
-  (:shadowing-import-from :fcg :size )
+  (:shadowing-import-from :cl-propbank :id)
+  (:shadowing-import-from :fcg :size)
   (:use :common-lisp :cl-user :utils :monitors :fcg :irl :web-interface :cl-propbank :cl-store))
