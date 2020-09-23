3dd803b4ed8f5ef65aa451c0e8df3f243519c05f
jnevens <jnevens@ai.vub.ac.be>
2020-09-23 10:13:59 +0200

    [experiment-framework] increased heap size when starting sbcl subprocess in run-parallel-batch

--- systems/experiment-framework/parallel-batch.lisp
+++ systems/experiment-framework/parallel-batch.lisp
@@ -46,7 +46,7 @@
 
 ;; the following two defparams are used for starting a particular 
 ;; client/inferior lisp 
-(defparameter *inferior-lisps* '((sbcl . ("sbcl" ("--noinform" "--disable-debugger")))
+(defparameter *inferior-lisps* '((sbcl . ("sbcl" ("--noinform" "--disable-debugger" "--dynamic-space-size" "4096")))
                                  (ccl . ("ccl" ("--batch" "--quiet")))
                                  (lx86cl . ("lx86cl64" ("--batch" "--quiet")))
                                  (lispworks . ("lispworks" ())))
