56e98860059ed9179e5ff1fd385bbfd46fef323f
jnevens <jnevens@ai.vub.ac.be>
2020-10-22 16:05:05 +0200

    [clevr-evaluation] minor changes

--- applications/clevr-evaluation/run-prolog.lisp
+++ applications/clevr-evaluation/run-prolog.lisp
@@ -20,55 +20,59 @@
   (format t "~%Writing batch files to ~a" (namestring batchdir))
   (format t "~%Writing data lines to ~a" (namestring datafile))
   (format t "~%Using batchsize ~a" batchsize)
-  ;; determine the number of batches
+  ;; read the number of lines and open the stream
   (let ((number-of-lines (number-of-lines inputfile))
-        (batch-directory (pathname-directory batchdir))
-        (input-name (pathname-name inputfile))
         (input-type (pathname-type inputfile))
-        (in (open inputfile :direction :input))
+        (in-stream (open inputfile :direction :input))
         (data-lines nil))
+    ;; determine the number of batches
     (multiple-value-bind (number-of-complete-batches number-of-lines-in-last-batch)
         (floor number-of-lines batchsize)
       ;; write the complete batches
       (dotimes (n number-of-complete-batches)
-        (let ((outputfile (make-pathname :directory batch-directory
-                                         :name (format nil "~a-batch-~a" input-name n)
-                                         :type input-type)))
+        (let ((outputfile
+               (merge-pathnames
+                (make-pathname :directory '(:relative)
+                               :name (format nil "batch-~a" n)
+                               :type input-type)
+                batchdir)))
           (ensure-directories-exist outputfile)
-          (with-open-file (out outputfile :direction :output
-                               :if-exists :supersede
-                               :if-does-not-exist :create)
+          (with-open-file (out-stream outputfile :direction :output
+                                      :if-exists :supersede
+                                      :if-does-not-exist :create)
             (loop repeat batchsize
-                  do (write-line (read-line in) out)))
+                  do (write-line (read-line in-stream) out-stream)))
           (push (list n outputfile batchsize) data-lines)))
       ;; do the final batch
       (when (> number-of-lines-in-last-batch 0)
-        (let ((outputfile (make-pathname :directory batch-directory
-                                         :name (format nil "~a-batch-~a" input-name
-                                                       number-of-complete-batches)
-                                         :type input-type)))
+        (let ((outputfile
+               (merge-pathnames
+                (make-pathname :directory '(:relative)
+                               :name (format nil "batch-~a" number-of-complete-batches)
+                               :type input-type)
+                batchdir)))
           (ensure-directories-exist outputfile)
-          (with-open-file (out outputfile :direction :output
-                               :if-exists :supersede
-                               :if-does-not-exist :create)
+          (with-open-file (out-stream outputfile :direction :output
+                                      :if-exists :supersede
+                                      :if-does-not-exist :create)
             (loop repeat number-of-lines-in-last-batch
-                  do (write-line (read-line in) out)))
+                  do (write-line (read-line in-stream) out-stream)))
           (push (list number-of-complete-batches
                       outputfile
                       number-of-lines-in-last-batch)
                 data-lines))))
     ;; close the input stream
-    (close in)
+    (close in-stream)
     ;; write the data file
     (ensure-directories-exist datafile)
-    (with-open-file (data datafile :direction :output
-                          :if-exists :supersede
-                          :if-does-not-exist :create)
-      (write-line "index, file, batchsize" data)
+    (with-open-file (data-stream datafile :direction :output
+                                 :if-exists :supersede
+                                 :if-does-not-exist :create)
+      (write-line "index, file, batchsize" data-stream)
       (loop for line in (reverse data-lines)
             for list-of-strings = (mapcar #'mkstr line)
             for string = (format nil "~{~a~^, ~}" list-of-strings)
-            do (write-line string data)))))
+            do (write-line string data-stream)))))
             
       
 (defun args->plist (args)
@@ -80,9 +84,9 @@
 
 (defun main (args)
   (let ((arg-plist (args->plist args)))
-    (loop for indicator in '(inputfile batchdir datafile batchsize)
-          unless (getf arg-plist indicator)
-          do (error "Missing command line argument: ~a" indicator))
+    (loop for arg in '(inputfile batchdir datafile batchsize)
+          unless (getf arg-plist arg)
+          do (error "Missing command line argument: ~a" arg))
     (split-into-batchfiles :inputfile (parse-namestring (getf arg-plist 'inputfile))
                            :batchdir (parse-namestring (getf arg-plist 'batchdir))
                            :datafile (parse-namestring (getf arg-plist 'datafile))
