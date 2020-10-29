e5368f2db67dbb87ac6a4c3dbf6ccfda5d008aa2
jnevens <jnevens@ai.vub.ac.be>
2020-10-29 12:07:07 +0100

    [clevr-evaluation] minor improvement to epilog script

--- applications/clevr-evaluation/run-epilog.lisp
+++ applications/clevr-evaluation/run-epilog.lisp
@@ -8,7 +8,9 @@
 ;; into a single output file
 ;; ######################################
 
-(defun merge-batch-files (inputdir outputfile &key (type :wild))
+(defun merge-batch-files (inputdir outputfile
+                                   &key (type :wild)
+                                   (skip-header t))
   ;; check that the inputdir exists
   (unless (probe-file inputdir)
     (error "Could not find the directory ~a" inputdir))
@@ -27,11 +29,16 @@
                   (make-pathname :directory (pathname-directory inputdir)
                                  :name :wild :type type))))
       (loop for file in files
+            for lines-read = 0
             do (format t "Reading from ~a~%" (namestring file))
             do (with-open-file (in-stream file :direction :input)
+                 (when skip-header
+                   (read-line in-stream nil nil))
                  (loop for line = (read-line in-stream nil nil)
                        while line
-                       do (write-line line out-stream)))))))
+                       do (write-line line out-stream)
+                       do (incf lines-read)))
+            do (format t "Read ~a lines~%" lines-read)))))
         
 
 (defun args->plist (args)
@@ -44,13 +51,24 @@
 (defun main (args)
   (let ((arg-plist (args->plist args)))
     ;; parse the command line arguments
-    (loop for indicator in '(inputdir outputfile type)
+    (loop for indicator in '(inputdir outputfile type skip-header)
           unless (getf arg-plist indicator)
           do (error "Missing command line argument: ~a" indicator))
     ;; merge the batch files
     (merge-batch-files (parse-namestring (getf arg-plist 'inputdir))
                        (parse-namestring (getf arg-plist 'outputfile))
-                       :type (getf arg-plist 'type))
+                       :type (getf arg-plist 'type)
+                       :skip-header (= (parse-integer (getf arg-plist 'skip-header)) 1))
     (format t "~%Done!")))
 
-(main ccl:*unprocessed-command-line-arguments*)
+#|
+How to call this script:
+ccl -l run-epilog -b
+    -- inputdir path/to/folder/with/csv/files/
+       outputfile path/to/file.csv
+       type csv
+       skip-header 1
+|#
+
+#-lispworks
+(main #+ccl ccl:*unprocessed-command-line-arguments*)
