4dcd1c8ecd61365f8bc3a8b925e9ce9a48fd574f
jnevens <jnevens@ai.vub.ac.be>
2020-12-01 16:08:32 +0100

    [coco-grammar] remove debugging function

--- grammars/coco-grammar/start.lisp
+++ grammars/coco-grammar/start.lisp
@@ -2,8 +2,6 @@
 (in-package :coco-grammar)
 (activate-monitor trace-fcg)
 
-(wi::add-element (make-html *COCO*))
-
 (comprehend "how many cows are there?")
 (comprehend "is there a sheep left of the cow?")
 (comprehend "how many sheep are right of the cow?")
@@ -13,20 +11,6 @@
 (comprehend "are there an equal number of cows and sheep?")
 (comprehend "are there more cows than sheep?")
 (comprehend "are there fewer cows than sheep?")
-
 (comprehend "is there a zebra left of the giraffe?")
 (comprehend "is there a sea below the sky?")
-
-(defun test ()
-  (multiple-value-bind (irl cipn)
-      (comprehend "is there a sea below the sky?")
-    (let* ((data (fcg::analyse-solution cipn '<-))
-           (s-dot (fcg::unit-bindings->graph :data data
-                                             :construction-inventory *COCO*
-                                             :prefered-font "Arial"))
-           (svg (wi::s-dot->svg s-dot)))
-      (add-element
-       `((div) ,svg)))))
-
-(test)
 
