a10da4d4ba76863801a04c1305517b94d147a536
jnevens <jnevens@ai.vub.ac.be>
2020-12-02 09:26:05 +0100

    [seq2seq-heuristics] seq2seq requests using streams

--- systems/seq2seq-heuristics/next-cxn.lisp
+++ systems/seq2seq-heuristics/next-cxn.lisp
@@ -11,25 +11,48 @@
 (defun seq2seq-next-cxn (utterance/meaning applied-cxns model endpoint
                                    &key (number-cutoff nil) (probability-cutoff nil))
   "Queries the seq2seq model for a probability distribution for the next cxn to apply."
-  (let* ((applied-cxns (if applied-cxns
-                         ;; the cxn names should not be transformed to camelcase
-                         (mapcar (compose #'mkstr #'name) applied-cxns)
-                         ;; if no applied cxns an empty vector will transform into [ ] (instead of '() into null)
-                         #()))
-         (json (cl-json:encode-json-alist-to-string `((:model . ,model)
-                                                      (:utterance/meaning . ,utterance/meaning)
-                                                      (:applied--cxns . ,applied-cxns)
-                                                      (:number--cutoff . ,number-cutoff)
-                                                      (:probability--cutoff . ,probability-cutoff))))
-         (response-string (drakma:http-request endpoint
-                                               :method :post
-                                               :content-type "application/json"
-                                               :content json
-                                               #+sbcl :connection-timeout nil
-                                               ))
-                          ;;#+ccl (dex:post endpoint :content json :headers '((Content-Type . "application/json"))))
-         (response-object (handler-case (cl-json:decode-json-from-string response-string)
-                            (error (c) (warn (format nil "Error decoding json in seq2seq-next-cxn."))))))
+  (let* ((applied-cxns
+          (if applied-cxns
+            ;; the cxn names should not be transformed to camelcase
+            (mapcar (compose #'mkstr #'name) applied-cxns)
+            ;; if no applied cxns an empty vector will transform into [ ] (instead of '() into null)
+            #()))
+         (json
+          (cl-json:encode-json-alist-to-string
+           `((:model . ,model)
+             (:utterance/meaning . ,utterance/meaning)
+             (:applied--cxns . ,applied-cxns)
+             (:number--cutoff . ,number-cutoff)
+             (:probability--cutoff . ,probability-cutoff))))
+         (response-string
+          (with-open-stream
+              (stream #+lispworks (drakma:http-request endpoint :method :post
+                                                       :content-type "application/json"
+                                                       :content json :want-stream t
+                                                       :connection-timeout nil
+                                                       :read-timeout nil
+                                                       :write-timeout nil)
+                      #+sbcl (drakma:http-request endpoint :method :post
+                                                  :content-type "application/json"
+                                                  :content json :want-stream t
+                                                  :connection-timeout nil)
+                      #+ccl (drakma:http-request endpoint :method :post
+                                                 :content-type "application/json"
+                                                 :content json :want-stream t
+                                                 :deadline (+ (get-universal-time) 1000000)))
+            (list-of-strings->string
+             (stream->list stream))))
+         ;(response-string (drakma:http-request endpoint
+         ;                                      :method :post
+         ;                                      :content-type "application/json"
+         ;                                      :content json
+         ;                                      :connection-timeout nil
+         ;                                      :read-timeout nil
+         ;                                      :write-timeout nil
+         ;                                      ))
+         (response-object
+          (handler-case (cl-json:decode-json-from-string response-string)
+            (error (c) (warn (format nil "Error decoding json in seq2seq-next-cxn."))))))
     (when response-object
       (assqv :probabilities response-object))))
     
