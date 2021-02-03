d692c17f679e5d4cfa1108a6ef1451c36639ba1a
jnevens <jnevens@ai.vub.ac.be>
2021-02-03 13:35:18 +0100

    [web-interface] added key 'render-program' to s-dot related functions

--- systems/web-interface/dot.lisp
+++ systems/web-interface/dot.lisp
@@ -48,7 +48,7 @@
 (defvar *graphviz-output-directory* 
   (babel-pathname :directory '(".tmp")))
 
-(defun s-dot->image (s-dot-expression &key path (format "png") (open nil))
+(defun s-dot->image (s-dot-expression &key path (format "png") (open nil) (render-program "dot"))
   "Renders s-dot-expression into dot syntax and then runs graphviz dot
    to create an image file. Returns the pathname of the generated
    graphic. When :open t, then it tries to open the resulting file."
@@ -58,39 +58,41 @@
                                      (make-pathname :name "" 
                                                     :type format))
                     (mkstr (make-id 'graph))))))
-    (ensure-directories-exist *graphviz-output-directory*)
-    ;; create the graphic file
-    (pipe-through 
-        ;; on windows the dot tool does not run if it can't write to stdout
-        ;; so we also have to open an input pipe
-        (input output "dot" (mkstr "-T" format) "-o"
-               #+(or :win32 :windows) (format nil "c:~a" path)
-               #-(or :win32 :windows) (mkstr path))
-      (s-dot:s-dot->dot input s-dot-expression)
-      (when input (close input))
-      (when output (close output)))
+    (if (which render-program)
+      (progn (ensure-directories-exist path)
+        ;; create the graphic file
+        (pipe-through 
+            ;; on windows the dot tool does not run if it can't write to stdout
+            ;; so we also have to open an input pipe
+            (input output render-program (mkstr "-T" format) "-o"
+                   #+(or :win32 :windows) (format nil "c:~a" path)
+                   #-(or :win32 :windows) (mkstr path))
+          (s-dot:s-dot->dot input s-dot-expression)
+          (when input (close input))
+          (when output (close output)))
     
-    ;; although we run dot with :wait t, on some machines it might
-    ;; happen that the resulting image is not accessible yet, so we
-    ;; wait. Although we also don't wait for more than 5 seconds
-    ;; because the file might, for some erroneous reason, never be
-    ;; written (e.g. dot is not found on the system).
-    (loop for i from 1 to 100
-       until (probe-file path)
-       do (sleep 0.05))
-    ;; try to open it
-    (when open 
-      (cond 
-        ((equal (software-type) "Darwin")
-         (run-prog "open" :args (list (format nil "~a" path))))
-        ((equal (software-type) "Linux")
-         (run-prog "see" :args (list (format nil "~a" path))))
-        ((equal (software-type) "Microsoft Windows")
-         (run-prog "cmd" 
-                   :args (list "/C"
-                               (string-replace 
-                                (format nil "c:~a" path) "/" "\\"))))))
-    path))
+        ;; although we run dot with :wait t, on some machines it might
+        ;; happen that the resulting image is not accessible yet, so we
+        ;; wait. Although we also don't wait for more than 5 seconds
+        ;; because the file might, for some erroneous reason, never be
+        ;; written (e.g. dot is not found on the system).
+        (loop for i from 1 to 100
+              until (probe-file path)
+              do (sleep 0.05))
+        ;; try to open it
+        (when open 
+          (cond 
+           ((equal (software-type) "Darwin")
+            (run-prog "open" :args (list (format nil "~a" path))))
+           ((equal (software-type) "Linux")
+            (run-prog "see" :args (list (format nil "~a" path))))
+           ((equal (software-type) "Microsoft Windows")
+            (run-prog "cmd" 
+                      :args (list "/C"
+                                  (string-replace 
+                                   (format nil "c:~a" path) "/" "\\"))))))
+        path)
+      (error "Unable to transform the s-dot expression to an image. Render program ~a is not found.~%" render-program))))
 
 
 
@@ -98,23 +100,25 @@
 ;; s-dot->svg
 ;; ----------------------------------------------------------------------------
 
-(defun s-dot->svg (s-dot-expression)
+(defun s-dot->svg (s-dot-expression &key (render-program "dot"))
   "Renders the s-dot expression to the dot format, then runs dot on it
    and returns the resulting svg xml expression."
-  (pipe-through (input output "dot" "-Tsvg")
-    (s-dot:s-dot->dot input s-dot-expression)
-    (force-output input)
-    (unless (eq input output) (close input))
-    (let ((lines nil) (line nil))
-      (loop do (setf line (read-line output nil))
-         ;; skip stuff before <svg  ...
-         until (and (> (length line) 3) (equal (subseq line 0 4) "<svg")))
-      (loop ;; skip comments
-         do (unless (and (> (length line) 3) (equal (subseq line 0 4) "<!--"))
-              (push line lines))
-           (setf line (if (equal line "</svg>")
-                          nil
-                          (read-line output nil)))
-         while line)
-      (when output (close output))
-      (reduce #'string-append (reverse lines)))))
+  (if (which render-program)
+    (pipe-through (input output render-program "-Tsvg")
+      (s-dot:s-dot->dot input s-dot-expression)
+      (force-output input)
+      (unless (eq input output) (close input))
+      (let ((lines nil) (line nil))
+        (loop do (setf line (read-line output nil))
+              ;; skip stuff before <svg  ...
+              until (and (> (length line) 3) (equal (subseq line 0 4) "<svg")))
+        (loop ;; skip comments
+              do (unless (and (> (length line) 3) (equal (subseq line 0 4) "<!--"))
+                   (push line lines))
+              (setf line (if (equal line "</svg>")
+                           nil
+                           (read-line output nil)))
+              while line)
+        (when output (close output))
+        (reduce #'string-append (reverse lines))))
+    (error "Unable to transform the s-dot expression to svg. Render program ~a is not found.~%" render-program)))
