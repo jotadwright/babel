743970db6d5a14409b2c29a1c6ec87554b9c0c1f
jnevens <jnevens@ai.vub.ac.be>
2021-02-25 13:12:04 +0100

    [clevr-world] import all cl-jonathan functions

--- sharing/clevr-world/package.lisp
+++ sharing/clevr-world/package.lisp
@@ -4,10 +4,6 @@
   (:use :common-lisp
         :utils
         #+:hunchentoot-available-on-this-platform :web-interface
-        :irl)
-  (:import-from :cl-jonathan
-   :decode-json-as-plist
-   :decode-json-as-alist
-   :decode-json-as-alist-from-source
-   :decode-json-as-plist-from-source)
+        :irl
+        :cl-jonathan)
   (:documentation "A package that provides an interface to the CLEVR dataset"))
