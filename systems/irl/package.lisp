05fc2b4055c40bf46afbd6953be82b9fd6696f25
jnevens <jnevens@ai.vub.ac.be>
2021-03-15 13:36:11 +0100

    [irl-2019] new irl was never made in a subrepo, fixing that now

--- systems/irl/package.lisp
+++
@@ -1,8 +0,0 @@
-
-(in-package :cl-user)
-
-(defpackage :irl
-  (:documentation "Incremental Recruitment Language (IRL)")
-  (:use :common-lisp :test-framework :utils :monitors
-        #+:hunchentoot-available-on-this-platform :web-interface)
-  (:shadow :context))
