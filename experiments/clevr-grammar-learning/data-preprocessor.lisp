49a91383c74266294cb85ea6dc6dafe3898c65b1
Jonas Doumen <jonas.doumen@kuleuven.be>
2021-06-02 15:08:42 +0200

    finished data preprocessor from csv to json

--- experiments/clevr-grammar-learning/data-preprocessor.lisp
+++ experiments/clevr-grammar-learning/data-preprocessor.lisp
@@ -1,12 +1,18 @@
 
-(defparameter *input-file*
-  (parse-namestring "/Users/jensnevens/Projects/CLEVR_val_enhanced.txt"))
-(defparameter *stage-1-dir*
-  (parse-namestring "/Users/jensnevens/Projects/clevr-learning-data/stage-1/"))
-(defparameter *stage-2-dir*
-  (parse-namestring "/Users/jensnevens/Projects/clevr-learning-data/stage-2/"))
-(defparameter *stage-3-dir*
-  (parse-namestring "/Users/jensnevens/Projects/clevr-learning-data/stage-3/"))
+(ql:quickload :cl-json)
+
+(defparameter *train-file*
+  (parse-namestring "/Users/u0077062/Box/CLEVR_data/CLEVR_train.csv"))
+
+(defparameter *validation-file*
+  (parse-namestring "/Users/u0077062/Box/CLEVR_data/CLEVR_val.csv"))
+
+(defparameter *stage-1-file*
+  (parse-namestring "/Users/u0077062/Projects/babel-corpora/clevr-grammar-learning/train/stage-1.txt"))
+(defparameter *stage-2-file*
+  (parse-namestring "/Users/u0077062/Projects/babel-corpora/clevr-grammar-learning/train/stage-2.txt"))
+(defparameter *stage-3-file*
+  (parse-namestring "/Users/u0077062/Projects/babel-corpora/clevr-grammar-learning/train/stage-3.txt"))
 
 
 (defparameter *stage-1-primitives*
@@ -19,48 +25,43 @@
            equal? exist filter get-context intersect
            query relate same union! unique))
 
+(defun main (data-path)
+  (with-open-file (s1 *stage-1-file* :direction :output
+                      :if-exists :supersede
+                      :if-does-not-exist :create)
+    (with-open-file (s2 *stage-2-file* :direction :output
+                        :if-exists :supersede
+                        :if-does-not-exist :create)
+      (with-open-file (s3 *stage-3-file* :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+        (with-open-file (stream data-path :direction :input)
+          (read-line stream nil nil) ;; skip header
+          (loop for line = (read-line stream nil nil)
+                for i from 1
+                while line
+                do (let* ((fields (split-sequence "," line))
+                          (question (second fields))
+                          (meaning-string (first (last fields)))
+                          (meaning (read-from-string meaning-string))
+                          (predicates (remove 'bind (mapcar #'car meaning)))
+                          (len (count #\space question))
+                          (json (cl-json:encode-json-to-string (list (cons "question" question)
+                                                                     (cons "meaning" meaning-string)
+                                                                     (cons "len" len)))))
+                          
+                          
+                     (cond ((loop for p in predicates
+                                  always (find p *stage-1-primitives*))
+                            ;; move to stage-1 file
+                            (write-line json s1))
+                                
+                           ((loop for p in predicates
+                                  always (find p *stage-2-primitives*))
+                            ;; move to stage-2 file
+                            (write-line json s2))
+                           (t
+                            ;; move to stage-3 file
+                            (write-line json s3))))))))))
 
-(defun main ()
-  (with-open-file (stream *input-file* :direction :input)
-    (loop for line = (read-line stream nil nil)
-          for i from 1
-          while line
-          do (let* ((parsed-line (cl-json:decode-json-from-string line))
-                    (question (rest (assoc :question parsed-line)))
-                    (meaning (read-from-string
-                              (rest (assoc :meaning parsed-line))))
-                    (predicates (remove 'bind (mapcar #'car meaning)))
-                    (len (count #\space question))
-                    (filename (format nil "question_~6,'0d_len_~3,'0d" i len)))
-               (cond ((loop for p in predicates
-                            always (find p *stage-1-primitives*))
-                      ;; move to stage-1 folder
-                      (let ((new-file
-                             (merge-pathnames
-                              (make-pathname :name filename :type "txt")
-                              *stage-1-dir*)))
-                      (ensure-directories-exist new-file)
-                      (with-open-file (out new-file)
-                        (write-line line out)
-                        (force-output out))))
-                     ((loop for p in predicates
-                            always (find p *stage-2-primitives*))
-                      ;; move to stage-2 folder
-                      (let ((new-file
-                             (merge-pathnames
-                              (make-pathname :name filename :type "txt")
-                              *stage-2-dir*)))
-                        (ensure-directories-exist new-file)
-                        (with-open-file (out new-file)
-                          (write-line line out)
-                          (force-output out))))
-                     (t
-                      ;; move to stage-3 folder
-                      (let ((new-file
-                             (merge-pathnames
-                              (make-pathname :name filename :type "txt")
-                              *stage-3-dir*)))
-                        (ensure-directories-exist new-file)
-                        (with-open-file (out new-file)
-                          (write-line line out)
-                          (force-output out)))))))))
+(main *train-file*)
