e1b187cef8e1ed2e9bf53900643a25a05c86e0d1
jnevens <jnevens@ai.vub.ac.be>
2021-06-07 14:41:34 +0200

    [clevr-learning] item-based competitors with exactly the same form now also punished

--- experiments/clevr-learning/grammar.lisp
+++ experiments/clevr-learning/grammar.lisp
@@ -151,6 +151,9 @@
                     :test #'string=)))
     competitors))
 
+(defun placeholderp (str)
+  (eql (char str 0) #\?))
+
 (defmethod meaning-competitors-for-cxn-type ((cxn construction)
                                              (cxn-inventory construction-inventory)
                                              (cxn-type (eql 'item-based))
@@ -167,8 +170,8 @@
         (possible-item-based-competitors
          (loop for other-cxn in (constructions-list cxn-inventory)
                when (and (eql (get-cxn-type other-cxn) 'item-based)
-                         (< (item-based-number-of-slots other-cxn)
-                            (item-based-number-of-slots cxn)))
+                         (<= (item-based-number-of-slots other-cxn)
+                             (item-based-number-of-slots cxn)))
                collect other-cxn))
         (item-based-competitors
          (loop for comp in possible-item-based-competitors
@@ -182,9 +185,9 @@
                                for comp-elem in comp-name-with-placeholders
                                for i from 0
                                always (or (string= cxn-elem comp-elem)
-                                          (and (string= (subseq cxn-elem 0 1) "?")
-                                               (string= (subseq comp-elem 0 1) "?"))
-                                          (and (string= (subseq cxn-elem 0 1) "?")
+                                          (and (placeholderp cxn-elem)
+                                               (placeholderp comp-elem))
+                                          (and (placeholderp cxn-elem)
                                                (string= comp-elem (nth i de-rendered-utterance))))))
                collect comp)) 
         (holophrase-competitors
@@ -194,7 +197,7 @@
                                   (list-of-strings->string
                                    (fcg::tokenize utterance))))
                collect other-cxn)))
-    (append holophrase-competitors item-based-competitors)))
+    (remove cxn (append holophrase-competitors item-based-competitors))))
 
 (defun get-meaning-competitors (agent applied-cxns utterance)
   "Get cxns with the same form as cxn"
