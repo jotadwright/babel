a20d44bb5b92e8782905463becd95a991fb57012
Paul Van Eecke <paul@ai86.vub.ac.be>
2021-11-30 11:47:32 +0100

    minor

--- grammars/propbank-grammar/start.lisp
+++ grammars/propbank-grammar/start.lisp
@@ -1,21 +1,76 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                                   ;;
-;; Learning and evaluating PropBank-based  grammars. ;;
+;; Learning and evaluating PropBank-based grammars.  ;;
 ;;                                                   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,;;;;
 
-;; Loading the :propbank-english system
-;;(ql:quickload :propbank-grammar)
+;; Loading the :propbank-grammar system
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ql:quickload :propbank-grammar)
 (in-package :propbank-grammar)
 
 
 ;; Activating spacy-api locally
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setf nlp-tools::*penelope-host* "http://127.0.0.1:5000")
 
-;; Loading the Propbank annotations (takes a couple of minutes)
-(load-propbank-annotations 'ewt :ignore-stored-data nil)
-(load-propbank-annotations 'ontonotes :ignore-stored-data nil)
-; *ewt-annotations*
-; *ontonotes-annotations*
 
- 
+;; Loading the Propbank annotations (takes a couple of minutes)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-propbank-annotations 'ewt :ignore-stored-data nil) ; *ewt-annotations*
+(load-propbank-annotations 'ontonotes :ignore-stored-data nil) ; *ontonotes-annotations*
+
+
+;; Storing and restoring grammars
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *restored-grammar*
+  (restore (babel-pathname :directory '("grammars" "propbank-grammar" "grammars")
+                           :name "propbank-grammar-ontonotes-ewt"
+                           :type "fcg")))
+
+(cl-store:store *propbank-ontonotes-ewt-learned-cxn-inventory*
+                (babel-pathname :directory '("grammars" "propbank-grammar" "grammars")
+                                :name "propbank-grammar-ontonotes-ewt"
+                                :type "fcg"))
+
+
+;; Learning grammars from the annotated data
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *training-configuration*
+  '((:de-render-mode .  :de-render-constituents-dependents)
+    (:node-tests :check-double-role-assignment)
+    (:parse-goal-tests :no-valid-children)
+    
+    (:max-nr-of-nodes . 100)
+    (:node-expansion-mode . :multiple-cxns)
+    (:priority-mode . :nr-of-applied-cxns)
+    (:queue-mode . :greedy-best-first)
+    (:hash-mode . :hash-lemma)
+    (:parse-order
+     lexical-cxn
+     argument-structure-cxn
+     argm-phrase-cxn
+     argm-leaf-cxn
+     word-sense-cxn)
+    (:replace-when-equivalent . nil)
+    (:learning-modes
+     :core-roles
+     ;:argm-pp
+     ;:argm-sbar
+     ;:argm-leaf
+     ;:argm-phrase-with-string
+     )
+    (:cxn-supplier-mode . :propbank-english)))
+
+
+
+(learn-propbank-grammar
+ (append (train-split *ontonotes-annotations*) (train-split *ewt-annotations*))
+ :selected-rolesets nil
+ :cxn-inventory '*propbank-ewt-learned-cxn-inventory*
+ :fcg-configuration *training-configuration*)
