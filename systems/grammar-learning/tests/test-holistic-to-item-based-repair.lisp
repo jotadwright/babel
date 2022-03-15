0b43a761565447fb31c3ce99edf58c31ec81937d
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-03-15 13:25:26 +0100

    adding extra testcase

--- systems/grammar-learning/tests/test-holistic-to-item-based-repair.lisp
+++ systems/grammar-learning/tests/test-holistic-to-item-based-repair.lisp
@@ -174,76 +174,10 @@
                                        (count! ?count-6 ?target-9843)
                                        (less-than ?target-74 ?count-5 ?count-6))))))))
 
-(defun run-holistic-to-item-based-tests ()
-  (test-holistic-to-item-based-from-substitution-comprehension)
-  (test-holistic-to-item-based-from-double-substitution-comprehension)
-  (test-multiple-holistic-to-item-based-repair-comprehension)
-  (test-holistic-to-item-based-duplicates-comprehension)
-  )
-
-#|
-
-(defun test-multiple-holistic-to-item-based-repair-comprehension ()
-  (defparameter *experiment* (set-up-cxn-inventory-and-repairs))
-   (let* (
-         (cxn-inventory (grammar (first (agents *experiment*)))))
-     (enable-learning cxn-inventory)
-    
-  (comprehend "The gray object is what shape?"
-              :cxn-inventory cxn-inventory
-              :gold-standard-meaning '((get-context ?source-1)
-                                       (filter ?target-2 ?target-1 ?color-2)
-                                       (unique ?source-9 ?target-2)
-                                       (bind shape-category ?shape-8 thing)
-                                       (bind attribute-category ?attribute-2 shape)
-                                       (filter ?target-1 ?source-1 ?shape-8)
-                                       (bind color-category ?color-2 gray)
-                                       (query ?target-7 ?source-9 ?attribute-2)))
-    
-  (comprehend "The large gray object is what shape?"
-              :cxn-inventory cxn-inventory
-              :gold-standard-meaning '((get-context ?source-1)
-                                       (filter ?target-39552 ?target-2 ?size-4)
-                                       (unique ?source-10 ?target-39552)
-                                       (bind color-category ?color-2 gray)
-                                       (filter ?target-1 ?source-1 ?shape-8)
-                                       (bind attribute-category ?attribute-2 shape)
-                                       (bind shape-category ?shape-8 thing)
-                                       (filter ?target-2 ?target-1 ?color-2)
-                                       (bind size-category ?size-4 large)
-                                       (query ?target-8 ?source-10 ?attribute-2)))
-
-  (comprehend "The yellow object is what shape?"
-              :cxn-inventory cxn-inventory
-              :gold-standard-meaning '((get-context ?source-1)
-                                       (filter ?target-2 ?target-1 ?color-2)
-                                       (unique ?source-9 ?target-2)
-                                       (bind shape-category ?shape-8 thing)
-                                       (bind attribute-category ?attribute-2 shape)
-                                       (filter ?target-1 ?source-1 ?shape-8)
-                                       (bind color-category ?color-2 yellow)
-                                       (query ?target-7 ?source-9 ?attribute-2)))
-
-  (comprehend "What is the shape of the large gray thing?"
-              :cxn-inventory cxn-inventory
-              :gold-standard-meaning '((get-context ?source-1)
-                                       (filter ?target-67855 ?target-2 ?size-4)
-                                       (unique ?target-object-1 ?target-67855)
-                                       (bind color-category ?color-2 gray)
-                                       (filter ?target-1 ?source-1 ?shape-8)
-                                       (bind attribute-category ?attribute-2 shape)
-                                       (bind shape-category ?shape-8 thing)
-                                       (filter ?target-2 ?target-1 ?color-2)
-                                       (bind size-category ?size-4 large)
-                                       (query ?target-4 ?target-object-1 ?attribute-2)))))
-
-(defun test-double-holistic-to-item-based-from-substitution-repair-comprehension ()
-  (defparameter *experiment* (set-up-cxn-inventory-and-repairs))
-   (let* (
-         (cxn-inventory (grammar (first (agents *experiment*)))))
-     (enable-learning cxn-inventory)
-    
-  (comprehend "The tiny gray object is what shape?"
+(deftest test-double-holistic-to-item-based-from-substitution-repair-comprehension ()
+  (let* ((experiment (set-up-cxn-inventory-and-repairs))
+         (cxn-inventory (grammar (first (agents experiment)))))
+    (comprehend "The tiny gray object is what shape?"
               :cxn-inventory cxn-inventory
               :gold-standard-meaning '((get-context ?source-1)
                                        (filter ?target-39552 ?target-2 ?size-4)
@@ -255,8 +189,7 @@
                                        (filter ?target-2 ?target-1 ?color-2)
                                        (bind size-category ?size-4 small)
                                        (query ?target-8 ?source-10 ?attribute-2)))
-    
-  (comprehend "The large yellow object is what shape?"
+    (comprehend "The large yellow object is what shape?"
               :cxn-inventory cxn-inventory
               :gold-standard-meaning '((get-context ?source-1)
                                        (filter ?target-39552 ?target-2 ?size-4)
@@ -268,8 +201,9 @@
                                        (filter ?target-2 ?target-1 ?color-2)
                                        (bind size-category ?size-4 large)
                                        (query ?target-8 ?source-10 ?attribute-2)))
-
-  (comprehend "What is the shape of the large yellow object?"
+    (test-repair-status 'repair-holistic->item-based-cxn
+                        (second (multiple-value-list
+                                 (comprehend "What is the shape of the large yellow object?"
               :cxn-inventory cxn-inventory
               :gold-standard-meaning '((get-context ?source-1)
                                        (filter ?target-56342 ?target-2 ?size-4)
@@ -280,5 +214,13 @@
                                        (bind shape-category ?shape-8 thing)
                                        (filter ?target-2 ?target-1 ?color-16)
                                        (bind size-category ?size-4 large)
-                                       (query ?target-4 ?target-object-1 ?attribute-2)))))
- |#
+                                       (query ?target-4 ?target-object-1 ?attribute-2))))))))
+
+(defun run-holistic-to-item-based-tests ()
+  (test-holistic-to-item-based-from-substitution-comprehension)
+  (test-holistic-to-item-based-from-double-substitution-comprehension)
+  (test-multiple-holistic-to-item-based-repair-comprehension)
+  (test-holistic-to-item-based-duplicates-comprehension)
+  (test-double-holistic-to-item-based-from-substitution-repair-comprehension)
+  )
+
