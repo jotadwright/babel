a91c4f56a6e258a3472a089acfe1fca98c420b16
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-06-13 16:03:14 +0200

    only search routine cxns in inventory to avoid duplicate work

--- systems/grammar-learning/utils/utils.lisp
+++ systems/grammar-learning/utils/utils.lisp
@@ -775,7 +775,9 @@
 
 (defun select-cxn-for-making-item-based-cxn (cxn-inventory utterance-form-constraints meaning meaning-representation-formalism)
   (loop for cxn in (sort (constructions cxn-inventory) #'> :key #'(lambda (x) (attr-val x :score)))
-        do (when (eql (attr-val cxn :cxn-type) 'holistic)
+        do (when (and (eql (attr-val cxn :cxn-type) 'holistic)
+                      (eql (attr-val cxn :label) 'fcg::routine))
+             
              (let* ((non-overlapping-meanings (multiple-value-list (diff-meaning-networks meaning (extract-meaning-predicates cxn) meaning-representation-formalism)))
                     (non-overlapping-meaning-observation (first non-overlapping-meanings))
                     (non-overlapping-meaning-cxn (second non-overlapping-meanings))
