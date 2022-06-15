64c37476a3eec1b5747358db24a5072fad9c0e76
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-06-15 20:04:39 +0200

    avoided double punishment in alignment

--- systems/grammar-learning/experiment-setup/alignment.lisp
+++ systems/grammar-learning/experiment-setup/alignment.lisp
@@ -49,41 +49,44 @@
                       :silent nil
                       :gold-standard-meaning (list (meaning agent))
                       :cxn-inventory (grammar agent))
-    (loop for solution-node in solution-cipns
-          for competitor-cxns = (set-difference (applied-constructions solution-node) (applied-constructions cipn) :key #'name)
-          do (loop for proc-bad-cxn in competitor-cxns
-                   for bad-cxn = (original-cxn proc-bad-cxn)
-                   do (dec-cxn-score agent bad-cxn :delta (get-configuration agent :cxn-decf-score))
-                   (dec-cxn-score agent (alter-ego-cxn bad-cxn (grammar agent)) :delta (get-configuration agent :cxn-decf-score))
-                   append (list bad-cxn (alter-ego-cxn bad-cxn (grammar agent))) into punished-cxns
-                   finally (notify cxns-punished punished-cxns)))
-    )
-  (set-configuration (grammar agent) :use-meta-layer t)
+    (let ((comprehended-punished-cxns (loop for solution-node in solution-cipns
+                               for competitor-cxns = (set-difference (applied-constructions solution-node) (applied-constructions cipn) :key #'name)
+                               do (loop for proc-bad-cxn in competitor-cxns
+                                        for bad-cxn = (original-cxn proc-bad-cxn)
+                                        do (dec-cxn-score agent bad-cxn :delta (get-configuration agent :cxn-decf-score))
+                                        (dec-cxn-score agent (alter-ego-cxn bad-cxn (grammar agent)) :delta (get-configuration agent :cxn-decf-score))
+                                        append (list bad-cxn (alter-ego-cxn bad-cxn (grammar agent))) into punished-cxns
+                                        finally (notify cxns-punished punished-cxns)
+                                        (return punished-cxns)))))
+      (loop for current-node in (traverse-depth-first (top-node (cip cipn)) :collect-fn #'identity)
+              when (and (field? (goal-test-data current-node) :result-goal-test-non-gold-standard-meaning)
+                        (not (get-data (goal-test-data current-node) :result-goal-test-non-gold-standard-meaning)))
+              do (loop for bad-node in (set-difference (remove (top-node (cip cipn))
+                                                               (cons current-node (all-parents current-node)))
+                                                       (remove (top-node (cip cipn))
+                                                               (cons cipn (all-parents cipn)))
+                                                       :key #'(lambda (node) (name (car-applied-cxn (cipn-car node)))))
+                       for bad-cxn = (original-cxn (car-applied-cxn (cipn-car bad-node)))
+                       unless (member bad-cxn comprehended-punished-cxns :key #'name)
+                       do (dec-cxn-score agent bad-cxn :delta (get-configuration agent :cxn-decf-score))
+                       (dec-cxn-score agent (alter-ego-cxn bad-cxn (grammar agent)) :delta (get-configuration agent :cxn-decf-score))
+                       append (list bad-cxn (alter-ego-cxn bad-cxn (grammar agent))) into punished-cxns
+                       finally (notify cxns-punished punished-cxns)))
+    
+      (set-configuration (grammar agent) :use-meta-layer t)
 
-  ;; align cxns
-  (let ((applied-cxns (original-applied-constructions cipn)))
+      ;; align cxns
+      (let ((applied-cxns (original-applied-constructions cipn)))
     
-    ;; reward the applied cxns 
-    (loop with cxn-delta = (get-configuration agent :cxn-incf-score)
-          for cxn in applied-cxns
-          for alter-ego-cxn = (alter-ego-cxn cxn (grammar agent))
-          do (inc-cxn-score cxn :delta cxn-delta)
-          (when alter-ego-cxn
-            (inc-cxn-score alter-ego-cxn :delta cxn-delta))
-          finally (notify cxns-rewarded applied-cxns))
+        ;; reward the applied cxns 
+        (loop with cxn-delta = (get-configuration agent :cxn-incf-score)
+              for cxn in applied-cxns
+              for alter-ego-cxn = (alter-ego-cxn cxn (grammar agent))
+              do (inc-cxn-score cxn :delta cxn-delta)
+              (when alter-ego-cxn
+                (inc-cxn-score alter-ego-cxn :delta cxn-delta))
+              finally (notify cxns-rewarded applied-cxns))
     
-     (loop for current-node in (traverse-depth-first (top-node (cip cipn)) :collect-fn #'identity)
-                 when (and (field? (goal-test-data current-node) :result-goal-test-non-gold-standard-meaning)
-                         (not (get-data (goal-test-data current-node) :result-goal-test-non-gold-standard-meaning)))
-                 do (loop for bad-node in (set-difference (remove (top-node (cip cipn))
-                                                                  (cons current-node (all-parents current-node)))
-                                                          (remove (top-node (cip cipn))
-                                                                  (cons cipn (all-parents cipn)))
-                                                          :key #'(lambda (node) (name (car-applied-cxn (cipn-car node)))))
-                          for bad-cxn = (original-cxn (car-applied-cxn (cipn-car bad-node)))
-                          do (dec-cxn-score agent bad-cxn :delta (get-configuration agent :cxn-decf-score))
-                          (dec-cxn-score agent (alter-ego-cxn bad-cxn (grammar agent)) :delta (get-configuration agent :cxn-decf-score))
-                          append (list bad-cxn (alter-ego-cxn bad-cxn (grammar agent))) into punished-cxns
-                          finally (notify cxns-punished punished-cxns)))))
+        ))))
 
       
