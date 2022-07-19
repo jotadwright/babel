55fbb8b27a4bac23dce73ab7c13a38c514ec118d
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-07-20 01:02:47 +0200

    updated geoquery preprocessor

--- experiments/grammar-learning/geoquery/geoquery-data-preprocessor.lisp
+++ experiments/grammar-learning/geoquery/geoquery-data-preprocessor.lisp
@@ -66,6 +66,7 @@
         with vars = (get-all-vars geo-prolog-string)
         with second-city-id-el = nil
         with city-id-found = nil
+        with geo-prolog-string = (replace-all geo-prolog-string "not((" "not_")
         for raw-el in (remove "" (cl-ppcre::split "[,\(\)]" geo-prolog-string) :test #'equal)
         for el = (replace-all (string-trim " " (replace-all raw-el "'" "")) " " "-")
         do (cond (;; we know the const term, skolemnise the city/country name as a new predicate
@@ -130,19 +131,78 @@
         finally (return (append result (list curr-predicate)))))
         
 
-(numberp (read-from-string "0"))
-;(parse-geoquery "/Users/u0077062/Projects/babel-corpora/geoquery/geoquery.xml")
+;(parse-geoquery "/Users/jonas/Downloads/wasp-2.0alpha/data/geo-funql/corpus.xml")
+
+
 (defun get-next-var-from-var-list (var-list)
   (get-next-var (first (sort var-list #'> :key #'(lambda (x) (position (symbol-name x) gl::+placeholder-vars+ :test #'equal))))))
 
 (defun get-next-var (last-var)
   (intern (nth (+ 1 (position (symbol-name last-var) gl::+placeholder-vars+ :test #'equal)) gl::+placeholder-vars+)))
 
+
+;; How many rivers do not traverse the state with the capital Albany ?
+;(geo-prolog-to-predicates "answer(A,count(B,(river(B),not((traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_))))),A))")
+(skolemnize-embedded-predicates
+ (geo-prolog-to-polish-notation "answer(A,count(B,(river(B),not((traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_))))),A))"))
+
+(serialize-embedded-predicates
+ (geo-prolog-to-polish-notation "answer(A,(size(B,A),const(B,cityid('new york',_))))"))
+
+(defun geo-prolog-to-polish-notation (geo-prolog-string)
+  (let* ((geo-prolog-string (cl-ppcre:regex-replace-all "([a-z]+)[\(]" geo-prolog-string "\(\\1 "))
+         (geo-prolog-string (cl-ppcre:regex-replace-all "([A-Z])" geo-prolog-string "?\\1"))
+         (geo-prolog-string (cl-ppcre:regex-replace-all "'([a-z]+) ([a-z]+)'" geo-prolog-string "\\1_\\2"))
+         (geo-prolog-string (replace-all geo-prolog-string "," " "))
+         (geo-prolog-string (string-append "("geo-prolog-string ")")))
+    (read-from-string geo-prolog-string)))
+
+(defun serialize-embedded-predicates (embedded-predicates)
+    (remove nil (loop with emb-preds = embedded-predicates
+          while emb-preds
+          append (loop 
+                   for predicate in emb-preds
+                   for term = (first predicate)
+                   for args = (rest predicate)
+                   for vars = (loop for el in args
+                                    when (variable-p el)
+                                    collect el)
+                   for cand-emb-preds = (loop for el in args
+                                              unless (variable-p el)
+                                              collect el)
+                   for accum-val = (append (list term) vars)
+                   do (cond ((equal (type-of term) 'cons)
+                             (setf accum-val nil)
+                             (setf emb-preds (append (list term) cand-emb-preds)))
+                            ((string= term 'CONST)
+                             (setf accum-val predicate))
+                            (t
+                             (setf emb-preds cand-emb-preds)))
+                   collect accum-val))))
+  
+          
+          
+  
+
+ 
 ;(geo-prolog-to-predicates "answer(A,(size(B,A),const(B,cityid('new york',_))))")
 
 ;(geo-prolog-to-predicates "answer(A,(population(B,A),const(B,cityid(springfield,_))))")
 
 #|
+(answer ?a)
+(count ?b ?a)
+(river ?b)
+(traverse ?b ?c)
+(state ?c)
+(loc ?d ?c)
+(capital ?d)
+(cityid ?d ?e ?f)
+(albany ?e)
+(_ ?f)
+
+
+ 
 (answer ?a)
 (river ?a)
 (loc ?a ?b)
