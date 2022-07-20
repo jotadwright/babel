ac0a8135d7482435b42bf2acc06d1ca402c94cec
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-07-20 21:35:10 +0200

    updated geoquery preprocessor

--- experiments/grammar-learning/geoquery/geoquery-data-preprocessor.lisp
+++ experiments/grammar-learning/geoquery/geoquery-data-preprocessor.lisp
@@ -31,10 +31,8 @@
           for pred-mr = (geo-prolog-to-predicates mr)
           do (loop for lang-entry in nl
                    for lang = (second (first (second lang-entry)))
-                   for len = (length pred-mr)
                    for value = (cl-json:encode-json-to-string (list (cons "utterance" (third lang-entry))
                                                                              (cons "meaning" (string-replace (format nil "~S" pred-mr) "GL-DATA::" ""))
-                                                                             (cons "len" len)
                                                                              (cons "id" id)
                                                                              (cons "lang" lang)))
                    do (push value (gethash lang lang-hash nil)))
@@ -58,77 +56,52 @@
 
                    
 (defun geo-prolog-to-predicates (geo-prolog-string)
-  (loop with curr-predicate = nil
-        with result = nil
-        with const-found = nil
-        with const-term = nil
-        with const-var = nil
-        with vars = (get-all-vars geo-prolog-string)
-        with second-city-id-el = nil
-        with city-id-found = nil
-        with geo-prolog-string = (replace-all geo-prolog-string "not((" "not_")
-        for raw-el in (remove "" (cl-ppcre::split "[,\(\)]" geo-prolog-string) :test #'equal)
-        for el = (replace-all (string-trim " " (replace-all raw-el "'" "")) " " "-")
-        do (cond (;; we know the const term, skolemnise the city/country name as a new predicate
-                  ;; (stateid ?b ?c)(arkansas ?c)
-                  (and const-term (not second-city-id-el))
-                  (if (equal const-term "cityid")
-                    (progn
-                      (setf city-id-found t)
-                      (setf result (append result (list (list (intern (string-upcase const-term) :GL-DATA)
-                                                            const-var
-                                                            (get-next-var-from-var-list vars)
-                                                            (get-next-var (get-next-var-from-var-list vars))
-                                                            )
-                                                        (list (intern (string-upcase el) :GL-DATA)
-                                                              (get-next-var-from-var-list vars))
-                                                             ))) 
-                      )
-                    (progn (setf result (append result (list (list (intern (string-upcase const-term) :GL-DATA)
-                                                            const-var
-                                                            (get-next-var-from-var-list vars)
-                                                            ))))
-                      (setf curr-predicate (list (intern (string-upcase el) :GL-DATA)
-                                                 (get-next-var-from-var-list vars)))
-                      ))
-                  (setf const-term nil))
-                 
-                 ;; append the last city identifier
-                 (city-id-found
-                  (setf curr-predicate (list (intern (string-upcase el) :GL-DATA)
-                                             (get-next-var (get-next-var-from-var-list vars))))
-                   (setf city-id-found nil)
-                   )
-                 
-                 ;; we've passed a const term, save the next term
-                 ((and const-found
-                       (lower-case-p (first (coerce el 'list))))
-                  (setf const-found nil)
-                  (setf const-term (string-downcase el)))
-
-                 ;; we found the const var
-                 ((and const-found
-                       (upper-case-p (first (coerce el 'list))))
-                  (setf const-var (variablify el)))
-                 
-                 ;; it's a term, append the last term to result, start a new one
-                 ((lower-case-p (first (coerce el 'list)))
-                  (when curr-predicate
-                    (setf result (append result (list curr-predicate))))
-                  (if (string= el "const")
-                    (setf const-found t)
-                    (setf curr-predicate (list (intern (string-upcase el) :GL-DATA)))))
-                 
-                 ;; it's a number
-                 ((numberp (read-from-string el))
+  (loop with stack = (list (geo-prolog-to-polish-notation geo-prolog-string))
+        with var-list = (get-all-vars geo-prolog-string)
+        with program-level = (first (push (get-next-var-from-var-list var-list) var-list))
+        
+        while stack     
+        for part = (pop stack)
+        append (loop with next-level = (get-next-var-from-var-list var-list)
+                     for predicate in part
+                     for term = (when (equal (type-of predicate) 'cons)
+                                  (first predicate))
+                     for args = (when (equal (type-of predicate) 'cons)
+                                  (rest predicate))
+                     for vars = (append (list program-level)
+                                        (loop for el in args
+                                              if (variable-p el)
+                                              collect el
+                                              else
+                                              collect next-level))
+                     for cand-emb-preds = (loop for el in args
+                                                unless (variable-p el)
+                                                collect el)
+                     for accum-val = (append (list term) vars)
+                     do (cond (;; predicate is a constant e.g. 'new_york
+                               (equal (type-of predicate) 'symbol)
+                               (setf accum-val (list predicate program-level)))
+                              ;; term is actually a list, continue with the list in next iteration   
+                              ((equal (type-of term) 'cons)
+                               (setf accum-val nil)
                   
-                  (setf curr-predicate (append curr-predicate (list (read-from-string el))))
-                  )
-                 ;; it's a variable
-                 (t
-                  (setf curr-predicate (append curr-predicate (list (variablify el))))
-                  (push (variablify el) vars)))
-        finally (return (append result (list curr-predicate)))))
+                               (setf stack (append stack (append (list term) cand-emb-preds))))
+                              ;; rest is not preceded by round brackets, add them
+                              ((and cand-emb-preds
+                                    (or
+                                     (equal (type-of (car cand-emb-preds)) 'symbol) ;(new_york)
+                                     (equal (type-of (caar cand-emb-preds)) 'symbol))) ;((cityid
+                               (setf stack (append stack (list cand-emb-preds))))
+                              
+                              ;; term is an actual term, collect the result and continue with the embedded predicates
+                              (t
+                               (setf stack (append stack cand-emb-preds))))
+
+                     when accum-val
+                     collect accum-val
+                     finally (when accum-val (setf program-level (first (push (get-next-var-from-var-list var-list) var-list)))))))
+        
+
         
 
 (defun get-next-var-from-var-list (var-list)
@@ -138,71 +111,56 @@
   (intern (nth (+ 1 (position (symbol-name last-var) gl::+placeholder-vars+ :test #'equal)) gl::+placeholder-vars+)))
 
 (defun geo-prolog-to-polish-notation (geo-prolog-string)
-  (let* ((geo-prolog-string (cl-ppcre:regex-replace-all "([a-z]+)[\(]" geo-prolog-string "\(\\1 "))
-         (geo-prolog-string (cl-ppcre:regex-replace-all "([A-Z_])" geo-prolog-string "?\\1"))
+  (let* ((geo-prolog-string (cl-ppcre:regex-replace-all "([a-z]+_?[a-z]+)[\(]" geo-prolog-string "\(\\1 "))
+         (geo-prolog-string (cl-ppcre:regex-replace-all "([A-Z])" geo-prolog-string "?\\1"))
+         (geo-prolog-string (cl-ppcre:regex-replace-all ",(_)" geo-prolog-string ",?\\1"))
          (geo-prolog-string (cl-ppcre:regex-replace-all "'([a-z]+) ([a-z]+)'" geo-prolog-string "\\1_\\2"))
          (geo-prolog-string (replace-all geo-prolog-string "," " "))
          (geo-prolog-string (string-append "("geo-prolog-string ")")))
     (read-from-string geo-prolog-string)))
 
-(defun serialize-embedded-predicates (geo-prolog-string)
-  (remove nil (loop with emb-preds = (geo-prolog-to-polish-notation geo-prolog-string)
-                    with var-list = (get-all-vars geo-prolog-string)
-                    with program-level = (first (push (get-next-var-from-var-list var-list) var-list))
-                    while emb-preds
-                    append (loop
-                            with next-level = (get-next-var-from-var-list var-list)
-                            for predicate in emb-preds
-                            for term = (when (equal (type-of predicate) 'cons)
-                                         (first predicate))
-                            for args = (when (equal (type-of predicate) 'cons)
-                                         (rest predicate))
-                            for vars = (append (list program-level)
-                                               (loop for el in args
-                                                     if (variable-p el)
-                                                     collect el
-                                                     else
-                                                     collect next-level))
-                            for cand-emb-preds = (loop for el in args
-                                                       unless (variable-p el)
-                                                       collect el)
-                            for accum-val = (append (list term) vars)
-                            do (cond (;; predicate is a constant e.g. 'new_york
-                                      (equal (type-of predicate) 'symbol)
-                                      (setf accum-val (list program-level predicate))
-                                      (setf emb-preds nil))
-                                      ;; term is actually a list, continue with the list in next iteration   
-                                     ((equal (type-of term) 'cons)
-                                      (setf accum-val nil)
-                                      (setf emb-preds (append (list term) cand-emb-preds)))
-                                     ;; term is an actual term, collect the result and continue with the embedded predicates
-                                     (t
-                                      (setf emb-preds cand-emb-preds)
-                                      ))
-                            when accum-val
-                            collect accum-val
-                            ;; update the program-level
-                            finally (when accum-val (setf program-level (first (push (get-next-var-from-var-list var-list) var-list))))))))
-  
-          
-          
+
+(geo-prolog-to-polish-notation "answer(A,(state(A),const(B,riverid(chattahoochee)),river(B),traverse(B,A)))")
+(geo-prolog-to-predicates "answer(A,(state(A),const(B,riverid(chattahoochee)),river(B),traverse(B,A)))")
+#|
+(answer ?c ?a ?d)
+(state ?d ?a)
+(const ?d ?b ?e)
+(riverid ?e ?f)
+(chattahoochee ?f)
+(river ?d ?b)
+(traverse ?d ?b ?a)
+|#
+
 
 ;; How many rivers do not traverse the state with the capital Albany ?
-;(geo-prolog-to-predicates "answer(A,count(B,(river(B),not((traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_))))),A))")
-
-(serialize-embedded-predicates "answer(A,count(B,(river(B),not((traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_))))),A))")
-
-(serialize-embedded-predicates "answer(A,(size(B,A),const(B,cityid('new york',_))))")
+;; (geo-prolog-to-predicates "answer(A,count(B,(river(B),not((traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_))))),A))")
 #|
-(answer ?x ?a ?y)
-(size ?y ?b ?a)
-(const ?y ?b ?z)
-(cityid ?z 'new_york ?_)
+(answer ?e ?a ?f)
+(count ?f ?b ?g ?a)
+(river ?g ?b)
+(not ?g ?h)
+(traverse ?h ?b ?c)
+(state ?h ?c)
+(loc ?h ?d ?c)
+(capital ?h ?d)
+(const ?h ?d ?i)
+(cityid ?i ?j ?_)
+(albany ?j)
+|#
+
+(geo-prolog-to-predicates "answer(A,(size(B,A),const(B,cityid('new york',_))))")
+#|
+(answer ?c ?a ?d)
+(size ?d ?b ?a)
+(const ?d ?b ?e)
+(cityid ?e ?f ?_)
+(new_york ?f)
 
 |#
 
 
-(serialize-embedded-predicates "answer(A,count(B,(state(B),not((loc(C,B),river(C)))),A))")
+(geo-prolog-to-predicates "answer(A,count(B,(state(B),not((loc(C,B),river(C)))),A))")
  #|
 (answer ?d ?a ?e)
 (count ?e ?b ?f ?a)
@@ -212,7 +170,20 @@
 (river ?g ?c)
 
     
-|# 
+|#
+
+(geo-prolog-to-predicates "answer(A,(high_point(B,A),state(B),next_to(B,C),const(C,stateid(mississippi))))")
+#|
+(answer ?d ?a ?e)
+(high_point ?e ?b ?a)
+(state ?e ?b)
+(next_to ?e ?b ?c)
+(const ?e ?c ?f)
+(stateid ?f ?g)
+(mississippi ?g)
+"answer(A,(high_point(B,A),state(B),next_to(B,C),const(C,(stateid(mississippi)))))"
+|#
+
 
  
 ;(geo-prolog-to-predicates "answer(A,(size(B,A),const(B,cityid('new york',_))))")
