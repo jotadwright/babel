fe0a616605d8302f7bc2b2c0ee5dc0766de28745
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-08-27 00:27:44 +0200

    bugfix for incorrect args in applied cxn in partial analysis repair

--- systems/grammar-learning/diagnostics-and-repairs/repair-item-based-to-holistic.lisp
+++ systems/grammar-learning/diagnostics-and-repairs/repair-item-based-to-holistic.lisp
@@ -50,12 +50,15 @@
              (inverted-cxn-meanings (get-inverted-cxn-meanings applied-cxns meaning))
              
              (remaining-meaning (subtract-cxn-meanings-from-gold-standard-meaning inverted-cxn-meanings meaning))
-             (args-holistic-cxn (extract-args-from-meaning-networks remaining-meaning (first inverted-cxn-meanings) meaning-representation-formalism)))
-             ;(args-holistic-cxn (extract-args-from-meaning-networks remaining-meaning (append parent-meaning (first inverted-cxn-meanings)) meaning-representation-formalism)))
+             
+             (args-holistic-cxn (extract-args-from-meaning-networks remaining-meaning (append parent-meaning (first inverted-cxn-meanings)) meaning-representation-formalism)))
              
         (when (and inverted-cxn-meanings
                    (if (equal meaning-representation-formalism :irl)
                         (= (length args-holistic-cxn) 2))
+                   (if (equal meaning-representation-formalism :irl)
+                     (= (length (extract-args-from-meaning-networks remaining-meaning (first inverted-cxn-meanings) meaning-representation-formalism))
+                        2)) ;; the args should both come from the inverted cxn meaning, otherwise it won't recombine
                    remaining-meaning
                    (connected-semantic-network remaining-meaning)
                    (check-meets-continuity remaining-form-constraints) ;there is one continuous string in root
