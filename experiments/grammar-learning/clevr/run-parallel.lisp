5060a59cef171148eb2bbccf946ebf8ce25b7ab9
Jonas Doumen <jonas.doumen@kuleuven.be>
2022-09-06 13:22:29 +0200

    adding run-parallel config for clevr-en

--- experiments/grammar-learning/clevr/run-parallel.lisp
+++ experiments/grammar-learning/clevr/run-parallel.lisp
@@ -1,26 +1,39 @@
-(ql:quickload :clevr-grammar-learning)
-(in-package :clevr-grammar-learning)
+(ql:quickload :grammar-learning)
+(in-package :grammar-learning)
 
 (let ((experiment-names
        '(training-10-series)))
   (run-parallel-batch-for-different-configurations 
-   :asdf-system "clevr-grammar-learning"
-   :package "clevr-grammar-learning"
-   :experiment-class "clevr-grammar-learning-experiment"
-   :number-of-interactions 47133
+   :asdf-system "grammar-learning"
+   :package "grammar-learning"
+   :experiment-class "grammar-learning-experiment"
+   :number-of-interactions 47134
    :number-of-series 10
    :monitors (append (get-all-export-monitors)
                      (get-all-lisp-monitors)
                      (get-all-csv-monitors))
    :configurations '(
                      (training-10-series
-                      ((:category-linking-mode . :neighbours)
-                       (:current-challenge-level . 1)
-                       (:number-of-epochs . 1)
+                      ((:number-of-epochs . 1)
                        ))
                      )
-   :shared-configuration '((:determine-interacting-agents-mode . :corpus-learner)
+   :shared-configuration `(
                            (:observation-sample-mode . :train)
-                           (:run-mode :training))
-   :output-dir (babel-pathname :directory '("experiments" "clevr-grammar-learning" "raw-data"))
+                           (:meaning-representation . :irl)
+                           (:de-render-mode . :de-render-string-meets-no-punct)
+                           (:corpus-files-root . ,(merge-pathnames
+                                     (make-pathname :directory '(:relative "clevr-grammar-learning"))
+                                     cl-user:*babel-corpora*))
+                           (:corpus-data-file . ,(make-pathname :directory '(:relative "train")
+                                                   :name "stage-1" :type "jsonl"))
+                           (:repairs . (add-categorial-links
+                                           item-based->item-based--substitution
+                                           item-based->holistic
+                                           holistic->item-based--substitution
+                                           ;holistic->item-based--addition
+                                           ;holistic->item-based--deletion
+                                           holistic->item-based
+                                           nothing->holistic))
+                           )
+   :output-dir (babel-pathname :directory '("experiments" "grammar-learning" "clevr" "raw-data"))
    :heap-size 12288))
