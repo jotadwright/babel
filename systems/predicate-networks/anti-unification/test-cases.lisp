8daec8a2f50fbab6b79911e1157d7de27971116b
jnevens <jnevens@ai.vub.ac.be>
2022-12-06 15:57:06 +0100

    anti-unification test cases and visualisation

--- systems/predicate-networks/anti-unification/test-cases.lisp
+++ systems/predicate-networks/anti-unification/test-cases.lisp
@@ -3,142 +3,148 @@
 
 ;; ############################################################################
 ;; Test Cases
+;; This file contains a number of examples of the anti-unification over
+;; predicate networks. Each example consists of two input networks
+;; (network-1 and network-2), their corresponding form in natural language,
+;; and the result of the anti-unification process. The latter consists of
+;; the generalisation, two bindings lists, and two deltas.
 ;; ----------------------------------------------------------------------------
 
 ;; Substitution example (same type in bind statement)
-(defparameter *pattern-network* ;; are there red cubes?
-  '((get-context ?context)
-    (filter ?set-1 ?context ?cube)
-    (bind shape-category ?cube cube)
-    (filter ?set-2 ?set-1 ?color-1)
-    (bind color-category ?color-1 red)
-    (exist ?target ?set-2)))
+;; ==================================================
 
-(defparameter *source-network* ;; are there red spheres?
-  '((get-context ?c)
-    (filter ?s1 ?c ?b1)
-    (bind shape-category ?b1 sphere)
-    (filter ?s2 ?s1 ?b2)
-    (bind color-category ?b2 red)
-    (exist ?t ?s2)))
+(defparameter *test-case-1*
+  (let (;; are there red cubes?
+        (network-1
+         '((get-context ?context)
+           (filter ?set-1 ?context ?cube)
+           (bind shape-category ?cube cube)
+           (filter ?set-2 ?set-1 ?color-1)
+           (bind color-category ?color-1 red)
+           (exist ?target ?set-2)))
 
-(reorder-source-network
- (shuffle *pattern-network*)
- (shuffle *source-network*)
- nil)
-;; => 1 solutions
-;; 1. every predicate from pattern gets assigned to the correct
-;;    predicate from source AND the non-matching bind statements
-;;    (i.e. cube and sphere) get assigned to each other
+        ;; are there red spheres?
+        (network-2
+         '((get-context ?c)
+           (filter ?s1 ?c ?b1)
+           (bind shape-category ?b1 sphere)
+           (filter ?s2 ?s1 ?b2)
+           (bind color-category ?b2 red)
+           (exist ?t ?s2)))
 
-(anti-unify
- (shuffle *pattern-network*)
- (shuffle *source-network*))
-;; ==> see web interface
+        ;; are there red X?
+        (msg 
+         '((get-context ?context)
+           (filter ?set-1 ?context ?X)
+           (bind shape-category ?X ?Y)
+           (filter ?set-2 ?set-1 ?color-1)
+           (bind color-category ?color-1 red)
+           (exist ?target ?set-2)))
 
-;; LGG (is this what we want?)
-;; are there red X?
-'((get-context ?context)
-  (filter ?set-1 ?context ?Y)
-  (?X ?Y)
-  (filter ?set-2 ?set-1 ?color-1)
-  (bind color-category ?color-1 red)
-  (exist ?target ?set-2))
-'(((bind shape-category ?Y cube) . ?X) (?cube . ?Y))
-'(((bind shape-category ?Y sphere) . ?X) (?b1 . ?Y))
+        (bindings-1
+         '((?cube . ?X) (cube . ?Y)))
+
+        (bindings-2
+         '((?b1 . ?X) (sphere . ?Y)))
+
+        (delta-1 nil)
+        (delta-2 nil))
+    (draw-anti-unification-test-case network-1 network-2 msg bindings-1 bindings-2 delta-1 delta-2)))
 
 ;; ############################################################################
 
 ;; Substitution example (different type in bind statement)
-(defparameter *pattern-network* ;; are there red cubes?
-  '((get-context ?context)
-    (filter ?set-1 ?context ?shape-1)
-    (bind shape-category ?shape-1 cube)
-    (filter ?set-2 ?set-1 ?color-1)
-    (bind color-category ?color-1 red)
-    (exist ?target ?set-2)))
+;; =======================================================
 
-(defparameter *source-network* ;; are there small cubes?
-  '((get-context ?c)
-    (filter ?s1 ?c ?b1)
-    (bind shape-category ?b1 cube)
-    (filter ?s2 ?s1 ?b2)
-    (bind size-category ?b2 small)
-    (exist ?t ?s2)))
+(defparameter *test-case-2*
+  (let (;; are there red cubes?
+        (network-1
+         '((get-context ?context)
+           (filter ?set-1 ?context ?shape-1)
+           (bind shape-category ?shape-1 cube)
+           (filter ?set-2 ?set-1 ?color-1)
+           (bind color-category ?color-1 red)
+           (exist ?target ?set-2)))
 
-(reorder-source-network
- (shuffle *pattern-network*)
- (shuffle *source-network*)
- nil)
-;; => 1 solutions
-;; 1. every predicate from pattern gets assigned to the correct
-;;    predicate from source AND the non-matching bind statements
-;;    (i.e. red and small) get assigned to each other
+        ;; are there small cubes?
+        (network-2
+         '((get-context ?c)
+           (filter ?s1 ?c ?b1)
+           (bind shape-category ?b1 cube)
+           (filter ?s2 ?s1 ?b2)
+           (bind size-category ?b2 small)
+           (exist ?t ?s2)))
+        
+        ;; are there X cubes?
+        (msg
+         '((get-context ?context)
+           (filter ?set-1 ?context ?shape-1)
+           (bind shape-category ?shape-1 cube)
+           (filter ?set-2 ?set-1 ?X)
+           (bind ?Y ?X ?Z)
+           (exist ?target ?set-2)))
 
-(anti-unify
- (shuffle *pattern-network*)
- (shuffle *source-network*))
-;; ==> see web interface
-
-;; LGG (is this what we want?)
-;; are there X cubes?
-'((get-context ?context)
-  (filter ?set-1 ?context ?shape-1)
-  (bind shape-category ?shape-1 cube)
-  (filter ?set-2 ?set-1 ?X)
-  ?Y
-  (exist ?target ?set-2))
-'(((bind color-category ?X red) . ?Y)  (?color-1 . ?X))
-'(((bind size-category ?X small) . ?Y) (?b2 . ?X))
+        (bindings-1
+         '((?color-1 . ?X) (color-category . ?Y) (red . ?Z)))
+        
+        (bindings-2
+         '((?b2 . ?X) (size-category . ?Y) (small . ?Z)))
+        (delta-1 nil)
+        (delta-2 nil))
+    (draw-anti-unification-test-case network-1 network-2 msg bindings-1 bindings-2 delta-1 delta-2)))
 
 ;; ############################################################################
 
 ;; Substitution example (different final predicate)
-(defparameter *pattern-network* ;; are there red cubes?
-  '((get-context ?context)
-    (filter ?set-1 ?context ?shape-1)
-    (bind shape-category ?shape-1 cube)
-    (filter ?set-2 ?set-1 ?color-1)
-    (bind color-category ?color-1 red)
-    (exist ?target ?set-2)))
+;; ================================================
 
-(defparameter *source-network* ;; how many red cubes?
-  '((get-context ?c)
-    (filter ?s1 ?c ?b1)
-    (bind shape-category ?b1 cube)
-    (filter ?s2 ?s1 ?b2)
-    (bind color-category ?b2 red)
-    (count ?t ?s2)))
+(defparameter *test-case-3*
+  (let (;; are there red cubes?
+        (network-1
+         '((get-context ?context)
+           (filter ?set-1 ?context ?shape-1)
+           (bind shape-category ?shape-1 cube)
+           (filter ?set-2 ?set-1 ?color-1)
+           (bind color-category ?color-1 red)
+           (exist ?target ?set-2)))
 
-(reorder-source-network
- (shuffle *pattern-network*)
- (shuffle *source-network*)
- nil)
-;; => 1 solutions
-;; 1. every predicate from pattern gets assigned to the correct
-;;    predicate from source AND the non-matching predicates
-;;    (i.e. exist and count) get assigned to each other
+        ;; how many red cubes?
+        (network-2
+         '((get-context ?c)
+           (filter ?s1 ?c ?b1)
+           (bind shape-category ?b1 cube)
+           (filter ?s2 ?s1 ?b2)
+           (bind color-category ?b2 red)
+           (count ?t ?s2)))
 
-(anti-unify
- (shuffle *pattern-network*)
- (shuffle *source-network*))
-;; ==> see web interface
+        ;; X red cubes?
+        (msg
+         '((get-context ?context)
+           (filter ?set-1 ?context ?shape-1)
+           (bind shape-category ?shape-1 cube)
+           (filter ?Y ?set-1 ?color-1)
+           (bind color-category ?color-1 red)))
 
-;; LGG (is this what we want?)
-;; X red cubes?
-'((get-context ?context)
-  (filter ?set-1 ?context ?shape-1)
-  (bind shape-category ?shape-1 cube)
-  (filter ?set-2 ?set-1 ?color-1)
-  (bind color-category ?color-1 red)
-  ?X)
-'(((exist ?target ?Y) . ?X) (?set-2 . Y))
-'(((count ?target ?Y) . ?X) (?s2 . Y))
+        (bindings-1
+         '((?set-2 . ?Y)))
+
+        (bindings-2
+         '((?s2 . ?Y)))
+
+        (delta-1
+         '((exist ?target ?set-2)))
+
+        (delta-2
+         '((count ?t ?s2))))
+    (draw-anti-unification-test-case network-1 network-2 msg bindings-1 bindings-2 delta-1 delta-2)))
 
 ;; ############################################################################
 
-;; Substitution example
-(defparameter *pattern-network*  ;; how many cubes or spheres
+;; Substitution example (network with cycle)
+;; =========================================
+
+;; how many cubes or spheres
+(defparameter *network-1*  
   '((get-context ?context)
     (filter ?cube-set ?context ?cube)
     (bind shape-category ?cube cube)
@@ -147,7 +153,8 @@
     (union ?set ?cube-set ?sphere-set)
     (count ?target ?set)))
 
-(defparameter *source-network* ;; how many cubes or cylinders
+;; how many cubes or cylinders
+(defparameter *network-2* 
   '((get-context ?c)
     (filter ?s1 ?c ?b1)
     (bind shape-category ?b1 cube)
@@ -156,35 +163,31 @@
     (union ?s3 ?s1 ?s2)
     (count ?t ?s3)))
 
-(reorder-source-network
- (shuffle *pattern-network*)
- (shuffle *source-network*)
- nil)
-;; => 1 solutions
-;; 1. every predicate from pattern gets assigned to the correct
-;;    predicate from source AND the non-matching bind statements
-;;    (i.e. sphere and cylinder) get assigned to each other
-
-(anti-unify
- (shuffle *pattern-network*)
- (shuffle *source-network*))
-;; ==> see web interface
-
-;; LGG (is this what we want?)
+;; MSG
+;; how many cubes or X
 '((get-context ?context)
   (filter ?cube-set ?context ?cube)
   (bind shape-category ?cube cube)
   (filter ?sphere-set ?context ?Y)
-  ?X
+  (bind shape-category ?Y ?X)
   (union ?set ?cube-set ?sphere-set)
   (count ?target ?set))
-'(((bind shape-category ?Y sphere) . ?X) (?sphere . ?Y))
-'(((bind shape-category ?Y cylinder) . ?X) (?b2 . ?Y))
+;; bindings-1
+'((?sphere . ?Y) (sphere . ?X))
+;; bindings-2
+'((?b2 . ?Y) (cylinder . ?X))
+;; delta-1
+nil
+;; delta-2
+nil
 
 ;; ############################################################################
 
 ;; Addition example
-(defparameter *pattern-network* ;; how many red cubes?
+;; ================
+
+;; how many red cubes?
+(defparameter *network-1* 
   '((get-context ?context)
     (filter ?set-1 ?context ?shape-1)
     (bind shape-category ?shape-1 cube)
@@ -192,7 +195,8 @@
     (bind color-category ?color-1 red)
     (count ?target ?set-2)))
 
-(defparameter *source-network* ;; how many large red cubes?
+;; how many large red cubes?
+(defparameter *network-2* 
   '((get-context ?c)
     (filter ?s1 ?c ?b1)
     (bind shape-category ?b1 cube)
@@ -202,29 +206,24 @@
     (bind size-category ?b3 large)
     (count ?t ?s3)))
 
-(reorder-source-network
- *pattern-network*
-  *source-network*
- nil)
-;; => 2 solutions
+;; MSG
+;; how many X red cubes?
 
-;; LGG (is this what we want?)
-;; How many X red cubes?
-'((get-context ?context)
-  (filter ?set-1 ?context ?shape-1)
-  (bind shape-category ?shape-1 cube)
-  (filter ?Y ?set-1 ?color-1)
-  (bind color-category ?color-1 red)
-  (?A ?X ?Y ?Z)
-  (?B ?Z)
-  (count ?target ?X))
-'((0 . ?A) (0 . ?B) (0 . ?Z) (?set-2 . ?Y) (?set-2 . ?X))
-'(((filter ?X ?Y ?Z) . ?A) ((bind size-category ?Z large) . ?B) (?s2 . ?Y) (?s3 . ?X) (?b3 . ?Z))
+;; bindings-1
+
+;; bindings-2
+
+;; delta-1
+
+;; delta-2
 
 ;; ############################################################################
 
 ;; Deletion example
-(defparameter *pattern-network* ;; how many large red cubes?
+;; ================
+
+;; how many large red cubes?
+(defparameter *network-1* 
   '((get-context ?c)
     (filter ?s1 ?c ?b1)
     (bind shape-category ?b1 cube)
@@ -234,7 +233,8 @@
     (bind size-category ?b3 large)
     (count ?t ?s3)))
 
-(defparameter *source-network* ;; how many red cubes?
+;; how many red cubes?
+(defparameter *network-2* 
   '((get-context ?context)
     (filter ?set-1 ?context ?shape-1)
     (bind shape-category ?shape-1 cube)
@@ -242,37 +242,33 @@
     (bind color-category ?color-1 red)
     (count ?target ?set-2)))
 
-(reorder-source-network
- (shuffle *pattern-network*)
- (shuffle *source-network*)
- nil)
-;; => NIL :-(
+;; MSG
+;; how many X red cubes?
 
-;; LGG (is this what we want?)
-;; How many X red cubes?
-'((get-context ?context)
-  (filter ?set-1 ?context ?shape-1)
-  (bind shape-category ?shape-1 cube)
-  (filter ?Y ?set-1 ?color-1)
-  (bind color-category ?color-1 red)
-  (?A ?X ?Y ?Z)
-  (?B ?Z)
-  (count ?target ?X))
-'((0 . ?A) (0 . ?B) (0 . ?Z) (?set-2 . ?Y) (?set-2 . ?X))
-'(((filter ?X ?Y ?Z) . ?A) ((bind size-category ?Z large) . ?B) (?s2 . ?Y) (?s3 . ?X) (?b3 . ?Z))
+;; bindings-1
+
+;; bindings-2
+
+;; delta-1
+
+;; delta-2
 
 ;; ############################################################################
 
-;; Substitution example with variables
-(defparameter *pattern-network* ;; how many X cubes?
-  '((get-context ?c)
-    (filter ?s1 ?c ?b1)
-    (bind shape-category ?b1 cube)
-    (filter ?s2 ?s1 ?Y)
-    (?X ?Y)
-    (count ?t ?s2)))
+;; Substitution example (generalising over previous generalisation)
+;; ================================================================
 
-(defparameter *pattern-network* ;; how many X spheres?
+;; how many X cubes?
+(defparameter *network-1* 
+  '((get-context ?context)
+    (filter ?set-1 ?context ?shape-1)
+    (bind shape-category ?shape-1 cube)
+    (filter ?set-2 ?set-1 ?Y)
+    (?X ?Y)
+    (count ?target ?set-3)))
+
+;; how many X spheres?
+(defparameter *network-2* 
   '((get-context ?c)
     (filter ?s1 ?c ?b1)
     (bind shape-category ?b1 sphere)
@@ -280,16 +276,92 @@
     (?X ?Y)
     (count ?t ?s2)))
 
-;; LGG (is this what we want?)
+;; MSG
 ;; how many X Y?
-'((get-context ?x)
-  (filter ?s1 ?c ?A)
-  (?Z ?A)
-  (filter ?s1 ?s1 ?b2)
-  ?X
-  (count ?t ?s2))
-'(((bind shape-category ?b1 cube) . ?Z) (?b1 . ?A))
-'(((bind shape-category ?b1 sphere) . ?Z) (?b1 . ?A))
+
+;; bindings-1
+
+;; bindings-2
+
+;; delta-1
+
+;; delta-2
+
+
+;; ############################################################################
+
+;; Substitution example (same operators in different order)
+;; ========================================================
+
+;; how many large red cubes?
+(defparameter *network-1* 
+  '((get-context ?c)
+    (filter ?s1 ?c ?b1)
+    (bind shape-category ?b1 cube)
+    (filter ?s2 ?s1 ?b2)
+    (bind color-category ?b2 red)
+    (filter ?s3 ?s2 ?b3)
+    (bind size-category ?b3 large)
+    (count ?t ?s3)))
+(add-element (predicate-network->html *pattern-network*))
+
+;; how many red large cubes?
+(defparameter *network-2* 
+  '((get-context ?context)
+    (filter ?set-1 ?context ?shape-1)
+    (bind shape-category ?shape-1 cube)
+    (filter ?set-2 ?set-1 ?size-1)
+    (bind size-category ?size-1 large)
+    (filter ?set-3 ?set-2 ?color-1)
+    (bind color-category ?color-1 red)
+    (count ?target ?set-3)))
+(add-element (predicate-network->html *source-network*))
+
+;; MSG
+
+;; bindings-1
+
+;; bindings-2
+
+;; delta-1
+
+;; delta-2
+
+;; ############################################################################
+
+;; Substitution example (not minimal difference)
+;; =============================================
+
+;; what color is the large cube?
+(defparameter *network-1*
+  )
+
+;; what color is the small blue sphere?
+(defparameter *network-2*
+  )
+
+;; MSG
+
+;; bindings-1
+
+;; bindings-2
+
+;; delta-1
+
+;; delta-2
+
+;; ############################################################################
+
+;; Substitution example (not minimal difference, with cycle)
+;; =========================================================
+
+;; how many cubes or large spheres?
+(defparameter *network-1*
+  )
+
+;; how many small spheres or cylinders?
+(defparameter *network-2*
+  )
 
 
 
