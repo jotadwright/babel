0e4a19212b27b33b829ae6984b530b7f2db092f0
ldevos <liesbet.devos@unamur.be>
2022-12-20 09:54:56 +0100

    Added evo-plot code to start

--- tutorial/naming-game/start.lisp
+++ tutorial/naming-game/start.lisp
@@ -5,6 +5,8 @@
 ;run batches of experiment;
 ;-------------------------;
 
+(defparameter *all-words* '())
+
 (Run-Batch-for-different-configurations
  :experiment-class 'naming-game-experiment
  :number-of-interactions 5000
@@ -21,6 +23,27 @@
              "display-lexicon-size"
              "export-lexicon-size")
  :output-dir (babel-pathname :directory '("tutorial" "naming-game" "raw-data")))
+
+(raw-files->evo-plot
+ :raw-file-paths '(("tutorial" "naming-game" "raw-data" "test" "communicative-success")
+                   ("tutorial" "naming-game" "raw-data" "test" "lexicon-size")
+		   )
+  :average-windows 100
+  :plot-file-name "naming-game-results" 
+  :plot-directory '("tutorial" "naming-game" "graphs")
+  :captions '("Communicative success")
+  :y1-min 0
+  :y1-max 1
+  :y2-min 0
+  :y2-max 50
+  :x-label "# Games"
+  :y1-label "communicative sucess"
+  :y2-label "lexicon size"
+  :use-y-axis '(1 2)
+  :error-bars '(:percentiles (25 75))
+  :error-bar-modes '(:filled)
+  :open t
+  )
 
 #| 
 (deactivate-all-monitors)
