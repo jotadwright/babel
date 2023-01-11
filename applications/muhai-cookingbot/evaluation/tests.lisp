2df9856b45c75793c109c5ebd34fd80b4e46438c
RobinDHVUB <72613839+RobinDHVUB@users.noreply.github.com>
2023-01-11 22:19:01 +0100

    Update tests.lisp

--- applications/muhai-cookingbot/evaluation/tests.lisp
+++ applications/muhai-cookingbot/evaluation/tests.lisp
@@ -8,7 +8,7 @@
 
 (defun test-perfect-10 ()
   "The same network as the simulation environment's solution, for the 10 initial recipes."
-  (let* ((solutions (evaluate-solutions "applications\\muhai-cookingbot\\evaluation\\tests\\test-perfect.solution" '(goal-condition-success dish-approximation-score execution-time))) ; smatch-score)))
+  (let* ((solutions (evaluate-solutions "applications\\muhai-cookingbot\\evaluation\\tests\\test-perfect-10.solution" '(goal-condition-success dish-approximation-score execution-time))) ; smatch-score)))
          (perfection (loop for solution in solutions
                              always (and ;(= (smatch-score solution) 1)
                                          (= (subgoals-ratio solution) 1)
