6b8f639747e55776167b399deed4e86b45fbfae5
Alexane Jouglar <73078605+AlexaneJ@users.noreply.github.com>
2023-01-18 09:31:57 +0100

    added some changes

--- sharing/nao-interface/flask-server-v4/py3-server/flask_server.py
+++ sharing/nao-interface/flask-server-v4/py3-server/flask_server.py
@@ -15,13 +15,8 @@ py3_server = Flask(__name__)
 
 @py3_server.route("/vision/analyse")
 def analyze_image():
-    im_fname = utils.download('https://raw.githubusercontent.com/zhreshold/' +
-                          'mxnet-ssd/master/data/demo/dog.jpg',
-                          path='dog.jpg')
-    x, img = data.transforms.presets.yolo.load_test(im_fname, short=512)
-    class_IDs, scores, bounding_boxs = vision_net(x)
-    ax = utils.viz.plot_bbox(img, bounding_boxs[0], scores[0], class_IDs[0], class_names=vision_net.classes)
-    return ax
+    
+    return "This is working"
 
 
 
