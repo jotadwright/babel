959ff3e80f60e228b6fa5de1782d39bac3944899
Alexane Jouglar <73078605+AlexaneJ@users.noreply.github.com>
2023-02-27 14:17:10 +0100

    small modif

--- experiments/ont-alignment/run.lisp
+++ experiments/ont-alignment/run.lisp
@@ -5,3 +5,8 @@
   (make-instance 'ont-alignment-experiment))
 
 (run-interaction *experiment*)
+
+
+;(postmodern::query "SELECT year FROM actorsfilms WHERE film = 'American Loser'")
+
+;(postmodern::query "SELECT film FROM actorsfilms WHERE actor = 'Nelson Franklin'")
