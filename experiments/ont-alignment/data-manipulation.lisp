c92991655ce892bb0d9f4659d172b54cb8415a7e
Alexane Jouglar <73078605+AlexaneJ@users.noreply.github.com>
2023-04-01 14:15:59 +0200

    advancing on predicates

--- experiments/ont-alignment/data-manipulation.lisp
+++ experiments/ont-alignment/data-manipulation.lisp
@@ -132,27 +132,35 @@
 
 ;(print-hash-table *sql-keywords-type*)
 
-(loop for elem in *sql-keyword-predicate*
-        do (print elem))
-;find a way to get the cdr of an element of the list given the elem
-
-(def get-cons-cdr (first-elem cons-item)
-     )
-
-        (member :select elem))
+(defun find-cons (list-of-cons value)
+  "Given an item and a list of cons, retrieves the corresponding cons where the car is equal to the item."
+  (cond ((null list-of-cons) nil)
+        ((equal (car (car list-of-cons)) value) (car list-of-cons))
+        (t (find-cons (cdr list-of-cons) value))))
 
 (defun sql-query-to-predicates (list-query)
   "From an sql keyword such as SELECT or WHERE, sends out a predicate equivalent."
-  (let ((keywords-list (list)))
-    (loop for elem in string-query
-       do (if (keywordp elem)
-           (push elem keywords-list)))
+  (let ((predicate-list '())
+        (start-index (position :where list-query)))
     ;let's check if the second element of the query (coming after the select) is a keyword
     (if (is-member (get-nth-element list-query 2) (gethash 'right-elem *sql-keywords-type*))
-      (
+      (push (find-cons *sql-keyword-predicate* (get-nth-element list-query 2)) predicate-list))
+    ;then we send the select element to the list
+    (push (find-cons *sql-keyword-predicate* :select) predicate-list)
+    ;then we send to the list any other predicate equivalent to a found keyword in the query
+    (loop for elem in (subseq list-query start-index)
+            do (if (symbolp elem)
+                 (push (find-cons *sql-keyword-predicate* elem) predicate-list)))
+    (print predicate-list)))
+
+;misses : how to push the surrounding element of a symbol to the predicate-list
+
+(sql-query-to-predicates *random-list*)
+
+(print (type-of (get-nth-element *random-list* 1)))
 
 (defun predicates-to-sql-query (predicate-query)
-  "From a predicate such as (select x), sends out the sql keyword equivalent."
+  "From a predicate such as, sends out the sql keyword equivalent."
   )
 
 ;(defvar *random-list* (list :select 'film :from 'actorsfilms :where (list := 'film "Perfect Combination")))
