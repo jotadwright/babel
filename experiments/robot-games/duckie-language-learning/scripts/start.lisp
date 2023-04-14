84e55d78bb009c12fea38ab6fbb69bf1382043a4
jerome <jerome@ai.vub.ac.be>
2023-04-14 17:55:22 +0200

    fix: start script

--- experiments/robot-games/duckie-language-learning/scripts/start.lisp
+++ experiments/robot-games/duckie-language-learning/scripts/start.lisp
@@ -6,9 +6,26 @@
 
 ;;;; START DEMO
 
+(setf *ontology* (build-initial-ontology))
+
 (setf *server-address* "http://192.168.2.5:7000/")
 
 (defparameter *demo*
   (make-instance 'duckie-language-learning-experiment))
 
 (run-interaction *demo*)
+
+;; symbolic evaluation
+(setf *duckie-world* (make-instance 'object-set
+                                    :objects (list (make-instance 'duckie-building
+                                                                  :zone 1
+                                                                  :building-function 'house
+                                                                  :color 'green
+                                                                  :rfid 1)
+                                                   (make-instance 'duckie-building
+                                                                  :zone 2
+                                                                  :building-function 'house
+                                                                  :color 'red
+                                                                  :rfid 2))))
+
+(set-data *ontology* 'world *duckie-world*)
