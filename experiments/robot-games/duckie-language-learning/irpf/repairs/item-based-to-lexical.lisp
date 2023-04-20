7f5220a8ae4d577862c97021e6cfee2ca31a5365
jerome <jerome@ai.vub.ac.be>
2023-04-21 01:23:25 +0200

    fix: notify start of intention reading in web-interface

--- experiments/robot-games/duckie-language-learning/irpf/repairs/item-based-to-lexical.lisp
+++ experiments/robot-games/duckie-language-learning/irpf/repairs/item-based-to-lexical.lisp
@@ -37,87 +37,88 @@
                (= (item-based-number-of-slots applied-item-based-cxn)
                   (1+ (length applied-lex-cxns))))
       (let* ((meaning-predicates-observed (mapcan #'extract-meaning-predicates applied-cxns))
-             (composer-strategy (get-configuration agent :composer-strategy))
-             (composer-solution (compose-program agent
-                                                 utterance
-                                                 :partial-program meaning-predicates-observed)))
-        (if composer-solution
-          (let* ((meaning-predicates-gold (append (bind-statements composer-solution)
-                                                  (irl-program (chunk composer-solution))))
-                 (meaning-predicates-lex-cxn (set-difference meaning-predicates-gold
-                                                             meaning-predicates-observed
-                                                             :test #'unify-irl-programs)))
-            ;; we don't know what the composer will return
-            ;; so we make sure that the meaning for the new
-            ;; lex cxn only contains a single element
-            (if (length= meaning-predicates-lex-cxn 1)
-              (let* ((form-predicates-lex-cxn
-                      remaining-strings-in-root)
-                     (existing-lex-cxn (find-cxn-by-type-form-and-meaning 'lexical form-predicates-lex-cxn
-                                                                          meaning-predicates-lex-cxn cxn-inventory))
-                     (cxn-name (make-const
-                                (make-cxn-name
-                                 (third (first form-predicates-lex-cxn)) cxn-inventory)))
-                     (unit-name (second (first form-predicates-lex-cxn)))
-                     (lex-class (if existing-lex-cxn
-                                  (lex-class-cxn existing-lex-cxn)
-                                  (intern (symbol-name (make-const unit-name)) :fcg)))
-                     (args (mapcar #'third meaning-predicates-lex-cxn))
-                     (initial-cxn-score (get-configuration agent :initial-cxn-score))
-                     (interaction (current-interaction (experiment agent)))
-                     (interaction-nr (interaction-number interaction))
-                     (new-lex-cxn (or existing-lex-cxn
-                                      (second
-                                       (multiple-value-list
-                                        (eval
-                                         `(def-fcg-cxn
-                                           ,cxn-name
-                                           ((,unit-name
-                                             (syn-cat (phrase-type lexical)
-                                                      (fcg::lex-class ,lex-class))
-                                             (args ,args))
-                                            <-
-                                            (,unit-name
-                                             (HASH meaning ,meaning-predicates-lex-cxn)
-                                             --
-                                             (HASH form ,form-predicates-lex-cxn)))
-                                           :attributes (:score ,initial-cxn-score
-                                                        :cxn-type lexical
-                                                        :repair item->lex
-                                                        :string ,(form-predicates->hash-string form-predicates-lex-cxn)
-                                                        :meaning ,(meaning-predicates->hash-meaning meaning-predicates-lex-cxn)
-                                                        :added-at ,interaction-nr)
-                                           :cxn-inventory ,(copy-object cxn-inventory)
-                                           :cxn-set non-holophrase))))))
-                     ;; make a list of all cxns, sort them
-                     (lex-cxns (sort-cxns-by-form-string
-                                (cons new-lex-cxn applied-lex-cxns)
-                                (remove-punctuation utterance)))
-                     (lex-classes-lex-cxns (mapcar #'lex-class-cxn lex-cxns))
-                     (lex-classes-item-based-units (get-all-unit-lex-classes applied-item-based-cxn))
-                     ;; assign all categorial links
-                     (categorial-network (categorial-network cxn-inventory))
-                     (categorial-links (when (and lex-classes-lex-cxns
-                                                  lex-classes-item-based-units
-                                                  (length= lex-classes-lex-cxns lex-classes-item-based-units))
-                                         (create-new-categorial-links lex-classes-lex-cxns
-                                                                      lex-classes-item-based-units
-                                                                      categorial-network))))
-                ;; return
-                (if categorial-links
-                  (progn
+             (composer-strategy (get-configuration agent :composer-strategy)))
+        (notify irl::chunk-composer-started (mkstr utterance))
+        (let ((composer-solution (compose-program agent
+                                                  utterance
+                                                  :partial-program meaning-predicates-observed)))
+          (if composer-solution
+            (let* ((meaning-predicates-gold (append (bind-statements composer-solution)
+                                                    (irl-program (chunk composer-solution))))
+                   (meaning-predicates-lex-cxn (set-difference meaning-predicates-gold
+                                                               meaning-predicates-observed
+                                                               :test #'unify-irl-programs)))
+              ;; we don't know what the composer will return
+              ;; so we make sure that the meaning for the new
+              ;; lex cxn only contains a single element
+              (if (length= meaning-predicates-lex-cxn 1)
+                (let* ((form-predicates-lex-cxn
+                        remaining-strings-in-root)
+                       (existing-lex-cxn (find-cxn-by-type-form-and-meaning 'lexical form-predicates-lex-cxn
+                                                                            meaning-predicates-lex-cxn cxn-inventory))
+                       (cxn-name (make-const
+                                  (make-cxn-name
+                                   (third (first form-predicates-lex-cxn)) cxn-inventory)))
+                       (unit-name (second (first form-predicates-lex-cxn)))
+                       (lex-class (if existing-lex-cxn
+                                    (lex-class-cxn existing-lex-cxn)
+                                    (intern (symbol-name (make-const unit-name)) :fcg)))
+                       (args (mapcar #'third meaning-predicates-lex-cxn))
+                       (initial-cxn-score (get-configuration agent :initial-cxn-score))
+                       (interaction (current-interaction (experiment agent)))
+                       (interaction-nr (interaction-number interaction))
+                       (new-lex-cxn (or existing-lex-cxn
+                                        (second
+                                         (multiple-value-list
+                                          (eval
+                                           `(def-fcg-cxn
+                                             ,cxn-name
+                                             ((,unit-name
+                                               (syn-cat (phrase-type lexical)
+                                                        (fcg::lex-class ,lex-class))
+                                               (args ,args))
+                                              <-
+                                              (,unit-name
+                                               (HASH meaning ,meaning-predicates-lex-cxn)
+                                               --
+                                               (HASH form ,form-predicates-lex-cxn)))
+                                             :attributes (:score ,initial-cxn-score
+                                                          :cxn-type lexical
+                                                          :repair item->lex
+                                                          :string ,(form-predicates->hash-string form-predicates-lex-cxn)
+                                                          :meaning ,(meaning-predicates->hash-meaning meaning-predicates-lex-cxn)
+                                                          :added-at ,interaction-nr)
+                                             :cxn-inventory ,(copy-object cxn-inventory)
+                                             :cxn-set non-holophrase))))))
+                       ;; make a list of all cxns, sort them
+                       (lex-cxns (sort-cxns-by-form-string
+                                  (cons new-lex-cxn applied-lex-cxns)
+                                  (remove-punctuation utterance)))
+                       (lex-classes-lex-cxns (mapcar #'lex-class-cxn lex-cxns))
+                       (lex-classes-item-based-units (get-all-unit-lex-classes applied-item-based-cxn))
+                       ;; assign all categorial links
+                       (categorial-network (categorial-network cxn-inventory))
+                       (categorial-links (when (and lex-classes-lex-cxns
+                                                    lex-classes-item-based-units
+                                                    (length= lex-classes-lex-cxns lex-classes-item-based-units))
+                                           (create-new-categorial-links lex-classes-lex-cxns
+                                                                        lex-classes-item-based-units
+                                                                        categorial-network))))
+                  ;; return
+                  (if categorial-links
+                    (progn
                     ;(add-composer-chunk agent (irl-program (chunk composer-solution)))
                     ;(notify item-based->lexical-new-cxn-and-categorial-links new-lex-cxn
                     ;        (categorial-network cxn-inventory) categorial-links)
-                    (set-data interaction :applied-repair 'item-based->lexical)
-                    ;; returns 
-                    ;; 1. existing cxns to apply
-                    ;; 2. new cxns to apply
-                    ;; 3. other new cxns
-                    ;; 4. categorial links
-                    (if existing-lex-cxn
-                      (list (cons new-lex-cxn applied-cxns) nil nil categorial-links)
-                      (list applied-cxns (list new-lex-cxn) nil categorial-links)))
-                  (progn (push 'fcg::repair-failed (statuses node)) nil)))
-              (progn (push 'fcg::repair-failed (statuses node)) nil)))
-          (progn (push 'fcg::repair-failed (statuses node)) nil))))))
+                      (set-data interaction :applied-repair 'item-based->lexical)
+                      ;; returns
+                      ;; 1. existing cxns to apply
+                      ;; 2. new cxns to apply
+                      ;; 3. other new cxns
+                      ;; 4. categorial links
+                      (if existing-lex-cxn
+                        (list (cons new-lex-cxn applied-cxns) nil nil categorial-links)
+                        (list applied-cxns (list new-lex-cxn) nil categorial-links)))
+                    (progn (push 'fcg::repair-failed (statuses node)) nil)))
+                (progn (push 'fcg::repair-failed (statuses node)) nil)))
+            (progn (push 'fcg::repair-failed (statuses node)) nil)))))))
--- experiments/robot-games/duckie-language-learning/irpf/repairs/lexical-to-item-based.lisp
+++ experiments/robot-games/duckie-language-learning/irpf/repairs/lexical-to-item-based.lisp
@@ -34,85 +34,86 @@
     (when (and applied-lex-cxns (null applied-item-based-cxn))
       (let* ((partial-program (deduplicate-variables (mapcan #'extract-meaning-predicates
                                                              applied-lex-cxns)))
-             (composer-strategy (get-configuration agent :composer-strategy))
-             (composer-solution (compose-program agent
-                                                 utterance
-                                                 :partial-program partial-program)))
-        (if composer-solution
-          (let* ((new-irl-program (append (bind-statements composer-solution)
-                                          (irl-program (chunk composer-solution))))
-                 (sorted-lex-cxns (sort-cxns-by-form-string applied-lex-cxns
-                                                            (remove-spurious-spaces (remove-punctuation utterance))))
-                 (var-form (form-constraints-with-variables utterance
-                                                            (get-configuration
-                                                             cxn-inventory
-                                                             :de-render-mode)))
-                 (subunit-names-and-non-overlapping-form (multiple-value-list
-                                                          (diff-non-overlapping-form var-form sorted-lex-cxns)))
-                 (subunit-names (first subunit-names-and-non-overlapping-form))
-                 (non-overlapping-form (second subunit-names-and-non-overlapping-form))
-                 (args-and-non-overlapping-meaning (multiple-value-list
-                                                    (diff-non-overlapping-meaning new-irl-program sorted-lex-cxns)))
-                 (args (first args-and-non-overlapping-meaning))
-                 (non-overlapping-meaning (second args-and-non-overlapping-meaning)))
-            (if (length= subunit-names args) ;; !!!
-              (let* ((cxn-name-item-based-cxn (make-const (make-cxn-name non-overlapping-form cxn-inventory)))
-                     (rendered-cxn-name-list (make-cxn-placeholder-name non-overlapping-form cxn-inventory))
-                     (placeholder-list (extract-placeholder-var-list rendered-cxn-name-list))
-                     (existing-item-based-cxn (find-cxn-by-type-form-and-meaning 'item-based
-                                                                                 non-overlapping-form
-                                                                                 non-overlapping-meaning
-                                                                                 cxn-inventory))
-                     (categorial-links (if existing-item-based-cxn
-                                         (mapcar #'cons (mapcar #'lex-class-cxn sorted-lex-cxns)
-                                                 (get-all-unit-lex-classes existing-item-based-cxn))
-                                         (create-categorial-network-links sorted-lex-cxns
-                                                                          (format nil "~{~a~^-~}" rendered-cxn-name-list)
-                                                                          placeholder-list
-                                                                          :item-based-numeric-tail t)))
-                     (lex-cxn-subunit-blocks (multiple-value-list (subunit-blocks-for-lex-cxns
-                                                                   sorted-lex-cxns
-                                                                   subunit-names
-                                                                   args
-                                                                   categorial-links)))
-                     (lex-cxn-conditional-units (first lex-cxn-subunit-blocks))
-                     (lex-cxn-contributing-units (second lex-cxn-subunit-blocks))
-                     (initial-cxn-score (get-configuration agent :initial-cxn-score))
-                     (interaction (current-interaction (experiment agent)))
-                     (interaction-nr (interaction-number interaction))
-                     (item-based-cxn (or existing-item-based-cxn
-                                         (second
-                                          (multiple-value-list
-                                           (eval
-                                            `(def-fcg-cxn
-                                              ,cxn-name-item-based-cxn
-                                              ((?item-based-unit
-                                                (syn-cat (phrase-type item-based))
-                                                (subunits ,subunit-names))
-                                               ,@lex-cxn-contributing-units
-                                               <-
-                                               (?item-based-unit
-                                                (HASH meaning ,non-overlapping-meaning)
-                                                --
-                                                (HASH form ,non-overlapping-form))
-                                               ,@lex-cxn-conditional-units)
-                                              :attributes (:cxn-type item-based
-                                                           :repair lex->item
-                                                           :score ,initial-cxn-score
-                                                           :string ,(form-predicates->hash-string non-overlapping-form)
-                                                           :meaning ,(meaning-predicates->hash-meaning non-overlapping-meaning)
-                                                           :added-at ,interaction-nr)
-                                              :cxn-inventory ,(copy-object cxn-inventory)
-                                              :cxn-set non-holophrase)))))))
+             (composer-strategy (get-configuration agent :composer-strategy)))
+        (notify irl::chunk-composer-started (mkstr utterance))
+        (let ((composer-solution (compose-program agent
+                                                  utterance
+                                                  :partial-program partial-program)))
+          (if composer-solution
+            (let* ((new-irl-program (append (bind-statements composer-solution)
+                                            (irl-program (chunk composer-solution))))
+                   (sorted-lex-cxns (sort-cxns-by-form-string applied-lex-cxns
+                                                              (remove-spurious-spaces (remove-punctuation utterance))))
+                   (var-form (form-constraints-with-variables utterance
+                                                              (get-configuration
+                                                               cxn-inventory
+                                                               :de-render-mode)))
+                   (subunit-names-and-non-overlapping-form (multiple-value-list
+                                                            (diff-non-overlapping-form var-form sorted-lex-cxns)))
+                   (subunit-names (first subunit-names-and-non-overlapping-form))
+                   (non-overlapping-form (second subunit-names-and-non-overlapping-form))
+                   (args-and-non-overlapping-meaning (multiple-value-list
+                                                      (diff-non-overlapping-meaning new-irl-program sorted-lex-cxns)))
+                   (args (first args-and-non-overlapping-meaning))
+                   (non-overlapping-meaning (second args-and-non-overlapping-meaning)))
+              (if (length= subunit-names args) ;; !!!
+                (let* ((cxn-name-item-based-cxn (make-const (make-cxn-name non-overlapping-form cxn-inventory)))
+                       (rendered-cxn-name-list (make-cxn-placeholder-name non-overlapping-form cxn-inventory))
+                       (placeholder-list (extract-placeholder-var-list rendered-cxn-name-list))
+                       (existing-item-based-cxn (find-cxn-by-type-form-and-meaning 'item-based
+                                                                                   non-overlapping-form
+                                                                                   non-overlapping-meaning
+                                                                                   cxn-inventory))
+                       (categorial-links (if existing-item-based-cxn
+                                           (mapcar #'cons (mapcar #'lex-class-cxn sorted-lex-cxns)
+                                                   (get-all-unit-lex-classes existing-item-based-cxn))
+                                           (create-categorial-network-links sorted-lex-cxns
+                                                                            (format nil "~{~a~^-~}" rendered-cxn-name-list)
+                                                                            placeholder-list
+                                                                            :item-based-numeric-tail t)))
+                       (lex-cxn-subunit-blocks (multiple-value-list (subunit-blocks-for-lex-cxns
+                                                                     sorted-lex-cxns
+                                                                     subunit-names
+                                                                     args
+                                                                     categorial-links)))
+                       (lex-cxn-conditional-units (first lex-cxn-subunit-blocks))
+                       (lex-cxn-contributing-units (second lex-cxn-subunit-blocks))
+                       (initial-cxn-score (get-configuration agent :initial-cxn-score))
+                       (interaction (current-interaction (experiment agent)))
+                       (interaction-nr (interaction-number interaction))
+                       (item-based-cxn (or existing-item-based-cxn
+                                           (second
+                                            (multiple-value-list
+                                             (eval
+                                              `(def-fcg-cxn
+                                                ,cxn-name-item-based-cxn
+                                                ((?item-based-unit
+                                                  (syn-cat (phrase-type item-based))
+                                                  (subunits ,subunit-names))
+                                                 ,@lex-cxn-contributing-units
+                                                 <-
+                                                 (?item-based-unit
+                                                  (HASH meaning ,non-overlapping-meaning)
+                                                  --
+                                                  (HASH form ,non-overlapping-form))
+                                                 ,@lex-cxn-conditional-units)
+                                                :attributes (:cxn-type item-based
+                                                             :repair lex->item
+                                                             :score ,initial-cxn-score
+                                                             :string ,(form-predicates->hash-string non-overlapping-form)
+                                                             :meaning ,(meaning-predicates->hash-meaning non-overlapping-meaning)
+                                                             :added-at ,interaction-nr)
+                                                :cxn-inventory ,(copy-object cxn-inventory)
+                                                :cxn-set non-holophrase)))))))
                 ;(add-composer-chunk agent non-overlapping-meaning)
-                (set-data interaction :applied-repair 'lexical->item-based)
-                ;; returns 
-                ;; 1. existing cxns to apply
-                ;; 2. new cxns to apply
-                ;; 3. other new cxns
-                ;; 4. categorial links
-                (if existing-item-based-cxn
-                  (list (cons item-based-cxn applied-lex-cxns) nil nil categorial-links)
-                  (list applied-lex-cxns (list item-based-cxn) nil categorial-links)))
-              (progn (push 'fcg::repair-failed (statuses node)) nil)))
-          (progn (push 'fcg::repair-failed (statuses node)) nil))))))
+                  (set-data interaction :applied-repair 'lexical->item-based)
+                  ;; returns
+                  ;; 1. existing cxns to apply
+                  ;; 2. new cxns to apply
+                  ;; 3. other new cxns
+                  ;; 4. categorial links
+                  (if existing-item-based-cxn
+                    (list (cons item-based-cxn applied-lex-cxns) nil nil categorial-links)
+                    (list applied-lex-cxns (list item-based-cxn) nil categorial-links)))
+                (progn (push 'fcg::repair-failed (statuses node)) nil)))
+            (progn (push 'fcg::repair-failed (statuses node)) nil)))))))
