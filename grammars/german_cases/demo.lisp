baaf88f4673e2bea0fa819bc7fda87e8a82a9f44
Veronica Juliana Schmalz <veronicajuliana.schmalz@kuleuven.be>
2023-05-19 11:28:19 +0200

    demo with extra examples

--- grammars/german_cases/demo.lisp
+++ grammars/german_cases/demo.lisp
@@ -50,9 +50,160 @@
                                              (arg2 ?v ?w)
                                              (topicalized ?d)) :cxn-inventory cxn-inventory)))
 
-
-
 (test-substitution-repair-comprehension-german)
+
+
+
+(defun test-dative-substitution-repair-comprehension-german ()
+  (let* ((experiment (set-up-cxn-inventory-and-repairs-german))
+         (cxn-inventory (grammar (first (agents experiment)))))
+    (comprehend "Der Doktor verkauft dem Clown das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d)))
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor verkauft der Frau das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (woman ?w)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?w)
+                                             (topicalized ?d))))))
+
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor verkauft der Kinderen das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (children ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d))))))
+
+    
+    (formulate '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (woman ?w)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?w)
+                                             (topicalized ?d)) :cxn-inventory cxn-inventory)))
+
+(test-dative-substitution-repair-comprehension-german)
+
+(defun test-object-substitution-repair-comprehension-german ()
+  (let* ((experiment (set-up-cxn-inventory-and-repairs-german))
+         (cxn-inventory (grammar (first (agents experiment)))))
+    (comprehend "Der Doktor verkauft dem Clown das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d)))
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor verkauft dem Clown den Stift"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (pen ?p)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?p)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d))))))
+
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor verkauft dem Clown die Blumen"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (flowers ?f)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?f)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d))))))
+    (formulate '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d)) :cxn-inventory cxn-inventory)))
+
+(test-object-substitution-repair-comprehension-german)
+
+
+(defun test-verb-substitution-repair-comprehension-german ()
+  (let* ((experiment (set-up-cxn-inventory-and-repairs-german))
+         (cxn-inventory (grammar (first (agents experiment)))))
+    (comprehend "Der Doktor verkauft dem Clown das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d)))
+    
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor gibt dem Clown das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((geben-01 ?g)
+                                             (doctor ?d)
+                                             (man ?m)
+                                             (book ?b)
+                                             (arg0 ?g ?d)
+                                             (arg1 ?g ?b)
+                                             (arg2 ?g ?m)
+                                             (topicalized ?d))))))
+
+    (test-repair-status 'holistic->item-based--substitution
+                        (second (multiple-value-list
+                                 (comprehend "Der Doktor zeigt dem Clown das Buch"
+                    :cxn-inventory cxn-inventory
+                    :gold-standard-meaning '((zeigen-01 ?z)
+                                             (doctor ?d)
+                                             (man ?m)
+                                             (book ?b)
+                                             (arg0 ?z ?d)
+                                             (arg1 ?z ?b)
+                                             (arg2 ?z ?m)
+                                             (topicalized ?d))))))
+    (formulate '((verkaufen-01 ?v)
+                                             (doctor ?d)
+                                             (clown ?c)
+                                             (book ?b)
+                                             (arg0 ?v ?d)
+                                             (arg1 ?v ?b)
+                                             (arg2 ?v ?c)
+                                             (topicalized ?d)) :cxn-inventory cxn-inventory)))
+
+(test-verb-substitution-repair-comprehension-german)
 
 
 
