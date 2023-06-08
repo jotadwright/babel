6eac393fe79f741f2a5ce14bea6c0869000a1fe4
jerome <jerome@ai.vub.ac.be>
2023-06-08 11:16:21 +0200

    fix: abstractions for gaussians with replay buffer and welford

--- experiments/concept-emergence2/distribution/welford.lisp
+++ experiments/concept-emergence2/distribution/welford.lisp
@@ -3,37 +3,36 @@
 ;; ---------------------------------------------
 ;; + Gaussian using welford's online algorithm +
 ;; ---------------------------------------------
+
 (defclass gaussian-welford (gaussian)
   ((M2
-    :initarg :M2 :accessor M2 :initform nil :type number)
-   (nr-of-samples
-    :initarg :nr-of-samples :accessor nr-of-samples :initform nil :type number))
+    :initarg :M2 :accessor M2 :initform nil :type number))
   (:documentation "Gaussian distribution using Welford's online algorithm"))
 
 ;; Constructor
-(defmethod make-distribution (agent exemplar (mode (eql :gaussian-welford)))
+(defmethod make-distribution (agent observation (mode (eql :gaussian-welford)))
   (let* ((M2 (get-configuration agent :M2))
          (nr-of-samples 1)
          (st-dev (sqrt (/ M2 nr-of-samples)))
-         (mean exemplar)
+         (mean observation)
          (history (list (list
                    (interaction-number (current-interaction (experiment agent)))
-                   exemplar
+                   observation
                    mean
                    st-dev))))
     (make-instance 'gaussian-welford
                    :mean mean
                    :st-dev st-dev
+                   :nr-of-samples nr-of-samples
                    :history history 
-                   :M2 0
-                   :nr-of-samples nr-of-samples)))
+                   :M2 M2)))
 
 ;; Update
 (defmethod update-distribution ((new-observation number)
                                 (distribution gaussian-welford))
-  ;; Step 1: Increment nr-of-samples
+  ;; Step 1: increment nr-of-samples
   (incf (nr-of-samples distribution))
-  ;; Step 2: Update
+  ;; Step 2: update using welford's algorithm
   (let* ((delta-1 (- new-observation (mean distribution)))
          (new-mean (+ (mean distribution) (/ delta-1 (nr-of-samples distribution))))
          (delta-2 (- new-observation new-mean))
@@ -42,20 +41,6 @@
           (st-dev distribution) (sqrt (/ new-M2 (nr-of-samples distribution)))
           (M2 distribution) new-M2)))
 
-;; ------------------------------
-;; + Updating prototype history +
-;; ------------------------------
-(defmethod update-prototype-history ((interaction-number number)
-                                     (new-observation number)
-                                     (distribution gaussian-welford)
-                                     &key &allow-other-keys)
-  "Update the distribution history."
-  (setf (history distribution) (cons (list interaction-number
-                                           new-observation
-                                           (mean distribution)
-                                           (st-dev distribution))
-                                     (history distribution))))
-
 ;; --------------------
 ;; + Helper functions +
 ;; --------------------
@@ -63,6 +48,6 @@
   (make-instance 'gaussian-welford
                  :mean (copy-object (mean distribution))
                  :st-dev (copy-object (st-dev distribution))
-                 :history (copy-object (history distribution))
                  :nr-of-samples (copy-object (nr-of-samples distribution))
+                 :history (copy-object (history distribution))
                  :M2 (copy-object (M2 distribution))))
