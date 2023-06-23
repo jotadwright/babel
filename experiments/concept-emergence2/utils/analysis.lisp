a43119a64efac1c456827ad28b83d5e6210ea1d7
jerome <jerome@ai.vub.ac.be>
2023-06-23 16:30:16 +0200

    fix: grid search utils

---
+++ experiments/concept-emergence2/utils/analysis.lisp
@@ -0,0 +1,150 @@
+(in-package :cle)
+
+;; -------------------
+;; + Generate Graphs +
+;; -------------------
+(defun get-statistics (base-dir raw-parameters filters)
+  "Helper function to find experiment names with particular filters." 
+  (let* ((exp-dir-path (asdf:system-relative-pathname "ecl" (format nil "logging/~a/experiments/" base-dir)))
+         (all-configs (sort (get-configurations exp-dir-path) (lambda (x y) (< (first x) (first y)))))
+         (configs (filter-experiments all-configs filters)) ; (loop for (index . config) in (filter-experiments all-configs filters) collect config))
+         (exp-names (loop for (index . config) in configs collect (downcase (assqv :EXPERIMENT-NAME config))))
+         (captions (generate-captions configs raw-parameters filters))
+         (title (generate-title base-dir filters)))
+    (list (length exp-names) title exp-names captions)))
+
+(defun graph-batch-experiments (base-dir raw-parameters filters &key (plot :all) (y-max 100) (y-min 0))
+  "Plot a batch of experiments."
+  (let* ((exp-dir-path (asdf:system-relative-pathname "ecl" (format nil "logging/~a/experiments/" base-dir)))
+         (all-configs (sort (get-configurations exp-dir-path) (lambda (x y) (< (first x) (first y)))))
+         (configs (filter-experiments all-configs filters))
+         (exp-names (loop for (index . config) in configs collect (downcase (assqv :EXPERIMENT-NAME config))))
+         (captions (generate-captions configs raw-parameters filters))
+         (title (generate-title base-dir filters)))
+    (when (or (eq plot :all) (eq plot :communicative-success))
+      (create-graph-comparing-strategies
+       :base-dir base-dir
+       :title title
+       :experiment-names exp-names
+       :measure-name "communicative-success"
+       :y1-label "Communicative success"
+       :y-min y-min
+       :y-max 1
+       :average-windows 200
+       :plot-file-name (format nil "~{~a~^-~}" (list title "comm-success"))
+       :captions captions
+       ))
+    (when (or (eq plot :all) (eq plot :lexicon-coherence))
+      (create-graph-comparing-strategies
+       :base-dir base-dir
+       :title title
+       :experiment-names exp-names
+       :measure-name "lexicon-coherence"
+       :y1-label "Lexicon coherence"
+       :y-min y-min
+       :y-max 1
+       :average-windows 500
+       :plot-file-name (format nil "~{~a~^-~}" (list title "lex-coherence"))
+       :captions captions
+       ))
+    (when (or (eq plot :all) (eq plot :lexicon-size))
+      (create-graph-comparing-strategies
+       :base-dir base-dir
+       :title title
+       :experiment-names exp-names
+       :measure-name "lexicon-size"
+       :y1-label "lexicon size"
+       :y-max y-max
+       :average-windows 100
+       :plot-file-name (format nil "~{~a~^-~}" (list title "lex-size"))
+       :captions captions
+       ))
+    ))
+
+(defun get-configurations (base-exp-dir)
+  "Returns a list of experiments by walking the directory."
+  (loop for experiment-dir in (uiop:subdirectories base-exp-dir)
+        for fpath = (merge-pathnames (make-pathname :name "experiment-configurations" :type "lisp")
+                                     experiment-dir)
+        for config = (with-open-file (stream fpath :direction :input) (read stream))
+        for exp-index = (parse-integer (first (last (split-sequence:split-sequence #\- (assqv :EXPERIMENT-NAME config)))))
+        collect (cons exp-index config)))
+
+(defun member-nested (el l)
+  "Whether el is a member of l, el can be atom or cons, l can be list of atoms or not"
+  (cond
+   ((null l) nil)
+   ((equal el (car l)) t)
+   ((consp (car l)) (or (member-nested el (car l))
+                        (member-nested el (cdr l))))
+   (t (member-nested el (cdr l)))))
+
+(defun config-has-params-p (config filters)
+  "Checks if a config has (exactly) a set of given parameters (the filters)."
+  (loop for (name . vals) in filters
+        always (member-nested (assqv name config) vals)))
+
+(defun filter-experiments (configurations &optional filter-params)
+  "Generate a set of experiments. Specify which parameters are variable
+   and what their possible values can be. Optionally specify an a-list
+   of shared configurations."
+  (if filter-params
+    (loop for (index . config) in configurations
+          if (config-has-params-p config filter-params)
+            collect (cons index config))
+    configurations))
+                                                                                                
+(defun generate-title (base-dir filters)
+  (format nil "~a = ~{~a~^ ~}" base-dir (loop for (param-name . param-values) in filters
+                                              for name-string = (mkstr param-name)
+                                              for str-length = (length name-string)
+                                              for value-string = (format nil "[~{~a~^ ~}]"
+                                                                         (loop for param-value in param-values
+                                                                               for raw = (cond ((typep param-value 'ratio) (float param-value))
+                                                                                               ((typep param-value 'keyword) (subseq (mkstr param-value) 0 (if (>= (length (mkstr param-value)) 9)
+                                                                                                                                                             9
+                                                                                                                                                             (length (mkstr param-value)))))
+                                                                                               (t param-value))
+                                                                               collect (mkstr raw)))
+                                              if (length= param-values 1)
+                                                collect (format nil "~a-~a"
+                                                                (subseq name-string 0 (if (>= str-length 3)
+                                                                                        3
+                                                                                        str-length))
+                                                                value-string))))
+
+(defun generate-captions (configurations raw-parameters filters)
+  "Generate a set of experiments. Specify which parameters are variable
+   and what their possible values can be. Optionally specify an a-list
+   of shared configurations."
+  (loop with variable-params = (loop for (name . val) in raw-parameters collect name)
+        for (index . config) in configurations
+        
+        for caption = (format nil "E~a ~{~a~^ ~}"
+                              (mkstr index)
+                              (loop for (param-name . param-value) in config
+                                    for name-string = (mkstr param-name)
+                                    for name-string-length = (length name-string)
+                                    for value-string = (mkstr param-value)
+                                    for value-string-length = (length value-string)
+                                    if (or
+                                        (and (member param-name variable-params) (> (length (assqv param-name filters)) 1))
+                                        (and (not (assq param-name filters)) (member param-name variable-params)))
+                                      collect (format nil "~a:~a"
+                                                      (subseq name-string 0 (if (>= name-string-length 3)
+                                                                              3
+                                                                              name-string-length))
+                                                      (subseq value-string 0 (if (>= value-string-length 9)
+                                                                               9
+                                                                               value-string-length)))))
+        collect caption))
+
+#|(defun generate-configurations (parameters)
+  "Generate a set of experiments. Specify which parameters are variable
+   and what their possible values can be. Optionally specify an a-list
+   of shared configurations."
+  (let* ((pairs (loop for (key . values) in parameters
+                      collect (loop for value in values
+                                    collect (cons key value))))
+         (configurations (apply #'combinations pairs)))
+    configurations))|#
---
+++ experiments/concept-emergence2/utils/graph.lisp
@@ -0,0 +1,48 @@
+(in-package :cle)
+
+;; ----------------------------------------
+;; + Graph Creation and parallel batching +
+;; ----------------------------------------
+
+(defun create-graph-for-single-strategy (exp-dir experiment-name measure-names
+                                         &rest evo-plot-keyword-args)
+  ;; take some arguments, but pass along the rest to raw-files->evo-plot
+  (format t "~%Creating graph for experiment ~a with measures ~a" experiment-name measure-names)
+  (let* ((raw-file-paths
+          (loop for measure-name in measure-names
+                collect `("experiments" "concept-emergence2" "logging" ,exp-dir ,experiment-name ,measure-name)))
+         (default-plot-file-name
+          (reduce #'(lambda (str1 str2) (string-append str1 "+" str2)) 
+                  raw-file-paths  :key #'(lambda (path) (first (last path)))))
+         (plot-file-name
+          (when (find :plot-file-name evo-plot-keyword-args)
+            (nth (1+ (position :plot-file-name evo-plot-keyword-args)) evo-plot-keyword-args))))
+    (apply #'raw-files->evo-plot
+           (append `(:raw-file-paths ,raw-file-paths
+                     :plot-directory ("experiments" "concept-emergence2" "logging" ,exp-dir ,experiment-name)
+                     :plot-file-name ,(if plot-file-name plot-file-name default-plot-file-name))
+                   evo-plot-keyword-args)))
+  (format t "~%Graphs have been created."))
+
+(defun create-graph-comparing-strategies (&key title base-dir experiment-names measure-name (y-max 1) (y-min 0) xlabel y1-label y2-label captions average-windows plot-file-name)
+  ;; This function allows you to compare a given measure accross different
+  ;; experiments, e.g. comparing lexicon size
+  (format t "~%Creating graph for experiments ~a with measure ~a" experiment-names measure-name)
+  (raw-files->evo-plot
+    :title title
+    :raw-file-paths
+    (loop for experiment-name in experiment-names
+          collect `("experiments" "concept-emergence2" "logging" ,base-dir ,experiment-name ,measure-name))
+    :average-windows average-windows
+    :captions captions
+    :plot-directory `("experiments" "concept-emergence2" "logging" ,base-dir "plots")
+    :plot-file-name plot-file-name
+    :error-bars '(:stdev)
+    :error-bar-modes '(:lines)
+    ;:series-numbers '(0 1)
+    :y1-min y-min
+    :y1-max y-max
+    :x-label (if xlabel xlabel "Number of Games")
+    :y1-label (when y1-label y1-label)
+    :y2-label (when y2-label y2-label))
+  (format t "~%Graphs have been created"))
