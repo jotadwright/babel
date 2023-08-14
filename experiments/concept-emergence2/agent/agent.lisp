0004686d02990b02998af31d4a44d8ca30113b22
jerome <jerome@ai.vub.ac.be>
2023-08-14 11:43:47 +0200

    fix: directly access experiment config instead of through indirection

--- experiments/concept-emergence2/agent/agent.lisp
+++ experiments/concept-emergence2/agent/agent.lisp
@@ -50,8 +50,8 @@
    The sensor-noise term is a fixed shift (in either direction).
    The observation noise term is different for every observation."
   (let ((raw-observation-val (get-object-val object attr))
-        (sensor-noise (noise-in-sensor agent attr (get-configuration agent :sensor-noise)))
-        (observation-noise (noise-in-observation agent attr (get-configuration agent :observation-noise))))
+        (sensor-noise (noise-in-sensor agent attr (get-configuration (experiment agent) :sensor-noise)))
+        (observation-noise (noise-in-observation agent attr (get-configuration (experiment agent) :observation-noise))))
     (if raw-observation-val
       (+ raw-observation-val sensor-noise observation-noise)
       nil)))
