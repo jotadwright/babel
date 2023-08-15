c258222ba119a1e469520e43adc670da3aab42d6
jerome <jerome@ai.vub.ac.be>
2023-08-15 23:56:15 +0200

    fix: add series and interactions as possible arg + updated hydra script

--- experiments/concept-emergence2/hydra/run.sh
+++ experiments/concept-emergence2/hydra/run.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
+#SBATCH --job-name=mid_august
+#SBATCH --error=/user/brussel/101/vsc10156/concept-emergence2/hydra/logs/mid_august_%a_e.txt
+#SBATCH --output=/user/brussel/101/vsc10156/concept-emergence2/hydra/logs/mid_august_%a_o.txt
 #SBATCH --time=120:00:00
 #SBATCH	--ntasks=1
-#SBATCH --job-name=mid_august
-#SBATCH --error $VSC_HOME/scratch/Projects/babel/experiments/concept-emergence2/logging/hydra/logs/mid_august_%a.err
-#SBATCH --output $VSC_HOME/scratch/Projects/babel/experiments/concept-emergence2/logging/hydra/logs/mid_august_%a.log
-#SBATCH --array=0-24
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=10G
+#SBATCH --array=1-24
 
 # move to dir
-cd $VSC_HOME/scratch/Projects/babel/experiments/concept-emergence2/hydra/
+cd $VSC_HOME/concept-emergence2/hydra/
 
 # load atools
 module purge
 module load atools/1.5.1-GCCcore-11.2.0
 
-# start logging
-alog --state start
-
 # read input data from csv
-source <(aenv --data $VSC_HOME/scratch/Projects/babel/experiments/concept-emergence2/hydra/mid-august.csv --sniff 4096)
+source <(aenv --data $VSC_HOME/concept-emergence2/hydra/mid_august.csv --sniff 4096)
 
 # load sbcl
 module purge
 module load SBCL/2.2.1-GCCcore-10.3.0 
 
 # run script
-sbcl --dynamic-space-size 8000 --load run.lisp \
+sbcl --dynamic-space-size 16000 --load run.lisp \
     exp-name $exp_name \
+    nr-of-series $nr_of_series \
+    nr-of-interactions $nr_of_interactions \
     population-size $population_size \
     dataset $dataset \
     dataset-split $dataset_split \
@@ -51,8 +52,3 @@ sbcl --dynamic-space-size 8000 --load run.lisp \
     switch-condition $switch_condition \
     switch-conditions-after-n-interactions $switch_conditions_after_n_interactions \
     stage-parameters $stage_parameters
-
-# stop logging
-module purge
-module load atools/1.5.1-GCCcore-11.2.0
-alog --state end
