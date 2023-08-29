7a8fc1f3b2e25116c577389287260d368f2d2ff9
jnevens <jnevens@ai.vub.ac.be>
2023-08-29 15:14:15 +0200

    restore store-monitor class

--- systems/pattern-finding/experiment/run-helpers.lisp
+++ systems/pattern-finding/experiment/run-helpers.lisp
@@ -71,3 +71,23 @@
     :y2-label (when y2-label y2-label)
     :open open)
   (format t "~%Graphs have been created"))
+
+;; MONITOR UTILS
+;; -------------
+
+(in-package :monitors)
+
+(export '(store-monitor))
+
+;;;; store-monitor
+(defclass store-monitor (monitor)
+  ((file-name :documentation "The file name of the file to write"
+              :initarg :file-name
+              :reader file-name))
+  (:documentation "Monitor that stores data using cl-store"))
+
+(defmethod initialize-instance :around ((monitor store-monitor)
+					&key &allow-other-keys)
+  (setf (error-occured-during-initialization monitor) t)
+  (setf (error-occured-during-initialization monitor) nil)
+  (call-next-method))
