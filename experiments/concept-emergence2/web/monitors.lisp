67f1d2b8215ea2f694f4f3af325fa9fa399348e9
jerome <jerome@ai.vub.ac.be>
2023-09-01 18:07:55 +0200

    fix: store every series such that tests also have confidence intervals

--- experiments/concept-emergence2/web/monitors.lisp
+++ experiments/concept-emergence2/web/monitors.lisp
@@ -120,15 +120,14 @@
 
 (define-monitor export-experiment-store)
 (define-event-handler (export-experiment-store run-series-finished)
-  (when (= (series-number experiment) 1)
-    (let* ((experiment-name (get-configuration experiment :experiment-name))
-           (output-dir (get-configuration experiment :output-dir))
-           (path (babel-pathname
-                  :directory `("experiments" "concept-emergence2" "logging" ,(downcase output-dir) ,(downcase experiment-name) "stores")
-                  :name (list-of-strings->string (list (write-to-string (series-number experiment)) "history") :separator "-") :type "store")))
-      (setf (world experiment) nil)
-      (ensure-directories-exist path)
-      (cl-store:store experiment path))))
+  (let* ((experiment-name (get-configuration experiment :experiment-name))
+          (output-dir (get-configuration experiment :output-dir))
+          (path (babel-pathname
+                :directory `("experiments" "concept-emergence2" "logging" ,(downcase output-dir) ,(downcase experiment-name) "stores")
+                :name (list-of-strings->string (list (write-to-string (series-number experiment)) "history") :separator "-") :type "store")))
+    (setf (world experiment) nil)
+    (ensure-directories-exist path)
+    (cl-store:store experiment path)))
 
 #|;; ---------------------
 ;; + Invention rate +
