7fbed6b890d6bfd909dc539c7807c62408848227
jerome <jerome@ai.vub.ac.be>
2023-10-06 11:16:54 +0200

    fix: minor

--- experiments/concept-emergence2/batch/run.lisp
+++ experiments/concept-emergence2/batch/run.lisp
@@ -34,7 +34,7 @@
     ;; --------------
     (:dot-interval . 5000)
     ;(:record-every-x-interactions . 100)
-    (:usage-table-window . 5000)
+    (:usage-table-window . 1000)
     (:save-distribution-history . nil)
     (:interacting-agents-strategy . :standard)
     (:initial-cxn-entrenchement . 0.5)
