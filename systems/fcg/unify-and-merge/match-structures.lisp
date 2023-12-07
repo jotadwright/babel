907da8ce8d5a406179b8aa08a993c4e4bc7afb44
Alexane Jouglar <73078605+AlexaneJ@users.noreply.github.com>
2023-12-07 21:34:33 +0100

    fix: calculate-unmatched-sequences (still one test to look at)

--- systems/fcg/unify-and-merge/match-structures.lisp
+++ systems/fcg/unify-and-merge/match-structures.lisp
@@ -434,15 +434,15 @@
       (setf (cdr (nthcdr (1- index) lst)) (cddr (nthcdr (1- index) lst))))
   lst)
 
-(defun has-jump-in-interval (numbers)
+#|(defun has-jump-in-interval (numbers)
   "Check if there is a jump in the interval between consecutive numbers."
   (let* ((differences (mapcar #'(lambda (x y) (abs (- x y)))
                               numbers
                               (cdr numbers)))
          (max-difference (apply #'max differences)))
-    (> max-difference 1)))
+    (> max-difference 1)))|#
 
-(defun find-continuous-sublists (list-of-numbers)
+#|(defun find-continuous-sublists (list-of-numbers)
   "Find continuous sublists in the interval between consecutive numbers."
   (let ((sublists '())
         (sublist '()))
@@ -457,9 +457,9 @@
              (pop list-of-numbers)
              (when (and sublist (= (length list-of-numbers) 0))
                (pushend sublist sublists)))
-    sublists))
+    sublists))|#
 
-(defun calculate-unmatched-intervals (matched-intervals root-intervals)
+#|(defun calculate-unmatched-intervals (matched-intervals root-intervals)
   "Calculates which spans in the root's form feature have not been matched by the current cxn application."
   (let* ((root-sequence-indices (calculate-index-list (sort root-intervals #'< :key #'first)))
          (cxn-sequence-indices (calculate-index-list (sort matched-intervals #'< :key #'first)))
@@ -486,7 +486,87 @@
           do (setf list-of-endpoints (list (first new-sequence-index) (first (last new-sequence-index))))
              (pushend list-of-endpoints new-indices))
     (setq new-indices (sort new-indices #'(lambda (x y) (< (first x) (first y)))))
-    new-indices))
+    new-indices))|#
+
+
+(defun expand-interval (interval)
+  "from a given interval expands it into a list of adjacent cons cells"
+  (let ((interval-cons-cells '()))
+    (loop for i from (first interval) to (- (first (last interval)) 1)
+          do (pushend (cons i (+ i 1)) interval-cons-cells))
+    interval-cons-cells))
+
+;; (expand-interval '(0 30))
+;; (expand-interval '(12 25))
+
+(defun collapse-intervals (interval-cons-cells)
+  "from a list of cons-cells, renders a list or multiple lists of adjacent cons cells"
+  (let ((intervals '())
+        (provisory-cons-cells-list '())
+        (interval '()))
+    (loop for i in interval-cons-cells
+          for pos-i = (position i interval-cons-cells) ;; position of the current element
+          for i+1 = nil ;; the next element, for now it is nil
+          do (pushend i provisory-cons-cells-list)
+             (if (not (eq i (first (last interval-cons-cells)))) ;; when we are not considering the last element of the list
+               (progn
+                 (setf i+1 (nth (+ pos-i 1) interval-cons-cells)) ;; we set the next-element
+                 (when (not (= (cdr i) (car i+1))) ;; if the cdr of the current element is not equal to the car of the next element
+                   ;; then we make an interval out of the previous considered cons cells that are stored in the provisory-cons-cells-list
+                   (pushend (car (first provisory-cons-cells-list)) interval) ;; left elem of the interval
+                   (pushend (cdr (first (last provisory-cons-cells-list))) interval) ;; right elem of the interval
+                   (pushend interval intervals) ;; we push the interval to the list of intervals
+                   (setf interval nil) ;; we set the interval to nil
+                   (setf provisory-cons-cells-list nil))) ;; we also set the provisory-cons-cells-list to nil
+               ;; and for the last element of the list:
+               (progn
+                 (pushend i provisory-cons-cells-list)
+                 (pushend (car (first provisory-cons-cells-list)) interval) ;; left elem of the interval
+                 (pushend (cdr (first (last provisory-cons-cells-list))) interval) ;; right elem of the interval
+                 (pushend interval intervals)))) ;; we push the interval to the list of intervals
+    intervals))
+
+;; (collapse-intervals '((0 . 1) (1 . 2) (2 . 3) (3 . 4) (4 . 5) (5 . 6) (6 . 7) (7 . 8) (8 . 9) (9 . 10) (10 . 11) (11 . 12) (25 . 26) (26 . 27) (27 . 28) (28 . 29) (29 . 30)))
+;; expected: '((0 12) (25 30))
+
+;; (collapse-intervals '((12 . 13) (24 . 25)))
+;; expected: '((12 13) (24 25))
+
+;; (collapse-intervals '((7 . 8) (9 . 10)))
+
+;; (defparameter my-list-of-cons '((0 . 1) (1 . 2) (2 . 3) (3 . 4) (4 . 5) (5 . 6) (6 . 7) (7 . 8) (8 . 9) (9 . 10) (10 . 11) (11 . 12) (25 . 26) (26 . 27) (27 . 28) (28 . 29) (29 . 30)))
+
+(defun calculate-unmatched-intervals (matched-intervals root-intervals)
+  (let ((final-intervals '()))
+    (loop for root-interval in root-intervals
+          for expanded-interval = (expand-interval root-interval)
+          for car-equality-position = nil
+          for cdr-equality-position = nil
+          do (loop for matched-interval in matched-intervals ;; we set car-equality-position and cdr-equality-position
+                   do (loop for i in expanded-interval
+                            do (when (equal (car i) (car matched-interval))
+                                 (setf car-equality-position (position i expanded-interval)))
+                               (when (equal (cdr i) (first (cdr matched-interval)))
+                                 (setf cdr-equality-position (position i expanded-interval))))
+                      (if car-equality-position
+                        (progn 
+                          (let* ((excluded-positions (loop for i from car-equality-position to cdr-equality-position collect i))
+                                (excluded-items (loop for i in excluded-positions collect (nth i expanded-interval))))
+                            (loop for excluded-item in excluded-items
+                                  do (setf expanded-interval (remove excluded-item expanded-interval)))))))
+             (when expanded-interval 
+               (setf collapsed-intervals (collapse-intervals expanded-interval))
+               (loop for collapsed-interval in collapsed-intervals
+                     do (pushend collapsed-interval final-intervals))))
+    final-intervals))
+                   
+;; using cons cells of the intervals
+;; (calculate-unmatched-intervals '((8 9)) '((3 4) (7 10) (17 18)))
+;; expected: '((3 4) (7 8) (9 10) (17 18))
+
+
+;; (calculate-unmatched-intervals '((29 30) (0 4)) '((0 12) (17 30)))
+;; expected: '((4 12) (17 29))
 
 ;; test: 
 ;; (recompute-root-sequence-features-based-on-bindings '((SEQUENCE "chairm" 0 6) (SEQUENCE "n of " 7 12) (SEQUENCE " committee" 15 25)) '((#:?AIR-UNIT-790 . #:AIR-UNIT-87) (#:?TAG-42752 FORM ((SEQUENCE "air" 2 5))) (#:?LEFT-14848 . 2) (#:?RIGHT-14848 . 5)))
