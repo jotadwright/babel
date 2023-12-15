51e12883de32ac7c321bf16793c83667131a52e5
jerome <jerome@ai.vub.ac.be>
2023-12-15 16:54:52 +0100

    fix: same dot interval as jens

--- experiments/concept-emergence2/scripts/demo.lisp
+++ experiments/concept-emergence2/scripts/demo.lisp
@@ -8,7 +8,7 @@
     (make-configuration
      :entries `(
                 ;; monitoring
-                (:dot-interval . 10)
+                (:dot-interval . 100)
                 (:usage-table-window . 100)
                 (:save-distribution-history . nil)
                 ;; setup interacting agents
