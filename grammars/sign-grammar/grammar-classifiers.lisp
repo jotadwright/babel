8724983f4711f5d020f4c9323816c6b7bd0fd5af
ldevos <liesbet.devos@unamur.be>
2024-01-17 16:32:49 +0100

    implemented example for mountain + hold

--- grammars/sign-grammar/grammar-classifiers.lisp
+++ grammars/sign-grammar/grammar-classifiers.lisp
@@ -99,11 +99,96 @@
                            ))
                )))
 
+(def-fcg-cxn guadalupe_peak-1-cxn
+             ((?guadalupe_peak-1-unit
+               (meaning-args (?F))
+               (boundaries (lh-leftmost ?fs-guadalupe-1)
+                           (lh-rightmost ?fs-guadalupe-1))
+               (category guadalupe_peak-1-cat))
+               <-
+              (?guadalupe_peak-1-unit
+               (HASH meaning ((GUADALUPE_PEAK ?F)))
+               --
+               (HASH form ((left-hand-articulation ?fs-guadalupe-1 fs-guadalupe)))
+               )))
+
 ;--------------------------;
 ; item-based constructions ;
 ;--------------------------;
 
-; slot1-country-id-const-1-cxn: classifier for countries. In the form it matches on the size and shape classifier that in general is used for geographic entities when they function as ground in the semantic structure. This classifier should immediately follow a slot-filler with the right category. The classifier is maintained on the non-dominant hand. In the meaning, it matches on ((CONST ?E ?B ?F)(COUNTRYID ?F ?H)) where ?H is  the argument passed on by the slot. So this construction classifies the referent ?H as a country and retrieves its id as a constant. 
+; slot1-country-id-const-1-cxn: classifier for countries. In the form it matches on the size and shape classifier that in general is used for geographic entities when they function as ground in the semantic structure. This classifier should immediately follow a slot-filler with the right category. The classifier is maintained on the non-dominant hand. In the meaning, it matches on ((CONST ?E ?B ?F)(COUNTRYID ?F ?H)) where ?H is  the argument passed on by the slot. So this construction classifies the referent ?H as a country and retrieves its id as a constant.
+
+(def-fcg-cxn const-placeid-slot1-1-cxn
+             ((?const-placeid-slot1-1-unit
+               (subunits (?slot1-unit))
+               (meaning-args (?D ?B))
+               (category const-placeid-slot1-1-cat)
+               (spatial-agreement ?location)
+               (boundaries (lh-leftmost ?montagne-1)
+                           (lh-rightmost ?slot1-lh-right)
+                           (rh-leftmost ?montagne-2)
+                           (rh-rightmost ?montagne-2)))
+              <-
+              (?const-placeid-slot1-1-unit
+               (HASH meaning ((CONST ?D ?B ?E)(PLACEID ?E ?F)))
+               
+               --
+               (HASH form ((left-hand-articulation ?montagne-1 montagne)
+                           (location ?montagne-1 ?location)
+                           (right-hand-articulation ?montagne-2 montagne)
+                           (location ?montagne-2 ?location)
+                           (temporal-relation ?montagne-1 ?montagne-2 starts)
+                           (left-hand-articulation ?pt-1 pt)
+                           (location ?pt-1 ?location)
+                           (left-hand-articulation ?pt-2 pt)
+                           (location ?pt-2 ?location)
+                           (modification ?pt-2 down-up)
+                           (meets ?montagne-1 ?pt-1)
+                           (meets ?pt-1 ?pt-2)
+                           (temporal-relation ?pt-1 ?montagne-2 during)
+                           (temporal-relation ?pt-2 ?montagne-2 during)
+                           (temporal-relation ?slot1-lh-left ?montagne-2 during)
+                           )))
+              (?slot1-unit
+               (meaning-args (?F))
+               (category const-placeid-slot1-1-slot1-cat)
+               --
+               (boundaries (lh-leftmost ?slot1-lh-left)
+                           (lh-rightmost ?slot1-lh-right))
+               )
+              ))
+
+(def-fcg-cxn slot1-elevation-1-cxn
+             ((?slot1-elevation-1-unit
+               (subunits (?slot1-unit))
+               (meaning-args (?A ?D))
+               (category slot1-elevation-1-cat)
+               (spatial-agreement ?location)
+               (boundaries (lh-leftmost ?slot1-lh-left)
+                           (lh-rightmost ?pt-1)
+                           (rh-leftmost ?slot1-rh-left)
+                           (rh-rightmost ?slot1-rh-right)))
+              <-
+              (?slot1-elevation-1-unit
+               (HASH meaning ((ELEVATION ?D ?B ?A)))
+               
+               --
+               (HASH form ((left-hand-articulation ?pt-1 pt)
+                           (location ?pt-1 ?location)
+                           (modification ?pt-1 down-up)
+                           (meets ?slot1-lh-right ?pt-1)
+                           (temporal-relation ?pt-1 ?slot1-rh-right finishes)
+                           )))
+              (?slot1-unit
+               (meaning-args (?D ?B))
+               (category slot1-elevation-1-slot1-cat)
+               --
+               (boundaries (lh-leftmost ?slot1-lh-left)
+                           (lh-rightmost ?slot1-lh-right)
+                           (rh-leftmost ?slot1-rh-left)
+                           (rh-rightmost ?slot1-rh-right))
+               )
+              ))
 
 (def-fcg-cxn slot1-countryid-const-1-cxn
              ((?slot1-countryid-const-1-unit
@@ -130,7 +215,7 @@
                )
               (?slot1-unit
                (meaning-args (?H))
-               (category slot1-countryid-const-slot1-1-cat)
+               (category slot1-countryid-const-1-slot1-cat)
                --
                (boundaries (lh-leftmost ?slot1-lh-left)
                            (lh-rightmost ?slot1-lh-right))
@@ -138,6 +223,9 @@
               ))
 
 ;slot1-loc-state-1-cxn: classifier verb. The construction expresses existentiality (and location): namely that there exist states (inside something else, identified by the slot). In the form, it matches on the sign il-y-a which is typical for existential constructions, and the size and shape classifier that is used for geographic entities that function as figure in the semantic structure. These signs on the dominant hand occur together with a held sign on the non-dominant hand (identified by the slot). In the meaning, the construction matches on (LOC ?E ?A ?B)(STATE ?E ?A), where ?E and ?B are arguments of the slot. 
+
+
+;((ANSWER ?C ?A ?D) (LARGEST ?D ?A ?E) (STATE ?E ?A) (LOC ?E ?A ?B) (CONST ?E ?B ?F) (COUNTRYID ?F ?G) (USA ?G))","geo-prolog":"answer(A,largest(A,(state(A),loc(A,B),const(B,countryid(usa)))))
 
 (def-fcg-cxn slot1-loc-state-1-cxn
              ((?slot1-loc-state-1-unit
@@ -165,7 +253,7 @@
                )
               (?slot1-unit
                (meaning-args (?E ?B))
-               (category slot1-loc-state-slot1-1-cat)
+               (category slot1-loc-state-1-slot1-cat)
                --
                (boundaries (lh-leftmost ?slot1-lh-left)
                            (lh-rightmost ?slot1-lh-right)
@@ -196,7 +284,7 @@
                )
               (?slot1-unit
                (meaning-args (?H))
-               (category slot1-stateid-const-1-slot1-1-cat)
+               (category slot1-stateid-const-1-slot1-cat)
                --
                (boundaries (lh-leftmost ?slot1-lh-left)
                            (lh-rightmost ?slot1-lh-right))
@@ -229,7 +317,7 @@
                )
               (?slot1-unit
                (meaning-args (?H))
-               (category slot1-cityid-const-1-slot1-1-cat)
+               (category slot1-cityid-const-1-slot1-cat)
                --
                (boundaries (lh-leftmost ?slot1-lh-left)
                            (lh-rightmost ?slot1-lh-right))
@@ -260,7 +348,7 @@
                            )))
               (?slot1-unit
                (meaning-args (?E ?B))
-               (category slot1-size-answer-slot1-1-cat)
+               (category slot1-size-answer-1-slot1-cat)
                --
                (boundaries (lh-leftmost ?slot1-lh-left)
                            (lh-rightmost ?slot1-lh-right)
@@ -274,20 +362,26 @@
                   alaska-1-cat
                   new_york-1-cat
                   slot1-countryid-const-1-cat
-                  slot1-countryid-const-slot1-1-cat
+                  slot1-countryid-const-1-slot1-cat
                   slot1-loc-state-1-cat
-                  slot1-loc-state-slot1-1-cat
+                  slot1-loc-state-1-slot1-cat
                   slot1-stateid-const-1-cat
-                  slot1-stateid-const-1-slot1-1-cat
-                  slot1-cityid-const-1-slot1-1-cat
-                  slot1-size-answer-slot1-1-cat
-                  slot1-cityid-const-1-cat)
+                  slot1-stateid-const-1-slot1-cat
+                  slot1-cityid-const-1-slot1-cat
+                  slot1-size-answer-1-slot1-cat
+                  slot1-cityid-const-1-cat
+                  const-placeid-slot1-1-slot1-cat
+                  const-placeid-slot1-1-cat
+                  slot1-elevation-1-slot1-cat
+                  guadalupe_peak-1-cat)
                 *fcg-constructions*)
 
 (progn
-  (add-link 'usa-1-cat 'slot1-countryid-const-slot1-1-cat *fcg-constructions*)
-  (add-link 'slot1-countryid-const-1-cat 'slot1-loc-state-slot1-1-cat *fcg-constructions*)
-  (add-link 'alaska-1-cat 'slot1-stateid-const-1-slot1-1-cat *fcg-constructions*)
-  (add-link 'new_york-1-cat 'slot1-cityid-const-1-slot1-1-cat *fcg-constructions*)
-  (add-link 'slot1-cityid-const-1-cat 'slot1-size-answer-slot1-1-cat *fcg-constructions*)
-  (add-link 'slot1-stateid-const-1-cat 'slot1-size-answer-slot1-1-cat *fcg-constructions*))
+  (add-link 'usa-1-cat 'slot1-countryid-const-1-slot1-cat *fcg-constructions*)
+  (add-link 'slot1-countryid-const-1-cat 'slot1-loc-state-1-slot1-cat *fcg-constructions*)
+  (add-link 'alaska-1-cat 'slot1-stateid-const-1-slot1-cat *fcg-constructions*)
+  (add-link 'new_york-1-cat 'slot1-cityid-const-1-slot1-cat *fcg-constructions*)
+  (add-link 'slot1-cityid-const-1-cat 'slot1-size-answer-1-slot1-cat *fcg-constructions*)
+  (add-link 'slot1-stateid-const-1-cat 'slot1-size-answer-1-slot1-cat *fcg-constructions*)
+  (add-link 'const-placeid-slot1-1-slot1-cat 'guadalupe_peak-1-cat *fcg-constructions*)
+  (add-link 'const-placeid-slot1-1-cat ' slot1-elevation-1-slot1-cat *fcg-constructions*))
