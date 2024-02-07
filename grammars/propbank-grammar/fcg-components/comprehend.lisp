30105ec876c88b6fbecc65824ab8ebbebab3b1e0
remivantrijp <remi@csl.sony.fr>
2024-02-07 15:22:43 +0100

    - The configuration of a cxn-inventory has a new key :model, whose value must be a string that corresponds to the spacy/benepar model (e.g. "en_benepar") - de-render now checks for that configuration - learn-propbank-grammar has a new key :model for passing the model - learn-propbank-grammar shortened by getting the def-fcg-constructions out. The eval remains to not break other code

--- grammars/propbank-grammar/fcg-components/comprehend.lisp
+++ grammars/propbank-grammar/fcg-components/comprehend.lisp
@@ -36,10 +36,14 @@
             (values 'time-out 'time-out 'time-out)))
       (values meaning cip-node cip))))
 
-(defmethod comprehend ((utterance string) &key (syntactic-analysis nil) (cxn-inventory *fcg-constructions*)  (silent nil) (selected-rolesets nil) (timeout 60))
+(defmethod comprehend ((utterance string) 
+                       &key (syntactic-analysis nil) 
+                       (cxn-inventory *fcg-constructions*)  (silent nil) (selected-rolesets nil) (timeout 60))
   (let* ((syntactic-analysis (nlp-tools:get-penelope-syntactic-analysis utterance
-                                                                       :model (get-configuration cxn-inventory :language-mode)))
+                                                                       :model (or (get-configuration cxn-inventory :model)
+                                                                                  "en_benepar")))
          (initial-cfs (de-render utterance (get-configuration cxn-inventory :de-render-mode)
+                                 :model (or (get-configuration cxn-inventory :model) "en_benepar")
                                  :cxn-inventory cxn-inventory :syntactic-analysis syntactic-analysis)))
     (unless silent (monitors:notify parse-started (listify utterance) initial-cfs))
     (multiple-value-bind (meaning cip-node cip)
