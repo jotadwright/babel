4987f79af5d36f44a4c22fd8f292d01fa92c6626
jnevens <jnevens@ai.vub.ac.be>
2024-03-22 13:47:56 +0100

    updated systems/cl-pcp with latest version of repo

--- systems/cl-pcp/client-process.lisp
+++ systems/cl-pcp/client-process.lisp
@@ -30,6 +30,9 @@
 (defparameter *process-fn* nil
   "The name of the client process.")
 
+(defparameter *process-fn-kwargs* nil
+  "The keyword arguments of the client process.")
+
 (defparameter *write-fn* nil
   "The name of the client process.")
 
@@ -48,28 +51,36 @@
 (defun initialise-process (&key lock-file
                                 output-file
                                 process-fn
+                                process-fn-kwargs
                                 write-fn
                                 keep-order)
   "Initialises the cl-pcp client process by setting the relevant global variables."
   ;; Set global variables
-  (setf *lock-file* lock-file
+  (setf ;*lock-file* lock-file
         *output-file* output-file
         *process-fn* process-fn
+        *process-fn-kwargs* process-fn-kwargs
         *write-fn* write-fn
         *keep-order* keep-order)
-  (delete-file *lock-file*)
-  (format nil "Process initialised with lock-file: ~s, temporary output-file: ~s, process-fn: ~a, write-fn: ~a and keep-order: ~a."
-          lock-file output-file process-fn write-fn keep-order))
+  ;; Delete the lock file
+  (safe-delete-file lock-file)
+  (format nil "Process initialised with lock-file: ~s, temporary output-file: ~s, process-fn: ~a, process-fn-kwargs: ~a, write-fn: ~a and keep-order: ~a."
+          lock-file output-file process-fn process-fn-kwargs write-fn keep-order))
 
-(defun run (item-nr input)
+(defun run (item-nr input lock-file)
   "Runs the client process for input."
-  (let* ((processed-input (funcall *process-fn* input)))
+  (let* ((processed-input
+          (if (null *process-fn-kwargs*)
+            (funcall *process-fn* input)
+            (apply *process-fn* input *process-fn-kwargs*))))
     ;; Now write the output
     (with-open-file (stream *output-file* :direction :output :if-does-not-exist :create :if-exists :append)
       (if *keep-order*
         (format stream "~a.~a~%" item-nr (funcall *write-fn* processed-input))
         (format stream "~a~%" (funcall *write-fn* processed-input)))
+      (force-output stream)
       (finish-output stream)))
   ;; Finally, unlock the process by deleting the lock file.
-  (delete-file *lock-file*)
+  (let ((lock-file-path (parse-namestring lock-file)))
+    (safe-delete-file lock-file-path))
   (format nil "Item ~a done." item-nr))
