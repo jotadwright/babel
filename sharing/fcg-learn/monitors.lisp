f497a01a66aa31a455dbe5b45cb3499d0d6c62b4
Paul <paul@insight-labs.eu>
2024-05-27 12:44:04 +0200

    progress on monitors

--- sharing/fcg-learn/monitors.lisp
+++ sharing/fcg-learn/monitors.lisp
@@ -17,8 +17,37 @@
   (add-element `((hr :style "margin-block-end: 0px;")))
   (add-element `((h2 :style "padding: 15px; margin: 0px; background-color: #33FFA4;") ,(format nil "Routine comprehension of: &quot;~a&quot; ~@[~a~]"
                                                                                                (form speech-act)
-                               (when n (format nil "(max ~a solution~p considered)" n n)))))
+                               (when n (format nil "(max ~a solution~:p considered)" n)))))
   (add-element `((hr :style "margin-block-start: 0px;"))))
+
+(define-event-handler (trace-fcg-learning all-diagnostics-run)
+  (when problems
+    (add-element `((hr :style "margin-block-end: 0px;")))
+    (add-element `((h2 :style "padding: 15px; margin: 0px; background-color: #ff5252;") "Meta-level learning"))
+    (add-element `((hr :style "margin-block-start: 0px;")))
+    (add-element `((h4) ,(format nil "~a problem~:p diagnosed:" (length problems))))
+    (loop for problem in problems
+          do (add-element (make-html problem)))))
+
+(define-event-handler (trace-fcg-learning notify-learning-finished)
+  (when problems
+    (if fixes
+      (progn
+        (add-element `((h4) ,(if (> (length fixes) 1)
+                               (format nil "~a fixes created:" (length fixes))
+                               "1 fix created:")))
+        (loop for fix in fixes
+              do (add-element (make-html fix))))
+      (add-element `((h4) "No fixes created.")))))
+
+
+
+(define-event meta-level-learning-finished (cip construction-inventory-processor) (solution cip-node))
+
+(define-event-handler (trace-fcg-learning meta-level-learning-finished)
+  (add-element `((h4) "Appying fixes:"))
+  (add-element (make-html-fcg-light cip :solutions (succeeded-nodes cip))))
+
 
 
 (define-event-handler (trace-fcg-learning fcg-apply-w-n-solutions-started)
