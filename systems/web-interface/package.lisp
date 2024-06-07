4784e7ad2b7f28b6fc255fa19d1f7af4481c3eea
Paul <paul@insight-labs.eu>
2024-06-07 14:27:01 +0200

    overwrite function from ht-simple-ajax to accomodate for bug in lispworks concatenate

--- systems/web-interface/package.lisp
+++ systems/web-interface/package.lisp
@@ -22,3 +22,25 @@
   (:nicknames #:wi)
   (:use :common-lisp :cl-user :hunchentoot :ht-simple-ajax :utils)
   (:documentation "A package for the web interface"))
+
+;; Temporarily overwrite this function from ht-simple-ajax until concatenate bug in LW has been resolved...
+
+(in-package :ht-simple-ajax)
+
+#+lispworks
+(defun call-lisp-function (processor)
+  "This is called from hunchentoot on each ajax request. It parses the 
+   parameters from the http request, calls the lisp function and returns
+   the response."
+  (let* ((fn-name (string-trim "/" (subseq (script-name* *request*)
+                                           (length (server-uri processor)))))
+         (fn (gethash fn-name (lisp-fns processor)))
+         (args (mapcar #'cdr (get-parameters* *request*))))
+    (unless fn
+      (error "Error in call-lisp-function: no such function: ~A" fn-name))
+    
+    (setf (reply-external-format*) (reply-external-format processor))
+    (setf (content-type*) (content-type processor))
+    (no-cache)
+    (format nil "<?xml version=\"1.0\"?>
+<response xmlns='http://www.w3.org/1999/xhtml'>~a</response>" (apply fn args))))
