b3482126d2e3d98d8900e4459dfe9a8bacc97c96
Katrien Beuls <katrien@insight-labs.eu>
2024-07-05 14:51:40 +0200

    expansions for dealing with sequences, number parsing

--- applications/muhai-cookingbot/language-processing/expansions.lisp
+++ applications/muhai-cookingbot/language-processing/expansions.lisp
@@ -5,7 +5,7 @@
 (defmethod fcg-expand ((type (eql :flatten-ingredients))
                        &key value source bindings merge? cxn-inventory)
   "Flatten ingredient list."
-  (declare (ignore source))
+  (declare (ignore cxn-inventory))
   (if merge?
     (when (assoc "INGREDIENTS" bindings :key #'get-base-name :test #'string=)
       (let* ((all-ingredient-bindings (loop for (key . value) in bindings
@@ -24,7 +24,7 @@
 (defmethod fcg-expand ((type (eql :lookup-in-ontology))
                        &key value source bindings merge? cxn-inventory)
   "Returns number from string."
-  (declare (ignore merge? source))
+  (declare (ignore merge? source cxn-inventory))
   (unless (null bindings)
     (let* ((class (handler-case (find-class value)
                     (error (c)
@@ -61,11 +61,6 @@
                                             (format nil "ONTOLOGICAL-CLASS-UTTERANCE-~a" (upcase suffix))
                                             "ONTOLOGICAL-CLASS-UTTERANCE")))
                          (cdr (assoc search-for bindings-list :key #'get-base-name :test #'string=)))))
-          ;for ontological-class-from-bindings
-          ;    = (cond ((string= (get-base-name value) "ONTOLOGICAL-CLASS-UTTERANCE")
-          ;             (cdr (assoc "ONTOLOGICAL-CLASS-WORLD" bindings-list :key #'get-base-name :test #'string=)))
-          ;            ((string= (get-base-name value) "ONTOLOGICAL-CLASS-WORLD")
-          ;             (cdr (assoc "ONTOLOGICAL-CLASS-UTTERANCE" bindings-list :key #'get-base-name :test #'string=))))
               
           if (and ontological-class-from-ts
                   ontological-class-from-bindings
@@ -83,31 +78,72 @@
           collect bindings-list into new-bindings
           finally (return (values value new-bindings))))))
 
-               
-
-#|
-  (if merge?
-    (let ((ontological-class-utterance (cdr (assoc "ONTOLOGICAL-CLASS-UTTERANCE" ;;brittle!!
-                                                   bindings :key #'get-base-name :test #'string=)))
-          (ontological-class-world (cdr (assoc "ONTOLOGICAL-CLASS-WORLD"
-                                               bindings :key #'get-base-name :test #'string=))))
-      (when (and ontological-class-utterance ontological-class-world)
-        (let* ((utterance-vector (hashtable-to-vector ontological-class-utterance cxn-inventory))
-               (world-vector (hashtable-to-vector ontological-class-world cxn-inventory))
-               (similarity-score (cosine-similarity utterance-vector world-vector)))
-          (when (> similarity-score 0.9)
-            ;(set-data (blackboard cxn-inventory) :similarity-score similarity-score)
-            ;(format t "comparing ~a and ~a: ~a ~%" ontological-class-utterance ontological-class-world  (get-data (get-self) :similarity-score))
-            (values value bindings)))))
-    (values value bindings))|#
   
+(defmethod fcg-expand ((type (eql :number-sequences))
+                       &key value source bindings merge? cxn-inventory)
+  "Matches on a number in a tag by means of sequence predicates."
+  (declare (ignore cxn-inventory))
+  (if (and (eq merge? t) (eq source nil)) 
+    ;; When called in either merge or remove-special-operators, than just return the value (the pattern from the cxn).
+    (values value bindings)
+    ;; When called in matching and the source contains a predicate with an number
+    ;; return the value (so that it can be removed from the root during merge)
+    ;; and add a new binding between the variable from the value and the number
+    ;; in the source.   
+    (let ((sequence-predicates (remove-if-not #'(lambda (predicate)
+                                                  (eql (first predicate) 'sequence)) (rest value))))
+      (if sequence-predicates
+        (let ((sequence-bindings-lists
+               (match-pattern-sequence-predicates-in-source-sequence-predicates-numbers-only sequence-predicates source bindings)))
+          (if sequence-bindings-lists
+            ;; If sequence predicates could be matched, add bindings
+            (let ((new-bindings (merge-bindings-lists bindings sequence-bindings-lists)))
+              (values source (mapcar #'(lambda (b)
+                                    (extend-bindings
+                                     (second (first (remove-special-operators value bindings)))
+                                     "120" ;;retrieve value in source
+                                     b)) new-bindings)))
+            ;; Fail if sequence predicates could not be matched
+            nil))
+        ;; If there are no sequence predicates (but only e.g. precedes constraints), continue and deal with it in merge (above).
+        (values source bindings)))))
+
+
+(defun match-pattern-sequence-predicates-in-source-sequence-predicates-numbers-only (pattern-sequence-predicates source-sequence-predicates bindings)
+  "Matches two sets of sequence-predicates, returns validated bindings-lists with all options. Takes into account existing bindings."
+  (when source-sequence-predicates
+    (loop for pattern-sequence-predicate in pattern-sequence-predicates
+          collect (match-pattern-sequence-predicate-in-source-sequence-predicates-numbers-only
+                   pattern-sequence-predicate source-sequence-predicates)
+            into possible-bindings-lists-per-pattern-predicate
+          finally (return (loop for list-of-lr-pairs in (filter-valid-sequence-bindings-lists
+                                                         (apply #'cartesian-product possible-bindings-lists-per-pattern-predicate) bindings)
+                                collect (loop for lr-pair in  list-of-lr-pairs
+                                              append lr-pair))))))
+
+(defun match-pattern-sequence-predicate-in-source-sequence-predicates-numbers-only (pattern-sequence-predicate source-sequence-predicates)
+  "Matches a sequence-predicate against a list of sequence-predicates, returns unfiltered potential bindings lists."
+  (loop for source-sequence-predicate in source-sequence-predicates
+        append (match-sequence-predicates-numbers-only pattern-sequence-predicate source-sequence-predicate)))
+
+(defun match-sequence-predicates-numbers-only (pattern-predicate source-predicate)
+  "Matches two sequence-predicates, returns unfiltered potential bindings lists."
+    (let* ((source-string (second source-predicate))
+           (index-offset (third source-predicate))
+           (all-match-positions (cl-ppcre:all-matches "[0-9]+" source-string))
+           (possible-bindings (loop for (left right) on all-match-positions by #'cddr
+                                        collect (list (+ left index-offset) (+ right index-offset)))))
+      (loop for pb in possible-bindings
+            collect (list (fcg::make-binding (third pattern-predicate) (first pb))
+                          (fcg::make-binding (fourth pattern-predicate) (second pb))))))
+
 (defmethod fcg-expand ((type (eql :number))
                        &key value source bindings merge? cxn-inventory)
   "Matches on a number in a tag."
-  (cond  ;; When called in either merge or remove-special-operators, than just
-         ;; return the value (the pattern from the cxn).
-         ((and (eq merge? t) (eq source nil))
-          (values value bindings))
+  (declare (ignore cxn-inventory))
+  (cond (;; When called in either merge or remove-special-operators, than just return the value (the pattern from the cxn).
+         (and (eq merge? t) (eq source nil))
+         (values value bindings))
          ;; When called in matching and the source contains a predicate with an number
          ;; return the value (so that it can be removed from the root during merge)
          ;; and add a new binding between the variable from the value and the number
@@ -133,7 +169,7 @@
 (defmethod fcg-expand ((type (eql :parse-integer))
                        &key value source bindings merge? cxn-inventory)
   "Returns number from string."
-  (declare (ignore merge? source))
+  (declare (ignore merge? source cxn-inventory))
   (unless (null bindings)
     (values (read-from-string value) bindings)))
 
