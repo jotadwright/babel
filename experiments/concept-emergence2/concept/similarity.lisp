b1f2a077070d2c3ea2bc4b20953964f39ae59c3d
jerome <jerome@ai.vub.ac.be>
2024-08-21 11:08:07 +0200

    fix: use standard formulation of hellinger distance between two gaussians

--- experiments/concept-emergence2/concept/similarity.lisp
+++ experiments/concept-emergence2/concept/similarity.lisp
@@ -55,9 +55,9 @@
         for proto1 in (get-available-prototypes agent concept1)
         for proto2 = (gethash (channel proto1) (prototypes concept2))
         if (and proto2 (not (zerop ledger1)) (not (zerop ledger2)))
-          sum (similar-prototypes proto1 proto2 ledger1 ledger2 (get-configuration agent :hellinger))))
+          sum (similar-prototypes proto1 proto2 ledger1 ledger2)))
 
-(defmethod similar-prototypes ((proto1 prototype) (proto2 prototype) (ledger1 number) (ledger2 number) (sim-mode symbol))
+(defmethod similar-prototypes ((proto1 prototype) (proto2 prototype) (ledger1 number) (ledger2 number))
   "Calculates the similarity between two prototypes.
    
    The similarity corresponds to a product t-norm of
@@ -71,6 +71,6 @@
         ;; similarity of the weights
         (weight-similarity (- 1 (abs (- (/ (weight proto1) ledger1) (/ (weight proto2) ledger2)))))
         ;; take complement of distance (1-h) so that it becomes a similarity metric
-        (prototype-similarity (- 1 (f-divergence (distribution proto1) (distribution proto2) sim-mode))))
+        (prototype-similarity (- 1 (f-divergence (distribution proto1) (distribution proto2)))))
     ;; multiple all three
     (* avg-weight weight-similarity prototype-similarity)))
