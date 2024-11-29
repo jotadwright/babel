0dd062a5b60d07ace61fb5090204773ca29a11ba
Lara Verheyen <lara.verheyen@ai.vub.ac.be>
2024-11-29 17:05:42 +0100

    make-initial grammar for naming game agents

--- experiments/crs-conventionality/grammar.lisp
+++ experiments/crs-conventionality/grammar.lisp
@@ -9,23 +9,17 @@
 (defgeneric make-initial-grammar (agent)
   (:documentation "Initialises the grammar of an agent."))
 
-#|
- 
+
+;; Naming game ;;
+;;;;;;;;;;;;;;;;;
+
 (defmethod make-initial-grammar ((agent naming-game-agent))
   "Initialises the grammar of a naming game agent."
   (let* ((grammar-name (make-const "grammar")))
     (setf (grammar agent) (eval `(def-fcg-constructions ,grammar-name
+                                   :hashed t
                                    :cxn-inventory ,grammar-name
-                                   :feature-types ((form set-of-predicates :handle-regex-sequences)
-                                                   (meaning set-of-predicates))
-                                   :fcg-configurations (
-                                                        (:node-expansion-mode . :full-expansion)
-                                                        ;; for using heuristics
-                                                        (:consolidate-repairs . t)
-                                                        (:de-render-mode . :de-render-sequence-predicates)
-                                                        (:render-mode . :render-sequences)
-                                                        (:category-linking-mode . :neighbours)
-                                                        (:parse-goal-tests :no-applicable-cxns :connected-semantic-network :no-sequence-in-root)
-                                                        (:production-goal-tests :no-applicable-cxns :no-meaning-in-root :connected-structure)))))))
-
-|#
+                                   :feature-types ((meaning set)
+                                                   (form set))
+                                   :fcg-configurations ((:de-render-mode . :de-render-raw)
+                                                        (:render-mode . :render-raw)))))))
