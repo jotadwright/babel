9bf9a8d756d7acaf99cbb4c7338247c78232544d
jnevens <jnevens@ai.vub.ac.be>
2024-12-12 14:09:45 +0100

    update test case: leave binding values unbound instead of nil

--- systems/irl/tests/test-evaluate-primitive.lisp
+++ systems/irl/tests/test-evaluate-primitive.lisp
@@ -16,53 +16,30 @@
          ;; give first
          (test-assert (= 1 (length (evaluate-primitive
                                     (find-primitive 'pick-apples *apple-counting-inventory*)
-                                    (list (make-instance 'binding
-                                                         :var '?var-1
-                                                         :score nil
-                                                         :value nil)
-                                          (make-instance
-                                           'binding
-                                           :var '?var-2
-                                           :score 0.5
-                                           :value (make-instance 'quantity :n 12)))
+                                    (make-bindings
+                                     `((?var-1)
+                                       (?var-2 0.5 ,(make-instance 'quantity :n 12))))
                                     *test-ontology*))))
          ;; give second
          (test-assert (= 1 (length (evaluate-primitive
                                     (find-primitive 'pick-apples *apple-counting-inventory*)
-                                    (list (make-instance 'binding
-                                                         :var '?var-1
-                                                         :score 0.5
-                                                         :value (make-apples-set 20))
-                                          (make-instance 'binding
-                                                         :var '?var-2
-                                                         :score nil
-                                                         :value nil))
+                                    (make-bindings
+                                     `((?var-1 0.5 ,(make-apples-set 20))
+                                       (?var-2)))
                                     *test-ontology*))))
          ;; give both consistent
          (test-assert (= 1 (length (evaluate-primitive
                                     (find-primitive 'pick-apples *apple-counting-inventory*)
-                                    (list (make-instance 'binding
-                                                         :var '?var-1
-                                                         :score 0.5
-                                                         :value (make-apples-set 20))
-                                          (make-instance
-                                           'binding
-                                           :var '?var-2
-                                           :score 0.5
-                                           :value (make-instance 'quantity :n 20)))
+                                    (make-bindings
+                                     `((?var-1 0.5 ,(make-apples-set 20))
+                                       (?var-2 0.5 ,(make-instance 'quantity :n 20))))
                                     *test-ontology*))))
          ;; give both inconsistent
          (test-equal 'inconsistent (evaluate-primitive
                                     (find-primitive 'pick-apples *apple-counting-inventory*)
-                                    (list (make-instance 'binding
-                                                         :var '?var-1
-                                                         :score 0.5
-                                                         :value (make-apples-set 20))
-                                          (make-instance
-                                           'binding
-                                           :var '?var-2
-                                           :score 0.5
-                                           :value (make-instance 'quantity :n 12)))
+                                    (make-bindings
+                                     `((?var-1 0.5 ,(make-apples-set 20))
+                                       (?var-2 0.5 ,(make-instance 'quantity :n 12))))
                                     *test-ontology*))
 
 
