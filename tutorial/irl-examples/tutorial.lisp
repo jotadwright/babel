2b6e7c9576a37aa0e2e333faae28ee246d62cb99
jnevens <jnevens@ai.vub.ac.be>
2024-12-16 14:02:16 +0100

    bugfix in irl tutorial: configuration has a different name

--- tutorial/irl-examples/tutorial.lisp
+++ tutorial/irl-examples/tutorial.lisp
@@ -664,7 +664,6 @@
 ;; No solutions left
 (get-next-solutions *my-composer*)
 
-
 ;;   ######################################################
 ;;   Part 7c: Confine the search
 
@@ -705,7 +704,7 @@
                  (create-chunk-from-primitive 'filter-by-size))
    :ontology *ontology* :primitive-inventory *irl-primitives*
    :configurations '((:max-search-depth . 6)
-                     (:check-node-modes :check-duplicate
+                     (:chunk-node-tests :check-duplicate
                                         :no-primitive-occurs-more-than-once))))
 
 (get-all-solutions *my-composer-2*) ;; now it only returns relevant results (2)
