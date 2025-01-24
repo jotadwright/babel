811c15c90e308ebd16478137fc913fe4abc57b25
Paul <paul@insight-labs.eu>
2025-01-24 14:59:08 +0100

    progress

--- sharing/distributional-fcg/demo.lisp
+++ sharing/distributional-fcg/demo.lisp
@@ -7,6 +7,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (ql:quickload :distributional-fcg)
+;; (activate-monitor trace-fcg)
 
 #|
 1. Introduction
@@ -57,20 +58,22 @@ For convenience, we can define a function that given a token computes the distri
           do (setf closest-token-so-far-with-score (cons other-token cosine-similarity))
         finally (return (values (car closest-token-so-far-with-score) (cdr closest-token-so-far-with-score)))))
 
-(closest-token "suv" '("girl" "cylinder" "vehicle" "car" "hardly" "he" "drives" "an"))
+(closest-token "suv" '("girl" "cylinder" "vehicle" "car" "hardly" "he" "drives" "an" "."))
 ;; => "vehicle", 0.8198127
 
 #|
 2. Operationalisation in FCG
 
 What do we need to configure in FCG?
-- Pretokenisation of the input utterance.
-  => FCG solution: de-rendering into tokens (using NLP-tools or by space)
+- Pretokenisation of the input utterance + quering and storing of pretrained embeddings.
+  => FCG solution: de-rendering into tokens and retrieving embeddings (using NLP-tools).
+     The transient structure holds pointers to these embeddings, the embeddings themselves
+     are stored in the :ts-token-embeddings field of the blackboard of the cxn-inventory.
 - Storing embedding vectors in cxns.
   => FCG solution: when creating a construction, we do not only store strings
      but also precomputed embeddings of the corresponding tokens. For visualisation
-     purposes, we store symbols pointing to the embedding vectors in the cxns and
-     the embedding vectors themselves in the :attributes of the cxns.
+     purposes, we store symbols in the cxn pointing to the embedding vectors in th the blackbboard of the cxn-inventory.
+     The field used is :cxn-token-embeddings.
 - Matching based on distributional similarity instead of symbol equivalence.
   => FCG solution: declaring form as a feature type with procedural attachment (:compare-embeddings),
      matching the vector with the embedding of the tokens, letting matching succeed above a certain threshold,
@@ -82,39 +85,88 @@ Let us start by creating the initial transient structure. As we pretokenize, we 
 each token that hold the string and a pointer to its (precomputed) embedding.
 |#
 
-(defmethod de-render ((utterance string) (mode (eql :de-render-token-embeddings)) &key &allow-other-keys)
+(defmethod de-render ((utterance string) (mode (eql :de-render-token-embeddings)) &key cxn-inventory &allow-other-keys)
   "Retrieves tokens and embeddings for string, creating one unit per token."
-  (let* ((embedding-data nil) (adjacency-constraints nil)
-         (token-embeddings (nlp-tools::get-word-embeddings utterance))
-         (token-units (loop for (token embedding) in token-embeddings
-                            for unit-name = (make-id token)
-                            for embedding-pointer = (make-id (string-append token "->"))
-                            do (push (cons embedding-pointer embedding) embedding-data)
-                            collect (list unit-name
-                                          `(token ,token)
-                                          `(form ((embedding ,unit-name ,embedding-pointer)))))))
-
-    
-    (do ((left (mapcar #'first token-units) (rest left)))
-	((null (cdr left)))
-      (push `(adjacent ,(first left) ,(second left))
-	    adjacency-constraints))
-
-    (make-instance 'coupled-feature-structure
-                   :data `((:token-embeddings ,@embedding-data))
-		   :left-pole `((root (meaning ())
-                                      (sem-cat ())
-                                      (form ,(reverse adjacency-constraints))
-                                      (syn-cat ()))
-                                ,@token-units)
-		   :right-pole '((root)))))
-    
-(setf *initial-cfs* (de-render "car suv vehicle" :de-render-token-embeddings))
-(inspect *initial-cfs*)
-
-(cdr (assoc (caar (get-data *initial-cfs* :token-embeddings)) (get-data *initial-cfs* :token-embeddings)))
+  (multiple-value-bind (units embedding-data)
+      (loop with token-embeddings = (nlp-tools::get-word-embeddings utterance)
+            for (token embedding) in token-embeddings
+            for unit-name = (make-id token)
+            for embedding-pointer = (intern (upcase (string-append "->" token)))
+            collect (cons embedding-pointer embedding)
+              into embedding-data
+            collect (make-unit :name unit-name
+                               :features `((token ,token)
+                                           (embedding ,embedding-pointer)))
+              into units
+            finally (return (values units embedding-data)))
+    ;; adjecency-constraints
+    (let ((adjacency-constraints (loop for (unit-name . rest) on (mapcar #'first units)
+                                       when rest
+                                         collect `(adjacent ,unit-name ,(first rest)))))
+      (set-data (blackboard cxn-inventory) :ts-token-embeddings embedding-data)
+      (make-instance 'coupled-feature-structure
+                     :left-pole `((root (meaning ())
+                                        (sem-cat ())
+                                        (form ,adjacency-constraints)
+                                        (syn-cat ()))
+                                  ,@units)
+                     :right-pole '((root))))))
 
 
+#|
+Let us now visualise the initial transient structure (in comprehension) for the utterance
+"the man drives an suv" using our fresh de-render method.
+|#
+
+(add-element (make-html-fcg-light (de-render "the man drives an suv."
+                                             :de-render-token-embeddings
+                                             :cxn-inventory *fcg-constructions*)
+                                  :feature-types (feature-types *fcg-constructions*)
+                                  :construction-inventory *fcg-constructions*))
+
+#|
+Let us create a small grammar fragment.
+|#
+
+(def-fcg-constructions distributional-fcg-grammar-ex-1 
+                       :feature-types ((form set-of-predicates)
+                                       (meaning set-of-predicates)
+                                       (subunits set)
+                                       (args sequence)
+                                       (footprints set)
+                                       (boundaries default))
+                       :fcg-configurations (;; --- (DE)RENDER ---
+                                            (:de-render-mode . :de-render-sequence-predicates)
+                                            (:render-mode . :render-sequences)
+
+                                            ;; --- HEURISTICS ---
+                                            ;; use dedicated cip
+                                            (:construction-inventory-processor-mode . :heuristic-search)
+                                            ;; always fully expands node immediately
+                                            (:node-expansion-mode . :full-expansion)
+                                            ;; returns all cxns at once
+                                            (:cxn-supplier-mode . :all-cxns)
+                                            ;; for using heuristics (alternatives: :depth-first, :breadth-first :random)
+                                            (:search-algorithm . :best-first)
+                                            ;; list of heuristic functions (modes of #'apply-heuristic) - only used with best-first search
+                                            (:heuristics :nr-of-applied-cxns :nr-of-units-matched)
+                                            ;; how to use results of heuristic functions for scoring a node
+                                            (:heuristic-value-mode . :sum-heuristics-and-parent) 
+
+                                            ;; --- GOAL TESTS ---
+                                            ;; goal tests for comprehension
+                                            (:parse-goal-tests
+                                             :no-applicable-cxns ;; succeeds if node is fully expanded and no cxns could apply to its children
+                                             :no-strings-in-root ;; succeeds if no string features remain in the root
+                                             :connected-semantic-network ;; succeeds if the semantic network is fully connected
+                                             :connected-structure ;; succeeds if all units are connected
+                                             )
+                                            ;; goal tests for formulation
+                                            (:production-goal-tests
+                                             :no-applicable-cxns ;; succeeds if node is fully expanded and no cxns could apply to its children
+                                             :no-meaning-in-root ;; succeeds if no meaning predicates remain in root
+                                             )))
+ 
 
 
 
