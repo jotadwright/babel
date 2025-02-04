a97988867d57060add8aa7e87b2e850e7ba9564c
jerome <jerome@ai.vub.ac.be>
2025-02-04 23:56:30 +0100

    configuration for init-babel-user to choose whether to persist gnuplot displays

--- systems/monitors/plot-monitors.lisp
+++ systems/monitors/plot-monitors.lisp
@@ -29,7 +29,7 @@
 (defun pipe-to-gnuplot ()
   "Starts a gnuplot process and returns a stream for writing to that process"
   (let ((stream (pipe-output  "gnuplot"
-                              :args (list "-persist" "-") :wait nil)))
+                              :args (list (format nil "-~apersist" (if cl-user::*persist-gnuplot-windows* "" "no"))) :wait nil)))
     #+(or :win32 :linux) (format stream "set terminal x11 1 noraise~%")
     #+darwin (format stream "set terminal qt noraise~%")
     stream))
