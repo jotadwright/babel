2411ff54254b82dc08df49d1f22b5ca0e1292e4f
Lara Verheyen <lara.verheyen@ai.vub.ac.be>
2025-02-24 10:20:24 +0100

    set discriminative power in node test and check if it is positive in the goal test

--- experiments/crs-conventionality/conceptualisation-and-production.lisp
+++ experiments/crs-conventionality/conceptualisation-and-production.lisp
@@ -40,33 +40,34 @@
                                                                                                       :scene scene)
                                                                             '-> n
                                                                             :notify (not silent))))
-         (solution-node (first solution-and-cip)) ;;TO DO: select best solution
+         (solution-nodes (first solution-and-cip)) ;; There can be multiple solution-nodes
+         (best-solution (first solution-nodes)) ;; The solution-nodes are ranked, the first is the one with the highest score (in concept learning, it is possible that there are more nodes with the same score, we just take the first and the other will be punished during alignment. )
          (cip (second solution-and-cip)))
 
     (unless silent
       (if (eq 'speaker (experiment-framework::discourse-role agent))
-        (notify routine-conceptualisation-finished cip solution-node agent)))
+        (notify routine-conceptualisation-finished cip solution-nodes agent)))
     
     ;; check if a solution is found
-    (if (not (succeeded-nodes cip))
-      (when use-meta-layer
-        ;; ! meta-layer !
-        ;;     if no solution is found, start invention and set utterance in agent
-        (unless silent (notify meta-conceptualisation-started topic agent scene))
-        (multiple-value-bind (cxn fix)
-            (fcg::invent cip agent topic scene)
-          (unless silent (notify meta-conceptualisation-finished fix agent))
-          (setf (conceptualised-utterance agent) (render (car-resulting-cfs (first (get-data (blackboard fix) 'fcg::fixed-cars)))
-                                                         (get-configuration (grammar agent) :render-mode)))
-          (setf (applied-constructions agent) (list (processing-cxn cxn)))))
-      ;; otherwise, render and set solution nodes
-      (progn
-        (setf (conceptualised-utterance agent) (render (car-resulting-cfs (fcg:cipn-car (first solution-node)))
-                                                       (get-configuration (grammar agent) :render-mode)))
-        (when (eq (discourse-role agent) 'speaker)
+            (if (not (succeeded-nodes cip))
+              (when use-meta-layer
+                ;; ! meta-layer !
+                ;;     if no solution is found, start invention and set utterance in agent
+                (unless silent (notify meta-conceptualisation-started topic agent scene))
+                (multiple-value-bind (cxn fix)
+                    (fcg::invent cip agent topic scene)
+                  (unless silent (notify meta-conceptualisation-finished fix agent))
+                  (setf (conceptualised-utterance agent) (render (car-resulting-cfs (first (get-data (blackboard fix) 'fcg::fixed-cars)))
+                                                                 (get-configuration (grammar agent) :render-mode)))
+                  (setf (applied-constructions agent) (list (processing-cxn cxn)))))
+              ;; otherwise, render and set solution nodes
+              (progn
+                (setf (conceptualised-utterance agent) (render (car-resulting-cfs (fcg:cipn-car best-solution))
+                                                               (get-configuration (grammar agent) :render-mode)))
+                (when (eq (discourse-role agent) 'speaker)
           
-          (setf (applied-constructions agent) (applied-constructions (first solution-node))) 
-          (setf (solution-node agent) (first solution-node)))))))
+                  (setf (applied-constructions agent) (applied-constructions best-solution)) 
+                  (setf (solution-node agent) best-solution)))))) ;; maybe we need all solution-nodes in the agent, for now, only pass the best solution
 
 
 (defmethod create-initial-structure ((topic crs-conventionality-entity-set)
@@ -90,16 +91,12 @@
 (defmethod cip-goal-test ((node cip-node) (mode (eql :topic-retrieved))) 
   "Checks whether the extracted meaning leads to the topic by evaluating irl program."
   (let* ((irl-program (extract-meanings (left-pole-structure (car-resulting-cfs (cipn-car node)))))
-         (applied-cxn (car-applied-cxn (cipn-car node)))
-         ;(renamings (when applied-cxn (renamings applied-cxn)))
          (renamed-irl-program (loop for predicate in irl-program
                                     for last-var = (fourth predicate)
                                     when last-var
                                       do (when (equal (get-base-name last-var)  "SCENE")
                                            (setf (fourth predicate) 'crs-conventionality::?scene))
                                     collect predicate))
-                 
-         ;(renamed-irl-program (sublis renamings irl-program))
          (topic (first (crs-conventionality::entities (find-data (blackboard (construction-inventory node)) :topic))))
          (primitive-inventory (find-data (blackboard (construction-inventory node)) :primitive-inventory))
          (ontology (find-data (blackboard primitive-inventory) :ontology))
@@ -107,6 +104,14 @@
          (irl-solution (first (irl::evaluate-irl-program renamed-irl-program ontology :primitive-inventory primitive-inventory :n 1))))
     (when target-var
       (irl::equal-entity topic (irl::value (find target-var irl-solution :key #'irl::var))))))
+
+
+(defmethod cip-goal-test ((node cip-node) (mode (eql :topic-discriminated))) 
+  "Discriminative power needs to be positive."
+  (let ((discriminative-power (cdr (find-data node :discriminative-power))))
+    (if discriminative-power
+      (>= discriminative-power 0)
+      nil)))
 
 
 (defmethod cip-node-test ((node cip-node) (mode (eql :check-branch-for-solution)))
@@ -117,3 +122,32 @@
         (push 'solution-in-branch (statuses node))
         nil)
       t)))
+
+(defmethod cip-node-test ((node cip-node) (mode (eql :discriminative-power)))
+  "Checks the discriminative power. Only do this in formulation. "
+  (let ((direction (direction (cip node))))
+    (if (equal direction 'fcg::->)
+  
+      (let* ((irl-program (extract-meanings (left-pole-structure (car-resulting-cfs (cipn-car node))))))
+        (if (not (and (equal (length irl-program) 1)
+                      (equal (first (first irl-program)) 'bind))) ;; if length is equal to 1 and just a bind statement, the goal test can never succeed.
+          (when (equal (length irl-program) 3) ;; FOR NOW ONLY GO TO IRL WHEN ONLY 1 CXN APPLIED
+            (let* ((renamed-irl-program (loop for predicate in irl-program
+                                              for last-var = (fourth predicate)
+                                              when last-var
+                                                do (when (equal (get-base-name last-var)  "SCENE")
+                                                     (setf (fourth predicate) 'crs-conventionality::?scene))
+                                              collect predicate))
+                   (topic (first (crs-conventionality::entities (find-data (blackboard (construction-inventory node)) :topic))))
+                   (primitive-inventory (find-data (blackboard (construction-inventory node)) :primitive-inventory))
+                   (ontology (find-data (blackboard primitive-inventory) :ontology))
+                   (target-var (irl::get-target-var renamed-irl-program))
+                   (bind-statement-topic `(bind ,(type-of topic) ,target-var ,topic))
+                   (irl-solution (first (irl::evaluate-irl-program (push bind-statement-topic renamed-irl-program) ontology :primitive-inventory primitive-inventory :n 1)))
+                   (discriminated (irl::score (find topic irl-solution :key #'value))))
+              (set-data node :discriminative-power (cons topic discriminated))
+              (if discriminated
+                discriminated
+                0)))
+          0)))
+    t))
