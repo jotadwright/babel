1da745ea0b56d3a06897fd7f0d973dd1ba259209
Jonas Doumen <jonas.doumen@kuleuven.be>
2025-03-05 16:47:25 +0100

    adding helper function to collect embeddings for a list of tokens and sort by similarity

--- sharing/distributional-fcg/utils.lisp
+++ sharing/distributional-fcg/utils.lisp
@@ -11,10 +11,19 @@
 (defun closest-token (target-token list-of-tokens)
   "Returns the token in list-of-tokens that is distributionally most similar to target-token."
   (loop with closest-token-so-far-with-score = (cons nil -inf)
-        with target-token-embedding =  (second (nlp-tools::get-word-embedding target-token))
-        for other-token in list-of-tokens
-        for other-token-embedding = (nlp-tools::get-word-embedding other-token)
+        with target-token-embedding = (second (nlp-tools::get-word-embedding target-token))
+        for other-token-embedding in (nlp-tools::get-word-embeddings list-of-tokens)
         for cosine-similarity = (cosine-similarity target-token-embedding (second other-token-embedding))
         when (> cosine-similarity (cdr closest-token-so-far-with-score))
-          do (setf closest-token-so-far-with-score (cons other-token cosine-similarity))
+          do (setf closest-token-so-far-with-score (cons (first other-token-embedding) cosine-similarity))
         finally (return (values (car closest-token-so-far-with-score) (cdr closest-token-so-far-with-score)))))
+
+(defun tokens-sorted-by-similarity (target-token list-of-tokens)
+  "Returns the list-of-tokens sorted by the similarity with the target-token."
+  (loop with target-token-embedding = (second (nlp-tools::get-word-embedding target-token))
+        for other-token-embedding in (nlp-tools::get-word-embeddings list-of-tokens)
+        for cosine-similarity = (cosine-similarity target-token-embedding (second other-token-embedding))
+        collect (cons (first other-token-embedding) cosine-similarity) into similarity-list
+        finally (return (sort similarity-list #'> :key #'cdr))))
+
+;;(closest-token "wire" '("send" "give" "pay"))
