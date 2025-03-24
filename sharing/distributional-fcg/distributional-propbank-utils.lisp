595b3bb11bbe898b757e851424d2a967a53aa7d3
Lara Verheyen <lara.verheyen@ai.vub.ac.be>
2025-03-24 17:45:46 +0100

    minor fixes

--- sharing/distributional-fcg/distributional-propbank-utils.lisp
+++ sharing/distributional-fcg/distributional-propbank-utils.lisp
@@ -16,7 +16,7 @@
             when similarity
               summing similarity into similarity-total
             finally (return similarity-total))
-      (if lemma-embedding-pointer
+      (if (and lemma-embedding-pointer (cdr (assoc (variablify lemma-embedding-pointer) bindings)))
         (cdr (assoc (variablify lemma-embedding-pointer) bindings))
         0))))
     
@@ -33,7 +33,8 @@
 (defun make-proto-role-embeddings (cxn-inventory)
   "For each argument-structure-cxn, set the proto-embedding in the corresponding slot."
   (let* ((arg-structure-cxns (get-argument-structure-cxns cxn-inventory)))
-    (loop for arg-structure-cxn in arg-structure-cxns
+    (loop for arg-structure-cxn-nr from 0 to (length arg-structure-cxns)
+          for arg-structure-cxn in arg-structure-cxns
           for strings-in-roles = (find-data (blackboard arg-structure-cxn) :strings-in-roles)
           for number-of-roles = (length (first strings-in-roles))
           for proto-role-embeddings = (loop for i from 0 to (- number-of-roles 1)
@@ -43,7 +44,10 @@
                                             for proto-role-embedding = (make-proto-embedding strings-in-role)
                                             when proto-role-embedding
                                               collect (cons proto-role-embedding-pointer proto-role-embedding))
-          do (append-data arg-structure-cxn :proto-role-embeddings proto-role-embeddings))))
+          do
+            (when (= 0 (mod arg-structure-cxn-nr 100))
+              (format t "~%---> Cxn ~a of ~a." arg-structure-cxn-nr (length arg-structure-cxns)))
+            (append-data arg-structure-cxn :proto-role-embeddings proto-role-embeddings))))
 
 ;; use the sum-list-of-vectors of math in babel utils
 (defmethod combine-word-embeddings ((embeddings-list list) &key (mode (eql 'addition)))
@@ -72,7 +76,7 @@
 
 
 ;; Added function to add the embeddings from the learned cxns and the ones from the transient structures of the annotation files to the blackboard of the cxn-inventory.
-(defun add-embeddings-to-cxn-inventory (cxn-inventory)
+(defun add-embeddings-to-cxn-inventory (cxn-inventory &key (role-embeddings? nil))
   "Retrieve all token embeddings for constructions that carry a token
 attribute and store them in the cxn inventory's blackboard under the
 field :cxn-token-embeddings"
@@ -85,7 +89,7 @@ field :cxn-token-embeddings"
         when cxn-token
           do (append-data (blackboard cxn-inventory) :cxn-token-embeddings (list (cons lemma-embedding-pointer
                                                                                        (second (nlp-tools:get-word-embedding (downcase (symbol-name cxn-token)))))))
-        when proto-embeddings
+        when (and role-embeddings? proto-embeddings)
           do (append-data (blackboard cxn-inventory) :cxn-token-embeddings proto-embeddings)))
 
 ;; Added function to get the string of the lemma
