86b6000204fc095d681ad01034afc4d2e87b1e0b
Jamie Wright <jamie.wright@unamur.be>
2025-04-01 10:16:27 +0200

    Slurm scripts

---
+++ experiments/crs-conventionality/batch/slurm/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+module purge
+module load SBCL/2.5.3-GCCcore-13.3.0
+module load binutils/2.42-GCCcore-13.3.0 # required for distributions package
+sbcl --noinform --eval "(ql:quickload :cle)" --quit
+
