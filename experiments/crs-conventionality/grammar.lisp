1d7ae03384000204f1e7e12b38db35cf6a502a50
Jamie Wright <jamie.wright@unamur.be>
2025-04-04 09:15:29 +0200

    Fix: if entrenchment score = 0, only look at discriminative power

--- experiments/crs-conventionality/grammar.lisp
+++ experiments/crs-conventionality/grammar.lisp
@@ -170,7 +170,9 @@
             (entrenchement-score (attr-val applied-cxn :score))
             (concept-entity-discriminative-power (find-data node :discriminative-power)))
         (if concept-entity-discriminative-power
-          (* entrenchement-score (cdr concept-entity-discriminative-power)) ;; check what to do if entrenchment-score is 0.
+          (if (= entrenchement-score 0) ;; if entrenchment score is 0, then only look at DP
+            (cdr concept-entity-discriminative-power) 
+            (* entrenchement-score (cdr concept-entity-discriminative-power))) 
           0))
       1)))
    
