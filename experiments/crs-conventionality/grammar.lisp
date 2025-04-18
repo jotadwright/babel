4c32191c69e49d7c0fcc87cf657f07b73a3b17d7
Jamie Wright <jamie.wright@unamur.be>
2025-04-18 12:27:20 +0200

    Node creation optimisation

--- experiments/crs-conventionality/grammar.lisp
+++ experiments/crs-conventionality/grammar.lisp
@@ -87,7 +87,7 @@
                                                         (:create-initial-structure-mode-comprehension . :utterance-and-scene)
                                                         ;; Construction supplier and search
                                                         (:construction-inventory-processor-mode . :heuristic-search)
-                                                        (:node-expansion-mode . :multiple-cxns)
+                                                        (:node-expansion-mode . :multiple-cxns-restrict-search-depth)
                                                         (:cxn-supplier-mode . :cascading-entrenchment)
                                                         ;; Diagnostics and repairs
                                                         (:conceptualisation-diagnostics fcg::diagnose-cip-conceptualisation-success)
@@ -175,4 +175,42 @@
             (* entrenchement-score (cdr concept-entity-discriminative-power))) 
           0))
       1)))
+
+
+(defmethod expand-cip-node ((node cip-node) (mode (eql :multiple-cxns-restrict-search-depth)))
+  "When next-cxn returns a list, then apply all these cxns to the cipn."
+  (if (< (length (all-parents node))
+           (- (get-configuration (construction-inventory (cip node)) :max-search-depth) 1))
+    
+    (loop with nodes-to-queue = nil
+          with failed-nodes = nil
+          with cxn-inventory = (construction-inventory node)
+          for cxns = (listify (next-cxn (cxn-supplier node) node))
+          when cxns
+            do (let ((succeeded-cars nil)
+                     (failed-cars nil))
+                 (dolist (cxn cxns)
+                   (multiple-value-bind (these-succeeded-cars these-failed-cars)
+                       (fcg-apply (safe-cxn cxn (applied-constructions node))
+                                  (car-resulting-cfs (cipn-car node))
+                                  (direction (cip node)) :notify nil
+                                  :configuration (configuration (construction-inventory node))
+                                  :cxn-inventory cxn-inventory)
+                     (setf succeeded-cars (append succeeded-cars these-succeeded-cars)
+                           failed-cars (append failed-cars these-failed-cars))))
+                 (loop for car in succeeded-cars
+                       do (push (cip-add-child node car)
+                                nodes-to-queue)
+                       when (apply-sequentially? node (car-applied-cxn car))
+                         do (setf (fully-expanded? node) t) (return))
+                 
+                 (loop for car in failed-cars
+                       do (push (cip-add-child node car :cxn-applied nil)
+                                failed-nodes)))
+          when nodes-to-queue do (return nodes-to-queue)
+          while cxns
+          finally (setf (fully-expanded? node) t))
+    (progn
+      (setf (fully-expanded? node) t)
+      nil)))
    
