037565063915fdacc503ddaf7fbd251eaea4f434
Paul Van Eecke <paul@ai.vub.ac.be>
2025-04-25 12:54:41 +0200

    everything now working cross-package

--- grammars/propbank-grammar/fcg-components/hash-mode.lisp
+++ grammars/propbank-grammar/fcg-components/hash-mode.lisp
@@ -11,7 +11,7 @@
                  &key &allow-other-keys)
   "Returns the lemma from the attributes of the construction"
   (when (attr-val construction :lemma)
-    (mapcar #'symbol-name (remove nil (list (attr-val construction :lemma))))))
+    (remove nil (list (attr-val construction :lemma)))))
 
 
 (defmethod hash ((node cip-node)
@@ -19,11 +19,10 @@
                  &key &allow-other-keys)
   "Checks all units for a lemma feature."
   (loop for unit in (fcg-get-transient-unit-structure node)
-        for lemma = (if (equalp (unit-feature-value unit 'node-type) 'leaf)
+        for lemma = (if (eq (unit-feature-value unit 'node-type) 'leaf)
                       (unit-feature-value unit 'lemma)
                       (or (unit-feature-value unit 'lemma) ;;for phrasals
-                          (unit-feature-value unit 'string)))
-        when (and lemma (symbolp lemma))
-          collect (symbol-name lemma)
-        when (and lemma (stringp lemma))
-          collect lemma))
+                          (intern (upcase (unit-feature-value unit 'string)) :propbank-grammar)))
+        when lemma
+          collect it))
+     
