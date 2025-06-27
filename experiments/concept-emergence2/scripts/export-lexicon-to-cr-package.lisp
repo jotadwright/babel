75c7d4a73edf04cdcbb68c3eee7648037622ab69
jerome <jerome@ai.vub.ac.be>
2025-06-27 16:19:51 +0200

    fix: update script

--- experiments/concept-emergence2/scripts/export-lexicon-to-cr-package.lisp
+++ experiments/concept-emergence2/scripts/export-lexicon-to-cr-package.lisp
@@ -9,134 +9,136 @@
 (ql:quickload :cle)
 (in-package :cle)
 
-;; Setup the concept learning experiment ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *seed* 10)
+
 
 (progn
-  (format t "~%Starting a new experiment.~%")
-  ;; reset the web interface
-  (wi::reset)
-  ;; reset monitors
-  (monitors::notify reset-monitors)
-  ;; deactivate all monitors (as a sanity check)
-  (deactivate-all-monitors)
-  ;; configure the game
-  (defparameter *concept-learning-game*
-    (make-configuration
-     :entries `(
+
+  ;; Setup the concept learning experiment ;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (progn
+    (format t "~%Starting a new experiment.~%")
+    ;; reset the web interface
+    (wi::reset)
+    ;; reset monitors
+    (monitors::notify reset-monitors)
+    ;; deactivate all monitors (as a sanity check)
+    (deactivate-all-monitors)
+    ;; configure the game
+    (defparameter *concept-learning-game*
+      (make-configuration
+       :entries `(
                 ;; monitoring
-                (:log-every-x-interactions . 100) ;; integer, frequence of when to log measures to standard output
-                (:usage-table-window . 5000) ;; integer, window size of the construction inventory usage table
-                (:save-distribution-history . nil) ;; t or nil, whether to save the history of updates to the distribution (very memory-intensive!)
-                ;; setup environment
-                (:dataset-loader . :precomputed) ;; keyword, :precomputed or :runtime, load data in by scene (:precomputed) or by objects (:runtime)
-                (:dataset-view . :shared-views) ;; keyword, :shared-views or :exclusive-views, all views are shared or is each agent assigned a view?
-                (:dataset "clevr-simulated") ;; list of strings, each string represents a view over a dataset
-                (:feature-set "clevr-simulated") ;; list of strings, each string represents a feature set (stored in Corpora/concept-emergence2/-feature-sets), every feature-set is associated to a coressponding element in :dataset
-                (:dataset-split . "train") ;; string, "train" or "test", which split of the data to use?
-                ;; population
-                (:population-size . 2) ;; integer, size of the population
-                (:network-topology . :fully-connected) ;; keyword, :fully-connected or :regular or :small-world
+                  (:log-every-x-interactions . 100) ;; integer, frequence of when to log measures to standard output
+                  (:usage-table-window . 5000) ;; integer, window size of the construction inventory usage table
+                  (:save-distribution-history . nil) ;; t or nil, whether to save the history of updates to the distribution (very memory-intensive!)
+                  ;; setup environment
+                  (:dataset-loader . :precomputed) ;; keyword, :precomputed or :runtime, load data in by scene (:precomputed) or by objects (:runtime)
+                  (:dataset-view . :shared-views) ;; keyword, :shared-views or :exclusive-views, all views are shared or is each agent assigned a view?
+                  (:dataset "clevr-simulated") ;; list of strings, each string represents a view over a dataset
+                  (:feature-set "clevr-simulated") ;; list of strings, each string represents a feature set (stored in Corpora/concept-emergence2/-feature-sets), every feature-set is associated to a coressponding element in :dataset
+                  (:dataset-split . "train") ;; string, "train" or "test", which split of the data to use?
+                  ;; population
+                  (:population-size . 2) ;; integer, size of the population
+                  (:network-topology . :fully-connected) ;; keyword, :fully-connected or :regular or :small-world
                 ;(:local-connectivity . 2) ;; integer, for :regular and :small-world
                 ;(:rewiring-probability . 0.3) ;; float, for :small-world
-                (:learning-environment . :tutor-learner) ;; keyword, :emergence or tutor-learner
-                (:interacting-agents-strategy . :tutor-learner) ;; keyword, :standard or :boltzmann-partner-selection or :tutor-learner
-                (:boltzmann-tau . 0) ;; integer, for :boltzmann-partner-selection
-                (:boltzmann-lr . 0.05) ;; float, for :boltzmann-partner-selection
-                (:min-context-size . 10) ;; integer, minimum number of context elements
-                (:max-context-size . 10) ;; integer, maximum number of context elements
-                ;; disable channels
-                (:disable-channels . :none) ;; keyword, :none or :random or :fixed
-                (:amount-disabled-channels . 0) ;; integer, amount of channels to disable
-                ;; noised channels
-                (:sensor-noise . :none) ;; keyword, :none or :shift
-                (:sensor-std . 0.0) ;; float, corresponds to calibration noise
-                (:observation-noise . :none) ;; keyword, :none or :shift
-                (:observation-std . 0.0) ;; float, noise during perception
-                ;; scene sampling
-                (:scene-sampling . :random) ;; :random
-                (:topic-sampling . :discriminative) ;; :random, :discriminative
-                ;; alignment
-                (:initial-cxn-entrenchement . 0.5) ;; default entrenchment score
-                (:align . t) ;; t or nil, activates alignment or not
-                (:entrenchment-incf . 0.0) ;; hyperparameter for alignment (:align)
-                (:entrenchment-decf . 0.0) ;; hyperparameter for alignment (:align)
-                (:entrenchment-li . -0.0) ;; lateral inhibition, hyperparameter for alignment (:align)
-                ;; concept representation parameters
-                (:M2 . 0.001) ;; float, default initialisation for gaussian distributions
-                ;; prototype weight inits
-                (:weight-update-strategy . :j-interpolation) ;; :standard or :j-interpolation
-                (:shift-weights-strategy . :shift-weights-by-single-dimension)
-                (:initial-weight . 0) ;; default weight
-                (:weight-incf . 1)    ;; :standard uses floats, j-interpolation uses int
-                (:weight-decf . -1)   ;; :standard uses floats, j-interpolation uses int
-                ;; experimental alternatives
-                (:prototype-distance . :paper) ;; :paper or :paper-wo-ledger
-                ;; staging
-                (:switch-condition . :none) ; :none, :after-n-interactions
+                  (:learning-environment . :tutor-learner) ;; keyword, :emergence or tutor-learner
+                  (:interacting-agents-strategy . :tutor-learner) ;; keyword, :standard or :boltzmann-partner-selection or :tutor-learner
+                  (:boltzmann-tau . 0) ;; integer, for :boltzmann-partner-selection
+                  (:boltzmann-lr . 0.05) ;; float, for :boltzmann-partner-selection
+                  (:min-context-size . 10) ;; integer, minimum number of context elements
+                  (:max-context-size . 10) ;; integer, maximum number of context elements
+                  ;; disable channels
+                  (:disable-channels . :none) ;; keyword, :none or :random or :fixed
+                  (:amount-disabled-channels . 0) ;; integer, amount of channels to disable
+                  ;; noised channels
+                  (:sensor-noise . :none) ;; keyword, :none or :shift
+                  (:sensor-std . 0.0) ;; float, corresponds to calibration noise
+                  (:observation-noise . :none) ;; keyword, :none or :shift
+                  (:observation-std . 0.0) ;; float, noise during perception
+                  ;; scene sampling
+                  (:scene-sampling . :random) ;; :random
+                  (:topic-sampling . :discriminative) ;; :random, :discriminative
+                  ;; alignment
+                  (:initial-cxn-entrenchement . 0.5) ;; default entrenchment score
+                  (:align . t) ;; t or nil, activates alignment or not
+                  (:entrenchment-incf . 0.0) ;; hyperparameter for alignment (:align)
+                  (:entrenchment-decf . 0.0) ;; hyperparameter for alignment (:align)
+                  (:entrenchment-li . -0.0) ;; lateral inhibition, hyperparameter for alignment (:align)
+                  ;; concept representation parameters
+                  (:M2 . 0.001) ;; float, default initialisation for gaussian distributions
+                  ;; prototype weight inits
+                  (:weight-update-strategy . :j-interpolation) ;; :standard or :j-interpolation
+                  (:shift-weights-strategy . :shift-weights-by-single-dimension)
+                  (:initial-weight . 0) ;; default weight
+                  (:weight-incf . 1)    ;; :standard uses floats, j-interpolation uses int
+                  (:weight-decf . -1)   ;; :standard uses floats, j-interpolation uses int
+                  ;; experimental alternatives
+                  (:prototype-distance . :paper) ;; :paper or :paper-wo-ledger
+                  ;; staging
+                  (:switch-condition . :none) ; :none, :after-n-interactions
                 ;(:switch-conditions-after-n-interactions . 50000) ;; for :after-n-interactions
                 ;(:stage-parameters ((:switch-disable-channels-half . 10))) ;; for :after-n-interactions
-                ;; measures
-                (:coherence-perspective . :hearer) ;; :hearer or :speaker, determines how conventionalisation is measured
-                ;; paths for exporting data to disk
-                (:exp-top-dir . "cxn-nlp")  ;; directory name that groups a set of related of experiments together (e.g. logging/train/<exp-top-dir>)
-                (:exp-name . "clevr-simulated")     ;; directory name that groups different runs of an experiment (i.e. logging/train/<exp-top-dir>/<exp-name>)
-                (:log-dir-name . "phase-1") ;; directory name for a single run (i.e. logging/train/<exp-top-dir>/<exp-name>/<log-dir-name>)
-                )))
-  ;; instantiate the concept learning experiment
-  (setf *experiment* (make-instance 'cle-experiment :configuration *concept-learning-game*)))
+                  ;; measures
+                  (:coherence-perspective . :hearer) ;; :hearer or :speaker, determines how conventionalisation is measured
+                  ;; paths for exporting data to disk
+                  (:exp-top-dir . "cxn-nlp")  ;; directory name that groups a set of related of experiments together (e.g. logging/train/<exp-top-dir>)
+                  (:exp-name . "clevr-simulated")     ;; directory name that groups different runs of an experiment (i.e. logging/train/<exp-top-dir>/<exp-name>)
+                  (:log-dir-name . "phase-1") ;; directory name for a single run (i.e. logging/train/<exp-top-dir>/<exp-name>/<log-dir-name>)
+                  )))
+    ;; instantiate the concept learning experiment
+    (setf *experiment* (make-instance 'cle-experiment :configuration *concept-learning-game*)))
 
 
-;; create visualisation of population
+  ;; create visualisation of population
 ;(setf output-fname (population-network->graphviz (agents *experiment*) :layout "sfdp" :use-labels? t))
 ;(draw-graphviz-image output-fname :layout "sfdp" :make-image t :open-image t)
 
 
-;; Option 1: run experiment for x interactions ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Option 1: run experiment for x interactions ;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(progn
-  ;; reset the web interface
-  (wi::reset)
-  ;; deactivate `tracer` monitors
-  (monitors::notify reset-monitors)
-  (deactivate-all-monitors)
-  ;; activate monitors
+           ;(loop for seed in (list 1 2 3 4 5 6 7 8 9 10)
+  
+  (set-configuration *experiment* :seed *seed*)
+  (set-configuration *experiment* :log-dir-name (format nil "phase-1-seed-~a" *seed*))
+         
 
-  (setf *success-per-concept* (make-hash-table :test #'equalp))
+  (progn
+    ;; reset the web interface
+    (wi::reset)
+    ;; deactivate `tracer` monitors
+    (monitors::notify reset-monitors)
+    (deactivate-all-monitors)
+    ;; activate monitors
 
-  (set-up-monitors (list "log-every-x-interactions-in-output-browser"
-                         "record-communicative-success"
-                         "record-construction-inventory-size"
-                         "export-communicative-success"
-                         "export-construction-inventory-size"
-                         )
-                   (list "train" "val")
-                   *experiment*)
+    (setf *success-per-concept* (make-hash-table :test #'equalp))
 
-  (format t "~% --> Running a new serie of interactions.~%")
-  (let ((nr-of-interactions 5000))
-    (set-configuration *experiment* :nr-of-interactions nr-of-interactions)
-    (run-interaction *experiment*)
-    (while (< (interaction-number (current-interaction *experiment*)) nr-of-interactions)
-      do (run-interaction *experiment*)))
+    (set-up-monitors (list "log-every-x-interactions-in-output-browser"
+                           "record-communicative-success"
+                           "record-construction-inventory-size"
+                           "export-communicative-success"
+                           "export-construction-inventory-size"
+                           )
+                     (list "train" "val")
+                     *experiment*)
 
-  (notify run-series-finished *experiment*)
-  (notify series-finished 1)
-  (notify batch-finished (class-string *experiment*))
-  )
+    (format t "~% --> Running a new serie of interactions.~%")
+    (let ((nr-of-interactions 5000))
+      (set-configuration *experiment* :nr-of-interactions nr-of-interactions)
+      (run-interaction *experiment*)
+      (while (< (interaction-number (current-interaction *experiment*)) nr-of-interactions)
+        do (run-interaction *experiment*)))
 
-(defun display-success ()
-  (loop for key being the hash-keys of *success-per-concept*
-        using (hash-value counts)
-        for num = (car counts)
-        for denom = (cdr counts)
-        do (format t "~%~,1f (~a)"
-                   (* 100 (/ num denom))
-                   key)))
+    (notify run-series-finished *experiment*)
+    (notify series-finished 1)
+    (notify batch-finished (class-string *experiment*))
+    ))
 
-(display-success)
-
+  
 
 (ql:quickload :concept-representations)
 (in-package :concept-representations)
@@ -195,4 +197,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 
 ;; display the lexicon of an agent
-(cle::display-lexicon (second (cle::agents cle::*experiment*)) :weight-threshold 0.1 :sort t)
+;(cle::display-lexicon (second (cle::agents cle::*experiment*)) :weight-threshold 0.1 :sort t)
+
+
+         #|(defun display-success ()
+  (loop for key being the hash-keys of *success-per-concept*
+        using (hash-value counts)
+        for num = (car counts)
+        for denom = (cdr counts)
+        do (format t "~%~,1f (~a)"
+                   (* 100 (/ num denom))
+                   key)))
+
+(display-success)|#
