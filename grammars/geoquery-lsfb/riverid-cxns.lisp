55a0203d6a85007e55f639d6e2b3f699580fa02d
ldevos <liesbet.devos@unamur.be>
2025-08-07 17:12:24 +0200

    delete old files

--- grammars/geoquery-lsfb/riverid-cxns.lisp
+++ grammars/geoquery-lsfb/riverid-cxns.lisp
@@ -1,20 +1,47 @@
 (in-package :geoquery-lsfb-grammar-copy)
 
-(def-fcg-cxn riverid-cxn
-             ((?river-unit
-               (footprints (identified))
-               (args ((target ?g)
-                      (scope ?f))))
-              <-
-              (?river-unit
-               (args ((target ?g)))
-               (HASH meaning ((riverid ?f ?g)))
-               (sem-cat (identified-category river)
-                        (identified yes))
-               --
-               (footprints (not identified))
-               (syn-cat noun)))
-             :cxn-inventory *geoquery-lsfb-copy*)
+(loop for river in *geoquery-rivers*
+      for river-meaning = (replace-spaces (cdr (assoc :name river)) :replacer "_")
+      for hamnosys = (make-fingerspelling river-meaning)
+      for construction-name = (read-from-string (format nil "~a-cxn-1" river-meaning))
+      for river-fcg-tag = (read-from-string (format nil "?fs-~a" river-meaning))
+      for river-unit-name = (read-from-string (format nil "?~a-unit" river-meaning))
+      for sem-categories = (append '(river)(determine-additional-categories river-meaning :type 'river))
+      for possible-category-len = (length sem-categories)
+      when (not (member 'river sem-categories))
+        do (eval
+            `(def-fcg-cxn ,construction-name
+                          ((,river-unit-name
+                            (syn-cat noun)
+                            ,(if (eql possible-category-len 1)
+                               `(sem-cat
+                                 (identified-category river)
+                                 (identified yes))
+                               `(sem-cat
+                                 (possible-categories ,sem-categories)
+                                 (identified no)))
+                            (footprints
+                             ,(if (eql possible-category-len 1)
+                                `(identified)
+                                `()))
+                            (args
+                             ,(if (eql possible-category-len 1)
+                                `((target ?f)
+                                  (scope ?e))
+                                `((target ?f))))
+                            (boundaries ((left ,river-fcg-tag)
+                                         (right ,river-fcg-tag))))
+                           <-
+                           (,river-unit-name
+                            (HASH meaning
+                                  ,(if (eql possible-category-len 1)
+                                     `((,(read-from-string river-meaning) ?f)
+                                       (riverid ?e ?f))
+                                     `((,(read-from-string river-meaning) ?f))))
+                            --
+                            (HASH form ((right-hand-articulation ,river-fcg-tag ,hamnosys)))))
+                          :cxn-inventory *geoquery-lsfb*)))
+
 
 (def-fcg-cxn const-?river-cxn\(riviere\,?pt\,?riviere\)
              ((?const-unit
@@ -55,7 +82,7 @@
                (HASH form ((two-hand-articulation ?riviere "")
                            (adjacent ?riviere ?pt-unit)
                            (adjacent ?pt-unit ?river-left-boundary)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 (def-fcg-cxn const-?river-cxn\(riviere\,?riviere\)
              ((?const-unit
@@ -86,7 +113,7 @@
                --
                (HASH form ((two-hand-articulation ?riviere "")
                            (adjacent ?riviere ?river-left-boundary)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 (def-fcg-cxn const-?river-cxn\(riviere\,?riviere\,riviere\)
              ((?const-unit
@@ -119,7 +146,7 @@
                            (two-hand-articulation ?riviere-2 "")
                            (adjacent ?riviere ?river-left-boundary)
                            (adjacent ?river-right-boundary ?riviere-2)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 (def-fcg-cxn const-?river-cxn\(eau\,riviere\,?ds\,?riviere\)
              ((?const-unit
@@ -165,7 +192,7 @@
                            (adjacent ?eau ?riviere)
                            (adjacent ?riviere ?ds-left)
                            (adjacent ?ds-right ?river-left-boundary)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 (def-fcg-cxn const-?river-cxn\(il-y-a\,riviere\,?riviere\,riviere\)
              ((?const-unit
@@ -200,7 +227,7 @@
                            (adjacent ?il-y-a ?riviere-1)
                            (adjacent ?riviere-1 ?river-left-boundary)
                            (adjacent ?river-right-boundary ?riviere-2)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 
 (def-fcg-cxn const-?river-cxn\(eau\,riviere\,?pt\,?riviere\,?pt\)
@@ -254,7 +281,7 @@
                            (adjacent ?riviere ?pt-unit-1)
                            (adjacent ?pt-unit-1 ?river-left-boundary)
                            (adjacent ?river-right-boundary ?pt-unit-2)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 
 (def-fcg-cxn const-?river-cxn\(eau\,riviere\,?pt\,?riviere\)
@@ -298,7 +325,7 @@
                            (adjacent ?eau ?riviere)
                            (adjacent ?riviere ?pt-unit-1)
                            (adjacent ?pt-unit-1 ?river-left-boundary)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 (def-fcg-cxn const-?river-cxn\(il-y-a\,riviere\,eau\,?pt\,riviere\,?pt\,nom\,?riviere\)
              ((?const-unit
@@ -357,7 +384,7 @@
                            (adjacent ?riviere-2 ?pt-unit-2)
                            (adjacent ?pt-unit-2 ?nom)
                            (adjacent ?nom ?river-left-boundary)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
 
 
 (def-fcg-cxn const-?river-cxn\(riviere\,nom\,?riviere\,?pt\,riviere\)
@@ -403,4 +430,4 @@
                            (adjacent ?nom ?river-left-boundary)
                            (adjacent ?river-right-boundary ?pt-unit)
                            (adjacent ?pt-unit ?riviere-2)))))
-             :cxn-inventory *geoquery-lsfb-copy*)
+             :cxn-inventory *geoquery-lsfb*)
