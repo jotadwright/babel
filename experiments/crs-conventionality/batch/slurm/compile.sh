7efaa0f443da1544fd4ff0f383222dab91821003
Jamie Wright <jamie.wright@unamur.be>
2025-09-05 15:50:55 +0200

    Remove crs-conventionality folder

--- experiments/crs-conventionality/batch/slurm/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module purge
-module load SBCL/2.5.3-GCCcore-13.3.0
-module load binutils/2.42-GCCcore-13.3.0 # required for distributions package
-sbcl --noinform --eval "(ql:quickload :crs-conventionality)" --quit
-
