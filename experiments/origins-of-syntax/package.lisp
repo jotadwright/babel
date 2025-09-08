3ecb1e5289dceb907edf2d19e85d9ead547f86f8
Jamie Wright <54960963+jotadwright@users.noreply.github.com>
2025-09-08 15:12:03 +0200

    Delete experiments/origins-of-syntax directory

--- experiments/origins-of-syntax/package.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-user)
-
-(defpackage :origins-of-syntax
-  (:documentation "A package for experiments on the origins of syntax using anti-unification and type hierarchies in FCG.")
-  (:use :common-lisp :utils :test-framework :irl :fcg :meta-layer-learning :experiment-framework :monitors :plot-raw-data #+:hunchentoot-available-on-this-platform :web-interface))
