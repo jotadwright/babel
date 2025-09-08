09e6f80ea5f442444e1b20d8673b2c4d29a982d1
Jamie Wright <54960963+jotadwright@users.noreply.github.com>
2025-09-08 15:12:16 +0200

    Delete experiments/pattern-finding/geoquery directory

--- experiments/pattern-finding/geoquery/utils.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :pf-for-sql)
-
-;;----------------------;;
-;; working with strings ;;
-;;----------------------;;
-
-(defun remove-last-character (str)
-  (subseq str 0 (- (length str) 1)))
